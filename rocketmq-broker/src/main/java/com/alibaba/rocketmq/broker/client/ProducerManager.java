75f52c1e93aca8652492bf440f76ce6af90d842d
shijia.wxr <shijia.wxr@taobao.com>
2013-05-16 19:49:23 +0800

    开发Producer连接管理，为Broker增加Producer连接管理

--- rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/client/ProducerManager.java
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/client/ProducerManager.java
@@ -3,11 +3,27 @@
  */
 package com.alibaba.rocketmq.broker.client;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import io.netty.channel.Channel;
+
+
 /**
  * 管理Producer组及各个Producer连接
  * 
  * @author vintage.wang@gmail.com shijia.wxr@taobao.com
  */
 public class ProducerManager {
+    private final ConcurrentHashMap<String, Set<Channel>> groupHashcodeChannelTable =
+            new ConcurrentHashMap<String, Set<Channel>>();
 
+
+    public ProducerManager() {
+
+    }
+
+
+    public void registerProducer(final String group, final ClientChannelInfo clientChannelInfo) {
+
+    }
 }
