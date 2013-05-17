c0febd8375e9717dade419f79a89d3fcad0bea26
shijia.wxr <shijia.wxr@taobao.com>
2013-05-17 14:30:29 +0800

    修改事务测试代码

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/simple/TranExecuterImpl.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/simple/TranExecuterImpl.java
@@ -1,24 +1,32 @@
 package com.alibaba.rocketmq.example.simple;
 
 import com.alibaba.rocketmq.client.producer.LocalTransactionExecuter;
+import com.alibaba.rocketmq.common.Message;
 
-public class TranExecuterImpl implements LocalTransactionExecuter{
-	private int transactionStats=-1;//0 send ,1 commit,2 rollback
-	@Override
-	public boolean executeLocalTransactionBranch() {
-		if (transactionStats ==1){
-		// TODO Auto-generated method stub
-			return true;
-		}else if(transactionStats ==2){
-			return false;
-		}
-		return false;
-	}
-	public int getTransactionStats() {
-		return transactionStats;
-	}
-	public void setTransactionStats(int transactionStats) {
-		this.transactionStats = transactionStats;
-	}
+
+public class TranExecuterImpl implements LocalTransactionExecuter {
+    private int transactionStats = -1;// 0 send ,1 commit,2 rollback
+
+
+    @Override
+    public boolean executeLocalTransactionBranch(Message msg) {
+        if (transactionStats == 1) {
+            return true;
+        }
+        else if (transactionStats == 2) {
+            return false;
+        }
+        return false;
+    }
+
+
+    public int getTransactionStats() {
+        return transactionStats;
+    }
+
+
+    public void setTransactionStats(int transactionStats) {
+        this.transactionStats = transactionStats;
+    }
 
 }
