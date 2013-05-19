dc5ade2a09bea8b96419fd5c0dc6921b2aef6695
shijia.wxr <shijia.wxr@taobao.com>
2013-05-19 17:44:11 +0800

    #3 客户端增加处理检测事务状态请求

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/ClientRemotingProcessor.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/ClientRemotingProcessor.java
@@ -8,6 +8,11 @@ import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 
 import com.alibaba.rocketmq.client.impl.factory.MQClientFactory;
+import com.alibaba.rocketmq.common.protocol.MQProtos.MQRequestCode;
+import com.alibaba.rocketmq.common.protocol.header.CheckTransactionStateRequestHeader;
+import com.alibaba.rocketmq.common.protocol.header.CheckTransactionStateResponseHeader;
+import com.alibaba.rocketmq.common.protocol.header.SendMessageRequestHeader;
+import com.alibaba.rocketmq.common.protocol.header.SendMessageResponseHeader;
 import com.alibaba.rocketmq.remoting.exception.RemotingCommandException;
 import com.alibaba.rocketmq.remoting.netty.NettyRequestProcessor;
 import com.alibaba.rocketmq.remoting.protocol.RemotingCommand;
@@ -32,7 +37,29 @@ public class ClientRemotingProcessor implements NettyRequestProcessor {
     @Override
     public RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand request)
             throws RemotingCommandException {
-        // TODO Auto-generated method stub
+        MQRequestCode code = MQRequestCode.valueOf(request.getCode());
+        switch (code) {
+        case CHECK_TRANSACTION_STATE:
+            return this.checkTransactionState(ctx, request);
+        default:
+            break;
+        }
         return null;
+    }
+
+
+    public RemotingCommand checkTransactionState(ChannelHandlerContext ctx, RemotingCommand request)
+            throws RemotingCommandException {
+        final RemotingCommand response =
+                RemotingCommand.createResponseCommand(CheckTransactionStateResponseHeader.class);
+        final CheckTransactionStateResponseHeader responseHeader =
+                (CheckTransactionStateResponseHeader) response.getCustomHeader();
+        final CheckTransactionStateRequestHeader requestHeader =
+                (CheckTransactionStateRequestHeader) request
+                    .decodeCommandCustomHeader(CheckTransactionStateRequestHeader.class);
+
+        response.setOpaque(request.getOpaque());
+
+        return response;
     }
 }
