e97faef85f6ab5c257246d0a17f592216d558fc9
shijia.wxr <shijia.wxr@taobao.com>
2013-05-23 10:55:54 +0800

    #19 重构事务Example，方便调试事务的各种状态

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/transaction/TransactionCheckListenerImpl.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/transaction/TransactionCheckListenerImpl.java
@@ -1,15 +1,29 @@
 package com.alibaba.rocketmq.example.transaction;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.alibaba.rocketmq.client.producer.LocalTransactionState;
 import com.alibaba.rocketmq.client.producer.TransactionCheckListener;
 import com.alibaba.rocketmq.common.MessageExt;
 
 
 public class TransactionCheckListenerImpl implements TransactionCheckListener {
+    private AtomicInteger transactionIndex = new AtomicInteger(1);
+
 
     @Override
     public LocalTransactionState checkLocalTransactionState(MessageExt msg) {
         System.out.println("server checking TrMsg " + msg.toString());
-        return LocalTransactionState.ROLLBACK_MESSAGE;
+
+        int value = transactionIndex.getAndIncrement();
+
+        if ((value % 2) == 0) {
+            return LocalTransactionState.ROLLBACK_MESSAGE;
+        }
+        else if ((value % 3) == 0) {
+            return LocalTransactionState.COMMIT_MESSAGE;
+        }
+
+        return LocalTransactionState.UNKNOW;
     }
 }
--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/transaction/TransactionExecuterImpl.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/transaction/TransactionExecuterImpl.java
@@ -1,32 +1,27 @@
 package com.alibaba.rocketmq.example.transaction;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.alibaba.rocketmq.client.producer.LocalTransactionExecuter;
 import com.alibaba.rocketmq.client.producer.LocalTransactionState;
 import com.alibaba.rocketmq.common.Message;
 
 
-public class TranExecuterImpl implements LocalTransactionExecuter {
-    private int transactionStats = -1;// 0 send ,1 commit,2 rollback
-
-
-    public int getTransactionStats() {
-        return transactionStats;
-    }
-
-
-    public void setTransactionStats(int transactionStats) {
-        this.transactionStats = transactionStats;
-    }
+public class TransactionExecuterImpl implements LocalTransactionExecuter {
+    private AtomicInteger transactionIndex = new AtomicInteger(1);
 
 
     @Override
     public LocalTransactionState executeLocalTransactionBranch(Message msg) {
-        if (transactionStats == 1) {
+        int value = transactionIndex.getAndIncrement();
+
+        if ((value % 2) == 0) {
             return LocalTransactionState.COMMIT_MESSAGE;
         }
-        else if (transactionStats == 2) {
+        else if ((value % 3) == 0) {
             return LocalTransactionState.ROLLBACK_MESSAGE;
         }
+
         return LocalTransactionState.UNKNOW;
     }
 }
