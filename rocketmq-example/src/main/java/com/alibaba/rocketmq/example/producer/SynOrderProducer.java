ada08f73d0ceb1989b85eefc1facbed077925851
jin.qian <jin.qian@alipay.com>
2013-05-26 13:12:49 +0800

    同步普通消息 异步普通消息 Oneway普通消息 同步顺序消息 异步顺序消息 Oneway顺序消息 分布式事务消息

---
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/producer/SynOrderProducer.java
@@ -0,0 +1,44 @@
+package com.alibaba.rocketmq.example.producer;
+
+import com.alibaba.rocketmq.client.exception.MQBrokerException;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.MQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.Message;
+import com.alibaba.rocketmq.remoting.exception.RemotingException;
+
+public class SynOrderProducer {
+	/**
+	 * @param args
+	 * @throws MQClientException 
+	 */
+	public static void main(String[] args) throws MQClientException {
+
+		MQProducer synproducer = new DefaultMQProducer("example.producer");
+
+		synproducer.start();
+
+        String[] tags = new String[] { "TagA", "TagB", "TagC", "TagD", "TagE" };
+
+        for (int i = 0; i < 10; i++) {
+			try {
+				Message msg =
+	                    new Message("TopicTest", tags[i % tags.length], "KEY" + i, ("Hello RocketMQ from synproducer" + i).getBytes());
+	            SendResult sendResult;
+				sendResult = synproducer.send(msg);
+				System.out.println(sendResult);
+			} catch (RemotingException e) {
+				e.printStackTrace();
+			} catch (MQBrokerException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (MQClientException e1) {
+				e1.printStackTrace();
+			}
+        }
+        synproducer.shutdown();
+        System.exit(0);
+	}
+}
