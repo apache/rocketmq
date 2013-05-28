a65544f30b3f6db79e115d005f9267e6149f13b1
jin.qian <jin.qian@alipay.com>
2013-05-28 13:23:49 +0800

    #19增加顺序消息Example

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/producer/AsyOrderProducer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/producer/AsyOrderProducer.java
@@ -1,11 +1,15 @@
 package com.alibaba.rocketmq.example.producer;
 
+import java.util.List;
+
 import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
 import com.alibaba.rocketmq.client.producer.MQProducer;
+import com.alibaba.rocketmq.client.producer.MessageQueueSelector;
 import com.alibaba.rocketmq.client.producer.SendCallback;
 import com.alibaba.rocketmq.client.producer.SendResult;
 import com.alibaba.rocketmq.common.Message;
+import com.alibaba.rocketmq.common.MessageQueue;
 import com.alibaba.rocketmq.remoting.exception.RemotingException;
 
 public class AsyOrderProducer {
@@ -32,11 +36,21 @@ public class AsyOrderProducer {
 			}
         	
         };
+        MessageQueueSelector selector = new MessageQueueSelector() {
+            @Override
+            public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                Integer id = (Integer) arg;
+                int index = id % mqs.size();
+                return mqs.get(index);
+            }
+        };
         for (int i = 0; i < 10; i++) {
 			try {
+				int orderId = i % 10;
 				Message msg =
 	                    new Message("TopicTest", tags[i % tags.length], "KEY" + i, ("Hello RocketMQ from asyproducer " + i).getBytes());
-				asyproducer.send(msg,sendCallback);
+				
+				asyproducer.send(msg, selector, orderId, sendCallback);
 			} catch (RemotingException e) {
 				e.printStackTrace();
 			} catch (InterruptedException e) {
