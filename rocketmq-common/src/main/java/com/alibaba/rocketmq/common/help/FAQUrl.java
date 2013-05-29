0270958369f1064f012d28c072bd0242b51af03d
shijia.wxr <shijia.wxr@taobao.com>
2013-05-29 11:29:13 +0800

    #54 将之前在Wiki上管理FAQ，改用Issue来管理，

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/help/FAQUrl.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/help/FAQUrl.java
@@ -6,5 +6,15 @@ package com.alibaba.rocketmq.common.help;
  * @author shijia.wxr<vintage.wang@gmail.com>
  */
 public class FAQUrl {
-    public static final String APPLY_TOPIC_URL = "https://github.com/alibaba/RocketMQ/wiki/FAQ#topic";
+    // Topic不存在，建议用户申请Topic
+    public static final String APPLY_TOPIC_URL = //
+            "https://github.com/alibaba/RocketMQ/issues/55";
+
+    // Client无法在本机启动多个JVM实例，建议用户修改成不同的实例名称
+    public static final String CLIENT_INSTACNCE_NAME_DUPLICATE_URL = //
+            "https://github.com/alibaba/RocketMQ/issues/56";
+
+    // Name Server地址不存在
+    public static final String NAME_SERVER_ADDR_NOT_EXIST_URL = //
+            "https://github.com/alibaba/RocketMQ/issues/57";
 }
