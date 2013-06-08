2873f8fb68e860774c65dd6b28a41d5a557fab37
shijia.wxr <shijia.wxr@taobao.com>
2013-06-08 16:11:30 +0800

    #18 mqadmin命令行工具增加日志初始化功能，

--- rocketmq-tools/src/main/java/com/alibaba/rocketmq/tools/MQAdminStartup.java
+++ rocketmq-tools/src/main/java/com/alibaba/rocketmq/tools/MQAdminStartup.java
@@ -3,6 +3,14 @@ package com.alibaba.rocketmq.tools;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+import com.alibaba.rocketmq.common.MixAll;
 import com.alibaba.rocketmq.tools.broker.BrokerSubCommand;
 import com.alibaba.rocketmq.tools.cluster.ClusterSubCommand;
 import com.alibaba.rocketmq.tools.connection.ConnectionSubCommand;
@@ -67,27 +75,45 @@ public class MQAdminStartup {
     }
 
 
-    public static void main(String[] args) {
+    private static void initLogback() throws JoranException {
+        String rocketmqHome =
+                System.getProperty(MixAll.ROCKETMQ_HOME_PROPERTY, System.getenv(MixAll.ROCKETMQ_HOME_ENV));
 
-        switch (args.length) {
-        case 0:
-        case 1:
-            printHelp();
-            break;
-        case 2:
-            if (args[0].equals("help")) {
-                SubCommand cmd = findSubCommand(args[1]);
+        // ≥ı ºªØLogback
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(lc);
+        lc.reset();
+        configurator.doConfigure(rocketmqHome + "/conf/log4j_tools.xml");
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            switch (args.length) {
+            case 0:
+            case 1:
+                printHelp();
+                break;
+            case 2:
+                if (args[0].equals("help")) {
+                    SubCommand cmd = findSubCommand(args[1]);
+                    if (cmd != null) {
+                        cmd.printHelp();
+                    }
+                    break;
+                }
+            default:
+                SubCommand cmd = findSubCommand(args[0]);
                 if (cmd != null) {
-                    cmd.printHelp();
+                    initLogback();
+                    cmd.execute(parseSubArgs(args));
                 }
                 break;
             }
-        default:
-            SubCommand cmd = findSubCommand(args[0]);
-            if (cmd != null) {
-                cmd.execute(parseSubArgs(args));
-            }
-            break;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
