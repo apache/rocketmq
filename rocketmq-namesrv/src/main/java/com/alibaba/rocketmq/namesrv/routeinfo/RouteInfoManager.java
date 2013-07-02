20a4afb3d1cc5daf8526d11c378c10ffee37d68b
shijia.wxr <shijia.wxr@taobao.com>
2013-07-02 19:25:10 +0800

    #92 优化Name Server

--- rocketmq-namesrv/src/main/java/com/alibaba/rocketmq/namesrv/routeinfo/RouteInfoManager.java
+++ rocketmq-namesrv/src/main/java/com/alibaba/rocketmq/namesrv/routeinfo/RouteInfoManager.java
@@ -1,9 +1,14 @@
 package com.alibaba.rocketmq.namesrv.routeinfo;
 
+import io.netty.channel.Channel;
+
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -12,6 +17,7 @@ import org.slf4j.LoggerFactory;
 
 import com.alibaba.rocketmq.common.DataVersion;
 import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.TopicConfig;
 import com.alibaba.rocketmq.common.constant.LoggerName;
 import com.alibaba.rocketmq.common.protocol.body.TopicConfigSerializeWrapper;
 import com.alibaba.rocketmq.common.protocol.route.BrokerData;
@@ -31,31 +37,32 @@ public class RouteInfoManager {
     private final HashMap<String/* topic */, List<QueueData>> topicQueueTable;
     private final HashMap<String/* brokerName */, BrokerData> brokerAddrTable;
     private final HashMap<String/* clusterName */, Set<String/* brokerName */>> clusterAddrTable;
-    private final HashMap<String/* brokerAddr */, Long/* last update timestamp */> brokerLiveTable;
-    private final HashMap<String/* brokerAddr */, DataVersion> topicVersionTable;
+    private final HashMap<String/* brokerAddr */, BrokerLiveInfo> brokerLiveTable;
 
 
     public RouteInfoManager() {
         this.topicQueueTable = new HashMap<String, List<QueueData>>(1024);
         this.brokerAddrTable = new HashMap<String, BrokerData>(128);
         this.clusterAddrTable = new HashMap<String, Set<String>>(32);
-        this.brokerLiveTable = new HashMap<String, Long>(256);
-        this.topicVersionTable = new HashMap<String, DataVersion>(256);
+        this.brokerLiveTable = new HashMap<String, BrokerLiveInfo>(256);
     }
 
 
     /**
      * 判断Topic配置信息是否发生变更
      */
-    private boolean isBrokerTopicConfigChanged(final String brokerAddr, final DataVersion dataVersion) {
-        DataVersion prev = this.topicVersionTable.get(brokerAddr);
+    private boolean isBrokerTopicConfigChanged(final String brokerAddr, final DataVersion dataVersion,
+            final Channel channel) {
+        BrokerLiveInfo prev = this.brokerLiveTable.get(brokerAddr);
         if (null == prev) {
-            this.topicVersionTable.put(brokerAddr, dataVersion);
+            this.brokerLiveTable.put(brokerAddr, new BrokerLiveInfo(System.currentTimeMillis(), dataVersion,
+                channel));
             return true;
         }
         else {
             if (!prev.equals(dataVersion)) {
-                this.topicVersionTable.put(brokerAddr, dataVersion);
+                this.brokerLiveTable.put(brokerAddr, new BrokerLiveInfo(System.currentTimeMillis(),
+                    dataVersion, channel));
                 return true;
             }
         }
@@ -64,18 +71,49 @@ public class RouteInfoManager {
     }
 
 
+    private void createAndUpdateQueueData(final String brokerName, final TopicConfig topicConfig) {
+        QueueData queueData = new QueueData();
+        queueData.setBrokerName(brokerName);
+        queueData.setWriteQueueNums(topicConfig.getWriteQueueNums());
+        queueData.setReadQueueNums(topicConfig.getReadQueueNums());
+        queueData.setPerm(topicConfig.getPerm());
+
+        List<QueueData> queueDataList = this.topicQueueTable.get(topicConfig.getTopicName());
+        if (null == queueDataList) {
+            queueDataList = new LinkedList<QueueData>();
+            queueDataList.add(queueData);
+            this.topicQueueTable.put(topicConfig.getTopicName(), queueDataList);
+        }
+        else {
+            Iterator<QueueData> it = queueDataList.iterator();
+            while (it.hasNext()) {
+                QueueData qd = it.next();
+                if (qd.getBrokerName().equals(brokerName)) {
+                    it.remove();
+                    break;
+                }
+            }
+
+            queueDataList.add(queueData);
+        }
+    }
+
+
     public void registerBroker(//
             final String clusterName,//
             final String brokerAddr,//
             final String brokerName,//
             final long brokerId,//
-            final TopicConfigSerializeWrapper topicConfig//
+            final TopicConfigSerializeWrapper topicConfigWrapper,//
+            final Channel channel//
     ) {
         try {
             try {
                 this.lock.writeLock().lockInterruptibly();
                 // 更新最后变更时间
-                this.brokerLiveTable.put(brokerAddr, System.currentTimeMillis());
+                this.brokerLiveTable.put(brokerAddr, //
+                    new BrokerLiveInfo(System.currentTimeMillis(), topicConfigWrapper.getDataVersion(),
+                        channel));
 
                 // 更新集群信息
                 Set<String> brokerNames = this.clusterAddrTable.get(clusterName);
@@ -98,10 +136,19 @@ public class RouteInfoManager {
                 brokerData.getBrokerAddrs().put(brokerId, brokerAddr);
 
                 // 更新Topic信息
-                if (null != topicConfig //
+                if (null != topicConfigWrapper //
                         && MixAll.MASTER_ID == brokerId) {
-                    if (this.isBrokerTopicConfigChanged(brokerAddr, topicConfig.getDataVersion())) {
-
+                    if (this.isBrokerTopicConfigChanged(brokerAddr,//
+                        topicConfigWrapper.getDataVersion(),//
+                        channel)) {
+                        ConcurrentHashMap<String, TopicConfig> tcTable =
+                                topicConfigWrapper.getTopicConfigTable();
+                        if (tcTable != null) {
+                            for (String topic : tcTable.keySet()) {
+                                TopicConfig topicConfig = tcTable.get(topic);
+                                this.createAndUpdateQueueData(brokerName, topicConfig);
+                            }
+                        }
                     }
                 }
             }
@@ -121,6 +168,97 @@ public class RouteInfoManager {
 
 
     public TopicRouteData pickupTopicRouteData(final String topic) {
+        TopicRouteData topicRouteData = new TopicRouteData();
+        boolean foundQueueData = false;
+        boolean foundBrokerData = false;
+        Set<String> brokerNameSet = new HashSet<String>();
+        List<BrokerData> brokerDataList = new LinkedList<BrokerData>();
+        topicRouteData.setBrokerDatas(brokerDataList);
+
+        try {
+            try {
+                this.lock.readLock().lockInterruptibly();
+                List<QueueData> queueDataList = this.topicQueueTable.get(topic);
+                if (queueDataList != null) {
+                    topicRouteData.setQueueDatas(queueDataList);
+                    foundQueueData = true;
+                }
+
+                // BrokerName去重
+                Iterator<QueueData> it = queueDataList.iterator();
+                while (it.hasNext()) {
+                    QueueData qd = it.next();
+                    brokerNameSet.add(qd.getBrokerName());
+                }
+
+                for (String brokerName : brokerNameSet) {
+                    BrokerData brokerData = this.brokerAddrTable.get(brokerName);
+                    if (null != brokerData) {
+                        brokerDataList.add(brokerData);
+                        foundBrokerData = true;
+                    }
+                }
+            }
+            finally {
+                this.lock.readLock().unlock();
+            }
+        }
+        catch (Exception e) {
+            log.error("pickupTopicRouteData Exception", e);
+        }
+
+        if (foundBrokerData && foundQueueData) {
+            return topicRouteData;
+        }
+
         return null;
+    }
+
+
+    public void scanNotActiveBroker() {
+    }
+}
+
+
+class BrokerLiveInfo {
+    private long lastUpdateTimestamp;
+    private DataVersion dataVersion;
+    private Channel channel;
+
+
+    public BrokerLiveInfo(long lastUpdateTimestamp, DataVersion dataVersion, Channel channel) {
+        this.lastUpdateTimestamp = lastUpdateTimestamp;
+        this.dataVersion = dataVersion;
+        this.channel = channel;
+    }
+
+
+    public long getLastUpdateTimestamp() {
+        return lastUpdateTimestamp;
+    }
+
+
+    public void setLastUpdateTimestamp(long lastUpdateTimestamp) {
+        this.lastUpdateTimestamp = lastUpdateTimestamp;
+    }
+
+
+    public DataVersion getDataVersion() {
+        return dataVersion;
+    }
+
+
+    public void setDataVersion(DataVersion dataVersion) {
+        this.dataVersion = dataVersion;
+    }
+
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
     }
 }
