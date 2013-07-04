4008ae0c12095ad544644cc6b27ea8c7ed9d19af
傅冲 <yubao.fyb@alibaba-inc.com>
2013-07-04 10:02:45 +0800

    重写平均分配队列算法

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/consumer/rebalance/AllocateMessageQueueAveragely.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/consumer/rebalance/AllocateMessageQueueAveragely.java
@@ -19,21 +19,37 @@ import com.alibaba.rocketmq.common.message.MessageQueue;
 public class AllocateMessageQueueAveragely implements AllocateMessageQueueStrategy {
     @Override
     public List<MessageQueue> allocate(String currentCID, List<MessageQueue> mqAll, List<String> cidAll) {
+        if(currentCID==null||currentCID.length()<1){
+            throw new IllegalArgumentException("currentCID is empty");
+        }
+        if(mqAll==null||mqAll.size()<1){
+            throw new IllegalArgumentException("mqAll is null or  mqAll'size  less one");
+        }
+        if(cidAll==null||cidAll.size()<1){
+            throw new IllegalArgumentException("cidAll is null or  cidAll'size less one");
+        }
+
         List<MessageQueue> result = new ArrayList<MessageQueue>();
-        int currentIndex = cidAll.indexOf(currentCID);
-        if (currentIndex < 0) {
+        if (!cidAll.contains(currentCID)) { // 不存在此ConsumerId ,直接返回
             return result;
         }
 
-        int mod = mqAll.size() / cidAll.size();
-        int rem = mqAll.size() % cidAll.size();
-        int startindex = mod * currentIndex;
-        int endindex = startindex + mod;
-        for (int i = startindex; i < endindex; i++) {
+        int index = cidAll.indexOf(currentCID);
+        int averageSize = mqAll.size() / cidAll.size();
+        int mod = mqAll.size() % cidAll.size();
+        int startIndex = index * averageSize;
+        int endIndex = (index + 1) * averageSize;
+        for (int i = startIndex; i < endIndex; i++) {
             result.add(mqAll.get(i));
         }
-        if (rem > currentIndex) {
-            result.add(mqAll.get(currentIndex + mod * cidAll.size()));
+
+        // 如果当前的consumerId最后一个且还有剩下的队列，应该把最后队列都放到当前consumerId队列里
+        boolean isAddRemainQueue = (index == cidAll.size()-1)&&mod > 0;
+        if (isAddRemainQueue) {
+            int messageQueueSize = mqAll.size();
+            for (int i = endIndex; i < messageQueueSize; i++) {
+                result.add(mqAll.get(i));
+            }
         }
         return result;
     }
