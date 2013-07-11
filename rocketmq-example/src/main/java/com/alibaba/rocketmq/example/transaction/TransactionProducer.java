b99c2d84045387cfd72316964661ddf50ccb7007
菱叶 <jin.qian@alipay.com>
2013-07-11 12:32:39 +0800

    事务回查压测代码

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/transaction/TransactionProducer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/transaction/TransactionProducer.java
@@ -19,6 +19,12 @@ public class TransactionProducer {
 
         TransactionCheckListener transactionCheckListener = new TransactionCheckListenerImpl();
         TransactionMQProducer producer = new TransactionMQProducer("example.producer");
+        //事务回查最小并发数
+        producer.setCheckThreadPoolMinSize(2);
+        //事务回查最大并发数
+        producer.setCheckThreadPoolMaxSize(2);
+        //队列数
+        producer.setCheckRequestHoldMax(2000);
         producer.setTransactionCheckListener(transactionCheckListener);
         producer.start();
 
