edbefcbf55dc92b3d806f1b02eb2de883bee53de
菱叶 <jin.qian@alipay.com>
2013-07-16 13:27:38 +0800

    MessageQueue 加serilizable

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/message/MessageQueue.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/message/MessageQueue.java
@@ -3,12 +3,15 @@
  */
 package com.alibaba.rocketmq.common.message;
 
+import java.io.Serializable;
+
 /**
  * 消息队列数据结构，对外提供
  * 
  * @author shijia.wxr<vintage.wang@gmail.com>
  */
-public class MessageQueue implements Comparable<MessageQueue> {
+public class MessageQueue implements Comparable<MessageQueue>,Serializable {
+    private static final long serialVersionUID = 1L;
     private String topic;
     private String brokerName;
     private int queueId;
