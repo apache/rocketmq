1dbb600f242763ff10a077139a3867a3124603ee
shijia.wxr <shijia.wxr@taobao.com>
2013-07-17 22:49:00 +0800

    #4 修复测试用例编译失败的问题

--- rocketmq-remoting/src/test/java/com/alibaba/rocketmq/remoting/NettyRPCTest.java
+++ rocketmq-remoting/src/test/java/com/alibaba/rocketmq/remoting/NettyRPCTest.java
@@ -10,6 +10,8 @@ import java.util.concurrent.Executors;
 
 import org.junit.Test;
 
+import com.alibaba.rocketmq.remoting.annotation.CFNullable;
+import com.alibaba.rocketmq.remoting.exception.RemotingCommandException;
 import com.alibaba.rocketmq.remoting.exception.RemotingConnectException;
 import com.alibaba.rocketmq.remoting.exception.RemotingSendRequestException;
 import com.alibaba.rocketmq.remoting.exception.RemotingTimeoutException;
@@ -167,4 +169,73 @@ public class NettyRPCTest {
         System.out.println("-----------------------------------------------------------------");
     }
 
+}
+
+
+class TestRequestHeader implements CommandCustomHeader {
+    @CFNullable
+    private Integer count;
+
+    @CFNullable
+    private String messageTitle;
+
+
+    @Override
+    public void checkFields() throws RemotingCommandException {
+    }
+
+
+    public Integer getCount() {
+        return count;
+    }
+
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+
+    public String getMessageTitle() {
+        return messageTitle;
+    }
+
+
+    public void setMessageTitle(String messageTitle) {
+        this.messageTitle = messageTitle;
+    }
+}
+
+
+class TestResponseHeader implements CommandCustomHeader {
+    @CFNullable
+    private Integer count;
+
+    @CFNullable
+    private String messageTitle;
+
+
+    @Override
+    public void checkFields() throws RemotingCommandException {
+
+    }
+
+
+    public Integer getCount() {
+        return count;
+    }
+
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+
+    public String getMessageTitle() {
+        return messageTitle;
+    }
+
+
+    public void setMessageTitle(String messageTitle) {
+        this.messageTitle = messageTitle;
+    }
 }
