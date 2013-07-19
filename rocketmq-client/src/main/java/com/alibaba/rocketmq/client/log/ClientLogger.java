b2afc5de0835d9bd9566b664176208fd65cb61f9
钱进 <jin.qian@alipay.com>
2013-07-19 17:48:15 +0800

    客户端去除logback依赖

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/log/ClientLogger.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/log/ClientLogger.java
@@ -5,8 +5,8 @@ import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.joran.JoranConfigurator;
+//import ch.qos.logback.classic.LoggerContext;
+//import ch.qos.logback.classic.joran.JoranConfigurator;
 
 import com.alibaba.rocketmq.common.constant.LoggerName;
 
@@ -21,30 +21,30 @@ public class ClientLogger {
 
 
     private static Logger createLogger(final String loggerName) {
-        String logConfigFilePath =
-                System.getProperty("rocketmq.client.log.configFile",
-                    System.getenv("ROCKETMQ_CLIENT_LOG_CONFIGFILE"));
-
-        try {
-            LoggerContext lc = new LoggerContext();
-            JoranConfigurator configurator = new JoranConfigurator();
-            configurator.setContext(lc);
-            lc.reset();
-
-            if (null == logConfigFilePath) {
-                // 如果应用没有配置，则使用jar包内置配置
-                URL url = ClientLogger.class.getClassLoader().getResource("logback_rocketmq_client.xml");
-                configurator.doConfigure(url);
-            }
-            else {
-                configurator.doConfigure(logConfigFilePath);
-            }
-
-            return lc.getLogger(LoggerName.ClientLoggerName);
-        }
-        catch (Exception e) {
-            System.err.println(e);
-        }
+//        String logConfigFilePath =
+//                System.getProperty("rocketmq.client.log.configFile",
+//                    System.getenv("ROCKETMQ_CLIENT_LOG_CONFIGFILE"));
+//
+//        try {
+//            LoggerContext lc = new LoggerContext();
+//            JoranConfigurator configurator = new JoranConfigurator();
+//            configurator.setContext(lc);
+//            lc.reset();
+//
+//            if (null == logConfigFilePath) {
+//                // 如果应用没有配置，则使用jar包内置配置
+//                URL url = ClientLogger.class.getClassLoader().getResource("logback_rocketmq_client.xml");
+//                configurator.doConfigure(url);
+//            }
+//            else {
+//                configurator.doConfigure(logConfigFilePath);
+//            }
+//
+//            return lc.getLogger(LoggerName.ClientLoggerName);
+//        }
+//        catch (Exception e) {
+//            System.err.println(e);
+//        }
 
         return LoggerFactory.getLogger(LoggerName.ClientLoggerName);
     }
