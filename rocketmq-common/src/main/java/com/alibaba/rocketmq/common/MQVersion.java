2d0635fc30b66ce6a5eba668e1327d037a80984b
shijia.wxr <shijia.wxr@taobao.com>
2013-07-25 14:25:14 +0800

    #4 修改版本，在开发分支上修改，改为3.0.0-SNAPSHOT

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/MQVersion.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/MQVersion.java
@@ -22,7 +22,7 @@ package com.alibaba.rocketmq.common;
  */
 public class MQVersion {
     // TODO 每次发布版本都要修改此处版本号
-    public static final int CurrentVersion = Version.V3_0_0_BETA1.ordinal();
+    public static final int CurrentVersion = Version.V3_0_0_SNAPSHOT.ordinal();
 
     enum Version {
         V3_0_0_SNAPSHOT,
