6ef66924560af2f642ce6aa4eba6719bac6a0fc9
钱进 <jin.qian@alipay.com>
2013-08-01 10:08:38 +0800

    增加sendback 监控

---
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/digestlog/SendbackmsgLiveMoniter.java
@@ -0,0 +1,19 @@
+package com.alibaba.rocketmq.broker.digestlog;
+
+import io.netty.channel.Channel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.rocketmq.remoting.common.RemotingHelper;
+import com.alibaba.rocketmq.remoting.protocol.RemotingCommand;
+import com.alibaba.rocketmq.store.PutMessageResult;
+
+public class SendbackmsgLiveMoniter {
+    private static final Logger log = LoggerFactory.getLogger("SendbackmsgLiveMoniter");
+    public static void printProcessSendmsgRequestLive(Channel channel, RemotingCommand request,PutMessageResult putMessageResult,int delayLevel,int reconsumeTimes){
+        if(log.isInfoEnabled()){
+            log.info("receive [{}] SendBackMessage request command[{}] , this msg ReconsumeTimes[{}] and DelayLevel [{}]. return result [{}] ",RemotingHelper.parseChannelRemoteName(channel),reconsumeTimes,delayLevel, request,putMessageResult);
+        }
+    }
+}
