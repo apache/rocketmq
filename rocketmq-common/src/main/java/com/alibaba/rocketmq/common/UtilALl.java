c2751f6540e0f194f1c137e086640823643aeed6
shijia.wxr <shijia.wxr@taobao.com>
2013-08-04 19:21:12 +0800

    fixed #198 开发完成查看Topic统计信息的命令行工具

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/UtilALl.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/UtilALl.java
@@ -103,6 +103,15 @@ public class UtilALl {
     }
 
 
+    public static String timeMillisToHumanString2(final long t) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(t);
+        return String.format("%04d-%02d-%02d %02d:%02d:%02d,%03d", cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.HOUR_OF_DAY),
+            cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND), cal.get(Calendar.MILLISECOND));
+    }
+
+
     /**
      * 获取磁盘分区空间使用率
      */
