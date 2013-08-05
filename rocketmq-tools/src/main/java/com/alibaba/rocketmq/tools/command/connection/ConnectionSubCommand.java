3c44c1adf9bce8e3d8e3acd5831562a0710ff788
shijia.wxr <shijia.wxr@taobao.com>
2013-08-05 17:22:01 +0800

    #200 连接命令工具开发ok

---
+++ rocketmq-tools/src/main/java/com/alibaba/rocketmq/tools/command/connection/ConnectionSubCommand.java
@@ -0,0 +1,139 @@
+package com.alibaba.rocketmq.tools.command.connection;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import com.alibaba.rocketmq.common.MQVersion;
+import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.protocol.body.Connection;
+import com.alibaba.rocketmq.common.protocol.body.ConsumerConnectionSerializeWrapper;
+import com.alibaba.rocketmq.common.protocol.body.ProducerConnectionSerializeWrapper;
+import com.alibaba.rocketmq.common.protocol.heartbeat.SubscriptionData;
+import com.alibaba.rocketmq.tools.admin.DefaultMQAdminExt;
+import com.alibaba.rocketmq.tools.command.SubCommand;
+
+
+/**
+ * 连接查询工具
+ * 
+ * @author shijia.wxr<vintage.wang@gmail.com>
+ * @since 2013-8-5
+ */
+public class ConnectionSubCommand implements SubCommand {
+
+    @Override
+    public String commandName() {
+        return "connectionList";
+    }
+
+
+    @Override
+    public String commandDesc() {
+        return "Fetch producer or consumer socket connection";
+    }
+
+
+    @Override
+    public Options buildCommandlineOptions(Options options) {
+        Option opt = new Option("p", "producerGroup", true, "producer group name");
+        opt.setRequired(false);
+        options.addOption(opt);
+
+        opt = new Option("c", "consumerGroup", true, "consumer group name");
+        opt.setRequired(false);
+        options.addOption(opt);
+
+        return options;
+    }
+
+
+    @Override
+    public void execute(CommandLine commandLine, Options options) {
+        DefaultMQAdminExt defaultMQAdminExt = new DefaultMQAdminExt();
+
+        defaultMQAdminExt.setInstanceName(Long.toString(System.currentTimeMillis()));
+
+        try {
+            if (commandLine.hasOption('t')) {
+                String topic = commandLine.getOptionValue('t');
+
+                // Producer
+                if (commandLine.hasOption('p')) {
+                    defaultMQAdminExt.start();
+
+                    String group = commandLine.getOptionValue('p');
+
+                    ProducerConnectionSerializeWrapper pc =
+                            defaultMQAdminExt.examineProducerConnectionInfo(group, topic);
+
+                    int i = 1;
+                    for (Connection conn : pc.getConnectionSet()) {
+                        System.out.printf("%04d %-32s %-22s %-8s %s\n",//
+                            i++,//
+                            conn.getClientId(),//
+                            conn.getClientAddr(),//
+                            conn.getLanguage(),//
+                            MQVersion.getVersionDesc(conn.getVersion())//
+                            );
+                    }
+
+                    return;
+                }
+                // Consumer
+                else if (commandLine.hasOption('c')) {
+                    defaultMQAdminExt.start();
+
+                    String group = commandLine.getOptionValue('c');
+
+                    ConsumerConnectionSerializeWrapper cc =
+                            defaultMQAdminExt.examineConsumerConnectionInfo(group, topic);
+
+                    // 打印连接
+                    int i = 1;
+                    for (Connection conn : cc.getConnectionSet()) {
+                        System.out.printf("%04d %-32s %-22s %-8s %s\n",//
+                            i++,//
+                            conn.getClientId(),//
+                            conn.getClientAddr(),//
+                            conn.getLanguage(),//
+                            MQVersion.getVersionDesc(conn.getVersion())//
+                            );
+                    }
+
+                    // 打印订阅关系
+                    System.out.println("\nBelow is subscription:");
+                    Iterator<Entry<String, SubscriptionData>> it =
+                            cc.getSubscriptionTable().entrySet().iterator();
+                    i = 1;
+                    while (it.hasNext()) {
+                        Entry<String, SubscriptionData> entry = it.next();
+                        SubscriptionData sd = entry.getValue();
+                        System.out.printf("%04d %Topic: -40s SubExpression: %s\n",//
+                            i++,//
+                            sd.getTopic(),//
+                            sd.getSubString()//
+                            );
+                    }
+
+                    // 打印其他订阅参数
+                    System.out.println("");
+                    System.out.printf("ConsumeType: %s\n", cc.getConsumeType());
+                    System.out.printf("MessageModel: %s\n", cc.getMessageModel());
+                    System.out.printf("ConsumeFromWhere: %s\n", cc.getConsumeFromWhere());
+                    return;
+                }
+            }
+            MixAll.printCommandLineHelp("mqadmin " + this.commandName(), options);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            defaultMQAdminExt.shutdown();
+        }
+    }
+}
