f44ba5ce6b235cac85a0bfaa728f1197d9ea62de
shijia.wxr <shijia.wxr@taobao.com>
2013-08-07 16:33:11 +0800

    fixed #208 修复获取Topic统计信息，Broker在计算Offset时计算错误的bug

--- rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/processor/AdminBrokerProcessor.java
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/processor/AdminBrokerProcessor.java
@@ -70,6 +70,7 @@ import com.alibaba.rocketmq.remoting.netty.NettyRequestProcessor;
 import com.alibaba.rocketmq.remoting.protocol.RemotingCommand;
 import com.alibaba.rocketmq.remoting.protocol.RemotingProtos.ResponseCode;
 import com.alibaba.rocketmq.remoting.protocol.RemotingSerializable;
+import com.alibaba.rocketmq.store.ConsumeQueue;
 
 
 /**
@@ -274,8 +275,10 @@ public class AdminBrokerProcessor implements NettyRequestProcessor {
 
             long timestamp = 0;
             if (max > 0) {
+                long absoluteOffset = (max - 1) * ConsumeQueue.CQStoreUnitSize;
                 timestamp =
-                        this.brokerController.getMessageStore().getMessageStoreTimeStamp(topic, i, max - 1);
+                        this.brokerController.getMessageStore().getMessageStoreTimeStamp(topic, i,
+                            absoluteOffset);
             }
 
             topicOffset.setMinOffset(min);
