6f5ca7f4c52a18a69eab4ad6cc33a23187b5b536
shijia.wxr <shijia.wxr@taobao.com>
2013-08-08 19:10:26 +0800

    #4 增加一个订阅Notify TRADE-SUB消息的例子。

---
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/notify/Consumer.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2010-2013 Alibaba Group Holding Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.rocketmq.example.notify;
+
+import java.util.List;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+
+/**
+ * 订阅Notify TRADE-SUB数据<br>
+ * RocketMQ维护了Notify TRADE-SUB topic下的全量数据
+ * 
+ * @author shijia.wxr<vintage.wang@gmail.com>
+ * @since 2013-8-8
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+
+        consumer.subscribe("TRADE-SUB",//
+            "10000-trade-paid-done || 1000-trade-paid-done || 1001-trade-paid-done");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                    ConsumeConcurrentlyContext context) {
+                System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.println("Consumer Started.");
+    }
+}
