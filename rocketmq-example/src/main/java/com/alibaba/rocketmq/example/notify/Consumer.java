d1fef319728c874e3e7fb6575727632e88e9106a
shijia.wxr <shijia.wxr@taobao.com>
2013-08-11 22:19:39 +0800

    #4 修改Notify订阅TRADE-SUB消息例子

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/notify/Consumer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/notify/Consumer.java
@@ -23,7 +23,6 @@ import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import com.alibaba.rocketmq.client.exception.MQClientException;
-import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
 
@@ -34,11 +33,10 @@ import com.alibaba.rocketmq.common.message.MessageExt;
 public class Consumer {
 
     public static void main(String[] args) throws MQClientException {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_5");
-        consumer.setNamesrvAddr("10.235.144.44:9876");
-        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_MIN_OFFSET);
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_10");
+
         consumer.subscribe("TRADE-SUB",//
-            "*");
+            "10000-trade-paid-done || 1000-trade-paid-done || 1001-trade-paid-done");
 
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             AtomicLong consumeTimes = new AtomicLong(0);
