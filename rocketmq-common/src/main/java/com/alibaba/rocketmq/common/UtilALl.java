b5f60448b2258bdefe1117d8b3fe2c171d862f06
manhong.yqd <manhong.yqd@taobao.com>
2013-08-22 13:59:14 +0800

    fixed #220  1.topic为空字符串时不允许创建 topic 或发送消息，抛MQClientException.  2.添加空字符串判断公用方法.

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/UtilALl.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/UtilALl.java
@@ -345,4 +345,18 @@ public class UtilALl {
 
         return str;
     }
+
+
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if ((Character.isWhitespace(str.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
