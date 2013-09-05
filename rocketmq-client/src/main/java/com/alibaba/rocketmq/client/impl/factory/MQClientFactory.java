fc5d157ddc853493c38336522d71c9295a224887
shijia.wxr <shijia.wxr@taobao.com>
2013-09-05 18:02:29 +0800

    fixed #242 修复线上环境关闭自动创建topic后，客户端仍然打印多余日志的问题。

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/factory/MQClientFactory.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/factory/MQClientFactory.java
@@ -613,7 +613,8 @@ public class MQClientFactory {
                     }
                 }
                 catch (Exception e) {
-                    if (!topic.startsWith(MixAll.RETRY_GROUP_TOPIC_PREFIX)) {
+                    if (!topic.startsWith(MixAll.RETRY_GROUP_TOPIC_PREFIX)
+                            && !topic.equals(MixAll.DEFAULT_TOPIC)) {
                         log.warn("updateTopicRouteInfoFromNameServer Exception", e);
                     }
                 }
