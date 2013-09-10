a425cc266da2e1ae4d2f79fe7c666c11d88fbfe8
manhong.yqd <manhong.yqd@taobao.com>
2013-09-10 15:23:29 +0800

    #4 测试

---
+++ rocketmq-qatest/src/test/java/com/alibaba/rocketmq/test/integration/benchmark/BenchmarkBaseTest.java
@@ -0,0 +1,45 @@
+package com.alibaba.rocketmq.test.integration.benchmark;
+
+import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.test.integration.BaseTest;
+import org.apache.commons.lang.StringUtils;
+
+
+/**
+ * @author manhong.yqd<jodie.yqd@gmail.com>
+ * @since 2013-8-26
+ */
+public class BenchmarkBaseTest extends BaseTest {
+    protected String consumerGroup = "qatest_Benchmark_consumer";
+    protected String producerGroup = "qatest_Benchmark_producer";
+    protected String topic = "qatest_BenchmarkTest";
+    static {
+        if (StringUtils.isNotBlank(NAME_SERVER_LIST)) {
+            System.setProperty(MixAll.NAMESRV_ADDR_PROPERTY, NAME_SERVER_LIST);
+        }
+        else {
+            try {
+                initNameServerAndBroker();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    protected static Message buildMessage(final int messageSize) {
+        Message msg = new Message();
+        msg.setTopic("BenchmarkTest");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < messageSize; i += 10) {
+            sb.append("hello baby");
+        }
+
+        msg.setBody(sb.toString().getBytes());
+
+        return msg;
+    }
+}
---
+++ rocketmq-qatest/src/test/java/com/alibaba/rocketmq/test/integration/benchmark/StatsBenchmarkProducer.java
@@ -0,0 +1,68 @@
+package com.alibaba.rocketmq.test.integration.benchmark;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * description
+ *
+ * @author: manhong.yqd<jodie.yqd@gmail.com>
+ * @since: 13-9-5
+ */
+public class StatsBenchmarkProducer {
+    // 1
+    private final AtomicLong sendRequestSuccessCount = new AtomicLong(0L);
+    // 2
+    private final AtomicLong sendRequestFailedCount = new AtomicLong(0L);
+    // 3
+    private final AtomicLong receiveResponseSuccessCount = new AtomicLong(0L);
+    // 4
+    private final AtomicLong receiveResponseFailedCount = new AtomicLong(0L);
+    // 5
+    private final AtomicLong sendMessageSuccessTimeTotal = new AtomicLong(0L);
+    // 6
+    private final AtomicLong sendMessageMaxRT = new AtomicLong(0L);
+
+
+    public Long[] createSnapshot() {
+        Long[] snap = new Long[] {//
+                System.currentTimeMillis(),//
+                        this.sendRequestSuccessCount.get(),//
+                        this.sendRequestFailedCount.get(),//
+                        this.receiveResponseSuccessCount.get(),//
+                        this.receiveResponseFailedCount.get(),//
+                        this.sendMessageSuccessTimeTotal.get(), //
+                };
+
+        return snap;
+    }
+
+
+    public AtomicLong getSendRequestSuccessCount() {
+        return sendRequestSuccessCount;
+    }
+
+
+    public AtomicLong getSendRequestFailedCount() {
+        return sendRequestFailedCount;
+    }
+
+
+    public AtomicLong getReceiveResponseSuccessCount() {
+        return receiveResponseSuccessCount;
+    }
+
+
+    public AtomicLong getReceiveResponseFailedCount() {
+        return receiveResponseFailedCount;
+    }
+
+
+    public AtomicLong getSendMessageSuccessTimeTotal() {
+        return sendMessageSuccessTimeTotal;
+    }
+
+
+    public AtomicLong getSendMessageMaxRT() {
+        return sendMessageMaxRT;
+    }
+}
