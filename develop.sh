257a16c6f50ed63f146028c1b40482f3d557ca15
shijia.wxr <shijia.wxr@taobao.com>
2013-11-21 15:42:23 +0800

    发布3.0.4-open版本

--- develop.sh
+++ develop.sh
@@ -9,4 +9,4 @@ export JAVA_HOME=/opt/taobao/java
 export PATH=/opt/taobao/mvn/bin:$JAVA_HOME/bin:$PATH
 mvn -Dmaven.test.skip=true clean package install assembly:assembly -U
 
-ln -s target/alibaba-rocketmq-3.0.4-SNAPSHOT.dir/alibaba-rocketmq devenv
+ln -s target/alibaba-rocketmq-3.0.4-open.dir/alibaba-rocketmq devenv
