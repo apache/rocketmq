a7f5d3479b5c6c5f913c25ab9737d6b0edb45ab6
vintagewang <vintage.wang@gmail.com>
2013-12-25 14:52:00 +0800

    更新Example，增加注释。

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/simple/Producer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/simple/Producer.java
@@ -21,33 +21,64 @@ import com.alibaba.rocketmq.client.producer.SendResult;
 import com.alibaba.rocketmq.common.message.Message;
 
 
-/**
- * Producer，发送消息
- */
 public class Producer {
     public static void main(String[] args) throws MQClientException, InterruptedException {
-        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        /**
+         * 一个应用创建一个Producer，由应用来维护此对象，可以设置为全局对象或者单例<br>
+         * 注意：ProducerGroupName需要由应用来保证唯一<br>
+         * ProducerGroup这个概念发送普通的消息时，作用不大，但是发送分布式事务消息时，比较关键，
+         * 因为服务器会回查这个Group下的任意一个Producer
+         */
+        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
 
+        /**
+         * Producer对象在使用之前必须要调用start初始化，初始化一次即可<br>
+         * 注意：切记不可以在每次发送消息时，都调用start方法
+         */
         producer.start();
 
-        String[] tags = new String[] { "TagA", "TagB", "TagC", "TagD", "TagE" };
-
-        for (int i = 0; i < 1000; i++) {
-            try {
-                Message msg = new Message("TopicTest",// topic
-                    tags[i % tags.length],// tag
-                    "KEY" + i,// key
-                    ("Hello RocketMQ " + i).getBytes()// body
-                        );
+        /**
+         * 下面这段代码表明一个Producer对象可以发送多个topic，多个tag的消息。
+         * 注意：send方法是同步调用，只要不抛异常就标识成功。但是发送成功也可会有多种状态，<br>
+         * 例如消息写入Master成功，但是Slave不成功，这种情况消息属于成功，但是对于个别应用如果对消息可靠性要求极高，<br>
+         * 需要对这种情况做处理。另外，消息可能会存在发送失败的情况，失败重试由应用来处理。
+         */
+        try {
+            {
+                Message msg = new Message("TopicTest1",// topic
+                    "TagA",// tag
+                    "OrderID001",// key
+                    ("Hello MetaQ").getBytes());// body
                 SendResult sendResult = producer.send(msg);
                 System.out.println(sendResult);
             }
-            catch (Exception e) {
-                e.printStackTrace();
-                Thread.sleep(1000);
+
+            {
+                Message msg = new Message("TopicTest2",// topic
+                    "TagB",// tag
+                    "OrderID0034",// key
+                    ("Hello MetaQ").getBytes());// body
+                SendResult sendResult = producer.send(msg);
+                System.out.println(sendResult);
+            }
+
+            {
+                Message msg = new Message("TopicTest3",// topic
+                    "TagC",// tag
+                    "OrderID061",// key
+                    ("Hello MetaQ").getBytes());// body
+                SendResult sendResult = producer.send(msg);
+                System.out.println(sendResult);
             }
         }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        /**
+         * 应用退出时，要调用shutdown来清理资源，关闭网络连接，从MetaQ服务器上注销自己
+         * 注意：我们建议应用在JBOSS、Tomcat等容器的退出钩子里调用shutdown方法
+         */
         producer.shutdown();
     }
 }
--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/simple/PushConsumer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/simple/PushConsumer.java
@@ -25,35 +25,63 @@ import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
 
-/**
- * PushConsumer，订阅消息
- */
 public class PushConsumer {
 
+    /**
+     * 当前例子是PushConsumer用法，使用方式给用户感觉是消息从MetaQ服务器推到了应用客户端。<br>
+     * 但是实际PushConsumer内部是使用长轮询Pull方式从MetaQ服务器拉消息，然后再回调用户Listener方法<br>
+     */
     public static void main(String[] args) throws InterruptedException, MQClientException {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_6");
-
         /**
-         * 订阅指定topic下所有消息
+         * 一个应用创建一个Consumer，由应用来维护此对象，可以设置为全局对象或者单例<br>
+         * 注意：ConsumerGroupName需要由应用来保证唯一
          */
-        consumer.subscribe("TopicTest1", "*");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ConsumerGroupName");
 
         /**
          * 订阅指定topic下tags分别等于TagA或TagC或TagD
          */
-        consumer.subscribe("TopicTest2", "TagA || TagC || TagD");
+        consumer.subscribe("TopicTest1", "TagA || TagC || TagD");
+        /**
+         * 订阅指定topic下所有消息<br>
+         * 注意：一个consumer对象可以订阅多个topic
+         */
+        consumer.subscribe("TopicTest2", "*");
 
         consumer.registerMessageListener(new MessageListenerConcurrently() {
 
+            /**
+             * 默认msgs里只有一条消息，可以通过设置consumeMessageBatchMaxSize参数来批量接收消息
+             */
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
                     ConsumeConcurrentlyContext context) {
                 System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
 
+                MessageExt msg = msgs.get(0);
+                if (msg.getTopic().equals("TopicTest1")) {
+                    // 执行TopicTest1的消费逻辑
+                    if (msg.getTags() != null && msg.getTags().equals("TagA")) {
+                        // 执行TagA的消费
+                    }
+                    else if (msg.getTags() != null && msg.getTags().equals("TagC")) {
+                        // 执行TagC的消费
+                    }
+                    else if (msg.getTags() != null && msg.getTags().equals("TagD")) {
+                        // 执行TagD的消费
+                    }
+                }
+                else if (msg.getTopic().equals("TopicTest2")) {
+                    // 执行TopicTest2的消费逻辑
+                }
+
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
 
+        /**
+         * Consumer对象在使用之前必须要调用start初始化，初始化一次即可<br>
+         */
         consumer.start();
 
         System.out.println("Consumer Started.");
