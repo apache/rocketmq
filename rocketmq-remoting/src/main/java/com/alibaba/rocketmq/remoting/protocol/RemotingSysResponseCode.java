9ca2f430c4a6970c9cc9798f921521325a0ca67c
shijia.wxr <shijia.wxr@taobao.com>
2014-03-21 15:44:39 +0800

    #372 删除无用的类

---
+++ rocketmq-remoting/src/main/java/com/alibaba/rocketmq/remoting/protocol/RemotingSysResponseCode.java
@@ -0,0 +1,12 @@
+package com.alibaba.rocketmq.remoting.protocol;
+
+public class RemotingSysResponseCode {
+    // 成功
+    public static final int SUCCESS = 0;
+    // 发生了未捕获异常
+    public static final int SYSTEM_ERROR = 1;
+    // 由于线程池拥堵，系统繁忙
+    public static final int SYSTEM_BUSY = 2;
+    // 请求代码不支持
+    public static final int REQUEST_CODE_NOT_SUPPORTED = 3;
+}
