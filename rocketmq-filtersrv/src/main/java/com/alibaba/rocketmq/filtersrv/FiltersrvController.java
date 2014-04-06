a88019555a1326354f747fbe66d26020001eb67a
shijia.wxr <shijia.wxr@taobao.com>
2014-04-06 10:37:02 +0800

    增加Filter Server工程

---
+++ rocketmq-filtersrv/src/main/java/com/alibaba/rocketmq/filtersrv/FiltersrvController.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (C) 2010-2013 Alibaba Group Holding Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.rocketmq.filtersrv;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.constant.LoggerName;
+import com.alibaba.rocketmq.filtersrv.processor.DefaultRequestProcessor;
+import com.alibaba.rocketmq.remoting.RemotingServer;
+import com.alibaba.rocketmq.remoting.netty.NettyRemotingServer;
+import com.alibaba.rocketmq.remoting.netty.NettyServerConfig;
+
+
+/**
+ * Name Server服务控制
+ * 
+ * @author shijia.wxr<vintage.wang@gmail.com>
+ * @since 2013-7-5
+ */
+public class FiltersrvController {
+    private static final Logger log = LoggerFactory.getLogger(LoggerName.NamesrvLoggerName);
+    // Filter Server配置
+    private final FiltersrvConfig filtersrvConfig;
+    // 通信层配置
+    private final NettyServerConfig nettyServerConfig;
+    // 服务端通信层对象
+    private RemotingServer remotingServer;
+    // 服务端网络请求处理线程池
+    private ExecutorService remotingExecutor;
+
+    // 定时线程
+    private final ScheduledExecutorService scheduledExecutorService = Executors
+        .newSingleThreadScheduledExecutor(new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "NamesrvControllerScheduledThread");
+            }
+        });
+
+
+    public FiltersrvController(FiltersrvConfig filtersrvConfig, NettyServerConfig nettyServerConfig) {
+        this.filtersrvConfig = filtersrvConfig;
+        this.nettyServerConfig = nettyServerConfig;
+    }
+
+
+    public boolean initialize() {
+        // 打印服务器配置参数
+        MixAll.printObjectProperties(log, this.filtersrvConfig);
+
+        // 初始化通信层
+        this.remotingServer = new NettyRemotingServer(this.nettyServerConfig);
+
+        // 初始化线程池
+        this.remotingExecutor =
+                Executors.newFixedThreadPool(nettyServerConfig.getServerWorkerThreads(), new ThreadFactory() {
+                    private AtomicInteger threadIndex = new AtomicInteger(0);
+
+
+                    @Override
+                    public Thread newThread(Runnable r) {
+                        return new Thread(r, "RemotingExecutorThread_" + threadIndex.incrementAndGet());
+                    }
+                });
+
+        this.registerProcessor();
+
+        this.scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+
+            @Override
+            public void run() {
+            }
+        }, 1000 * 10, 1000 * 120, TimeUnit.MILLISECONDS);
+
+        return true;
+    }
+
+
+    private void registerProcessor() {
+        this.remotingServer
+            .registerDefaultProcessor(new DefaultRequestProcessor(this), this.remotingExecutor);
+    }
+
+
+    public void start() throws Exception {
+        this.remotingServer.start();
+    }
+
+
+    public void shutdown() {
+        this.remotingServer.shutdown();
+        this.remotingExecutor.shutdown();
+        this.scheduledExecutorService.shutdown();
+    }
+
+
+    public RemotingServer getRemotingServer() {
+        return remotingServer;
+    }
+
+
+    public void setRemotingServer(RemotingServer remotingServer) {
+        this.remotingServer = remotingServer;
+    }
+
+
+    public ExecutorService getRemotingExecutor() {
+        return remotingExecutor;
+    }
+
+
+    public void setRemotingExecutor(ExecutorService remotingExecutor) {
+        this.remotingExecutor = remotingExecutor;
+    }
+
+
+    public FiltersrvConfig getFiltersrvConfig() {
+        return filtersrvConfig;
+    }
+
+
+    public NettyServerConfig getNettyServerConfig() {
+        return nettyServerConfig;
+    }
+
+
+    public ScheduledExecutorService getScheduledExecutorService() {
+        return scheduledExecutorService;
+    }
+}
---
+++ rocketmq-filtersrv/src/main/java/com/alibaba/rocketmq/filtersrv/FiltersrvStartup.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright (C) 2010-2013 Alibaba Group Holding Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.rocketmq.filtersrv;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+
+import com.alibaba.rocketmq.common.MQVersion;
+import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.constant.LoggerName;
+import com.alibaba.rocketmq.remoting.netty.NettyServerConfig;
+import com.alibaba.rocketmq.remoting.protocol.RemotingCommand;
+
+
+/**
+ * Name server 启动入口
+ * 
+ * @author shijia.wxr<vintage.wang@gmail.com>
+ * @since 2013-7-5
+ */
+public class FiltersrvStartup {
+
+    public static Options buildCommandlineOptions(final Options options) {
+        Option opt = new Option("c", "configFile", true, "Filter server config properties file");
+        opt.setRequired(false);
+        options.addOption(opt);
+
+        opt = new Option("p", "printConfigItem", false, "Print all config item");
+        opt.setRequired(false);
+        options.addOption(opt);
+
+        return options;
+    }
+
+
+    public static void main(String[] args) {
+        main0(args);
+    }
+
+
+    public static FiltersrvController main0(String[] args) {
+        System.setProperty(RemotingCommand.RemotingVersionKey, Integer.toString(MQVersion.CurrentVersion));
+
+        try {
+            // 解析命令行
+            Options options = MixAll.buildCommandlineOptions(new Options());
+            final CommandLine commandLine =
+                    MixAll.parseCmdLine("mqfiltersrv", args, buildCommandlineOptions(options),
+                        new PosixParser());
+            if (null == commandLine) {
+                System.exit(-1);
+                return null;
+            }
+
+            // 初始化配置文件
+            final FiltersrvConfig filtersrvConfig = new FiltersrvConfig();
+            final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+            nettyServerConfig.setListenPort(0);
+            nettyServerConfig.setServerAsyncSemaphoreValue(512);
+            nettyServerConfig.setServerCallbackExecutorThreads(64);
+            nettyServerConfig.setServerWorkerThreads(64);
+
+            if (commandLine.hasOption('c')) {
+                String file = commandLine.getOptionValue('c');
+                if (file != null) {
+                    InputStream in = new BufferedInputStream(new FileInputStream(file));
+                    Properties properties = new Properties();
+                    properties.load(in);
+                    MixAll.properties2Object(properties, filtersrvConfig);
+                    MixAll.properties2Object(properties, nettyServerConfig);
+                    System.out.println("load config properties file OK, " + file);
+                    in.close();
+                }
+            }
+
+            // 打印默认配置
+            if (commandLine.hasOption('p')) {
+                MixAll.printObjectProperties(null, filtersrvConfig);
+                MixAll.printObjectProperties(null, nettyServerConfig);
+                System.exit(0);
+            }
+
+            MixAll.properties2Object(MixAll.commandLine2Properties(commandLine), filtersrvConfig);
+
+            if (null == filtersrvConfig.getRocketmqHome()) {
+                System.out.println("Please set the " + MixAll.ROCKETMQ_HOME_ENV
+                        + " variable in your environment to match the location of the RocketMQ installation");
+                System.exit(-2);
+            }
+
+            // 初始化Logback
+            LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(lc);
+            lc.reset();
+            configurator.doConfigure(filtersrvConfig.getRocketmqHome() + "/conf/logback_namesrv.xml");
+            final Logger log = LoggerFactory.getLogger(LoggerName.NamesrvLoggerName);
+
+            // 初始化服务控制对象
+            final FiltersrvController controller =
+                    new FiltersrvController(filtersrvConfig, nettyServerConfig);
+            boolean initResult = controller.initialize();
+            if (!initResult) {
+                controller.shutdown();
+                System.exit(-3);
+            }
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                private volatile boolean hasShutdown = false;
+                private AtomicInteger shutdownTimes = new AtomicInteger(0);
+
+
+                @Override
+                public void run() {
+                    synchronized (this) {
+                        log.info("shutdown hook was invoked, " + this.shutdownTimes.incrementAndGet());
+                        if (!this.hasShutdown) {
+                            this.hasShutdown = true;
+                            long begineTime = System.currentTimeMillis();
+                            controller.shutdown();
+                            long consumingTimeTotal = System.currentTimeMillis() - begineTime;
+                            log.info("shutdown hook over, consuming time total(ms): " + consumingTimeTotal);
+                        }
+                    }
+                }
+            }, "ShutdownHook"));
+
+            // 启动服务
+            controller.start();
+
+            String tip = "The Filter Server boot success.";
+            log.info(tip);
+            System.out.println(tip);
+
+            return controller;
+        }
+        catch (Throwable e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        return null;
+    }
+}
