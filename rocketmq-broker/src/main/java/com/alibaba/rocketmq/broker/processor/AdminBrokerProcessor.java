04663fe8ea860a5285866988408774d4709d34eb
shijia.wxr <shijia.wxr@taobao.com>
2014-04-11 10:13:53 +0800

    fixed #386 优化Consumer第一次启动从哪里开始消费的问题。

--- rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/processor/AdminBrokerProcessor.java
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/processor/AdminBrokerProcessor.java
@@ -83,7 +83,6 @@ import com.alibaba.rocketmq.remoting.netty.NettyRequestProcessor;
 import com.alibaba.rocketmq.remoting.protocol.RemotingCommand;
 import com.alibaba.rocketmq.remoting.protocol.RemotingSerializable;
 import com.alibaba.rocketmq.store.DefaultMessageStore;
-import com.alibaba.rocketmq.store.StoreUtil;
 
 
 /**
@@ -775,10 +774,7 @@ public class AdminBrokerProcessor implements NettyRequestProcessor {
             long minOffset =
                     this.brokerController.getMessageStore().getMinOffsetInQuque(requestHeader.getTopic(),
                         requestHeader.getQueueId());
-            long maxOffset =
-                    this.brokerController.getMessageStore().getMaxOffsetInQuque(requestHeader.getTopic(),
-                        requestHeader.getQueueId());
-            // 订阅组不存在情况下，如果这个队列的消息最小Offset是0，则表示这个Topic上线时间不行，服务器堆积的数据也不多，那么这个订阅组就从0开始消费。
+            // 订阅组不存在情况下，如果这个队列的消息最小Offset是0，则表示这个Topic上线时间不长，服务器堆积的数据也不多，那么这个订阅组就从0开始消费。
             // 尤其对于Topic队列数动态扩容时，必须要从0开始消费。
             if (minOffset <= 0) {
                 responseHeader.setOffset(0L);
@@ -786,59 +782,9 @@ public class AdminBrokerProcessor implements NettyRequestProcessor {
                 response.setRemark(null);
             }
             // 新版本服务器不做消费进度纠正
-            else if (request.getVersion() >= MQVersion.Version.V3_0_6_SNAPSHOT.ordinal()) {
+            else {
                 response.setCode(ResponseCode.QUERY_NOT_FOUND);
                 response.setRemark("Not found, V3_0_6_SNAPSHOT maybe this group consumer boot first");
-            }
-
-            // TODO：以下流程待所有客户端都升级后，可以删除此段代码
-            else {
-                boolean consumeFromMinEnable = false;
-                if (0 == minOffset && maxOffset > 0) {
-                    long minCommitLogOffset =
-                            this.brokerController.getMessageStore().getCommitLogOffsetInQueue(
-                                requestHeader.getTopic(), requestHeader.getQueueId(), minOffset);
-                    long maxCommitLogOffset =
-                            this.brokerController.getMessageStore().getCommitLogOffsetInQueue(
-                                requestHeader.getTopic(), requestHeader.getQueueId(), maxOffset - 1);
-
-                    long memorySpan =
-                            (long) (StoreUtil.TotalPhysicalMemorySize * (this.brokerController
-                                .getMessageStoreConfig().getAccessMessageInMemoryMaxRatio() / 100.0));
-
-                    long diff = maxCommitLogOffset - minCommitLogOffset;
-                    if (diff < memorySpan) {
-                        consumeFromMinEnable = true;
-                        log.info(
-                            "the consumer group[{}] first subscribed, minOffset: {} maxOffset: {}, from min.",//
-                            requestHeader.getConsumerGroup(),//
-                            minOffset,//
-                            maxOffset);
-                    }
-                }
-                else if (minOffset > 0 && maxOffset > 0) {
-                    consumeFromMinEnable = false;
-                }
-                else {
-                    consumeFromMinEnable = true;
-                    log.info(
-                        "the consumer group[{}] first subscribed, minOffset: {} maxOffset: {}, from min, and unknow offset.",//
-                        requestHeader.getConsumerGroup(),//
-                        minOffset,//
-                        maxOffset);
-                }
-
-                // 说明这个队列在服务器存储的消息比较少或者没有消息
-                // 订阅组消费进度不存在情况下，从0开始消费
-                if (consumeFromMinEnable) {
-                    responseHeader.setOffset(0L);
-                    response.setCode(ResponseCode.SUCCESS);
-                    response.setRemark(null);
-                }
-                else {
-                    response.setCode(ResponseCode.QUERY_NOT_FOUND);
-                    response.setRemark("Not found, maybe this group consumer boot first");
-                }
             }
         }
 
