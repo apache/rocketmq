bfd96341ee21d3f1626019c50919f7bdbf91bcdb
shijia.wxr <shijia.wxr@taobao.com>
2014-04-20 18:44:33 +0800

    服务器增加动态编译代码

--- rocketmq-filtersrv/src/main/java/com/alibaba/rocketmq/filtersrv/filter/FilterClassManager.java
+++ rocketmq-filtersrv/src/main/java/com/alibaba/rocketmq/filtersrv/filter/FilterClassManager.java
@@ -2,24 +2,34 @@ package com.alibaba.rocketmq.filtersrv.filter;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.constant.LoggerName;
 import com.alibaba.rocketmq.common.filter.MessageFilter;
 
 
 public class FilterClassManager {
-    private ConcurrentHashMap<String/* className@consumerGroup, 必须含有全包名 */, FilterClassInfo> filterClassTable =
+    private static final Logger log = LoggerFactory.getLogger(LoggerName.NamesrvLoggerName);
+
+    private ConcurrentHashMap<String/* topic@consumerGroup */, FilterClassInfo> filterClassTable =
             new ConcurrentHashMap<String, FilterClassInfo>(128);
 
     private FilterClassLoader filterClassLoader = new FilterClassLoader();
 
+    // 只为编译加锁使用
+    private final Object compileLock = new Object();
 
-    private static String buildKey(final String consumerGroup, final String className) {
-        return className + "@" + consumerGroup;
+
+    private static String buildKey(final String consumerGroup, final String topic) {
+        return topic + "@" + consumerGroup;
     }
 
 
-    public void registerFilterClass(final String consumerGroup, final String className, final int classCRC,
-            final byte[] classBinary) throws InstantiationException, IllegalAccessException {
-        final String key = buildKey(consumerGroup, className);
+    public boolean registerFilterClass(final String consumerGroup, final String topic,
+            final String className, final int classCRC, final byte[] filterSourceBinary) {
+        final String key = buildKey(consumerGroup, topic);
 
         // 先检查是否存在，是否CRC相同
         boolean registerNew = false;
@@ -35,19 +45,34 @@ public class FilterClassManager {
 
         // 注册新的Class
         if (registerNew) {
-            Class<?> newClass =
-                    filterClassLoader.createNewClass(className, classBinary, 0, classBinary.length);
-            Object newInstance = newClass.newInstance();
-            FilterClassInfo filterClassInfoNew = new FilterClassInfo();
-            filterClassInfoNew.setClassName(className);
-            filterClassInfoNew.setClassCRC(classCRC);
-            filterClassInfoNew.setMessageFilter((MessageFilter) newInstance);
-            this.filterClassTable.put(key, filterClassInfoNew);
+            synchronized (this.compileLock) {
+                filterClassInfoPrev = this.filterClassTable.get(key);
+                if (null != filterClassInfoPrev && filterClassInfoPrev.getClassCRC() == classCRC) {
+                    return true;
+                }
+
+                try {
+                    String javaSource = new String(filterSourceBinary, MixAll.DEFAULT_CHARSET);
+                    Class<?> newClass = DynaCode.compileAndLoadClass(className, javaSource);
+                    Object newInstance = newClass.newInstance();
+                    FilterClassInfo filterClassInfoNew = new FilterClassInfo();
+                    filterClassInfoNew.setClassName(className);
+                    filterClassInfoNew.setClassCRC(classCRC);
+                    filterClassInfoNew.setMessageFilter((MessageFilter) newInstance);
+                    this.filterClassTable.put(key, filterClassInfoNew);
+                }
+                catch (Throwable e) {
+                    log.error("compileAndLoadClass Exception", e);
+                    return false;
+                }
+            }
         }
+
+        return true;
     }
 
 
-    public FilterClassInfo findFilterClass(final String consumerGroup, final String className) {
-        return this.filterClassTable.get(buildKey(consumerGroup, className));
+    public FilterClassInfo findFilterClass(final String consumerGroup, final String topic) {
+        return this.filterClassTable.get(buildKey(consumerGroup, topic));
     }
 }
