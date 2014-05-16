c8d445da27844576deba9edb8746fb2a399b1ef7
lansheng.zj <lansheng.zj@taobao.com>
2014-05-16 18:31:29 +0800

    定时消息偏移量无效修复 add comment

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/schedule/ScheduleMessageService.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/schedule/ScheduleMessageService.java
@@ -299,8 +299,11 @@ public class ScheduleMessageService extends ConfigManager {
                                         }
                                     }
                                     catch (Exception e) {
-                                        // FIXME
-                                        // 可能导致messageTimeup(msgExt);执行失败的定时消息被跳过
+                                        /*
+                                         * FIXME
+                                         * msgExt里面的内容不完整，如没有REAL_QID,REAL_TOPIC之类的
+                                         * ，导致数据无法正常的投递到正确的消费队列，所以暂时先直接跳过该条消息
+                                         */
                                         log.error("messageTimeup execute error. msgExt=" + msgExt
                                                 + ", nextOffset=" + nextOffset + ",offsetPy=" + offsetPy
                                                 + ",sizePy=" + sizePy, e);
@@ -329,8 +332,11 @@ public class ScheduleMessageService extends ConfigManager {
                     }
                 } // end of if (bufferCQ != null)
                 else {
-                    // 索引文件被删除offset没有纠正还指向一个老的位置
-                    long cqMinOffset = cq.getMinLogicOffset() / ConsumeQueue.CQStoreUnitSize;
+                    /*
+                     * 索引文件被删除，定时任务中记录的offset已经被删除，会导致从该位置中取不到数据，
+                     * 这里直接纠正下一次定时任务的offset为当前定时任务队列的最小值
+                     */
+                    long cqMinOffset = cq.getMinOffsetInQuque();
                     if (offset != cqMinOffset) {
                         failScheduleOffset = cqMinOffset;
                         log.error("schedule CQ offset invalid. offset=" + offset + ", cqMinOffset="
