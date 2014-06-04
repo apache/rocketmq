90a014ac0437390cb35581fd319166627dc70003
shijia.wxr <shijia.wxr@taobao.com>
2014-06-04 11:38:41 +0800

    删除无用的代码

--- rocketmq-filtersrv/src/main/java/com/alibaba/common/lang/i18n/provider/TraditionalToSimplifiedChineseProvider.java
+++
@@ -1,7 +0,0 @@
-package com.alibaba.common.lang.i18n.provider;
-
-public class TraditionalToSimplifiedChineseProvider extends ChineseCharConverterProvider {
-    protected String getCodeTableName() {
-        return "TraditionalToSimplifiedChinese";
-    }
-}
