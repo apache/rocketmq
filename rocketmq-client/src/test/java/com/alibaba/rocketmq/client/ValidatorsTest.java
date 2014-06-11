4acf57f1622c33f985dcd0c0c8ef1f0fd4ab039b
shijia.wxr <shijia.wxr@taobao.com>
2014-06-11 18:38:36 +0800

    Topic校验允许%通过。

---
+++ rocketmq-client/src/test/java/com/alibaba/rocketmq/client/ValidatorsTest.java
@@ -0,0 +1,23 @@
+package com.alibaba.rocketmq.client;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ValidatorsTest {
+
+    @Test
+    public void testTopic() {
+        try {
+            Validators.checkTopic("Hello");
+            Validators.checkTopic("%RETRY%Hello");
+            Validators.checkTopic("_%RETRY%Hello");
+            Validators.checkTopic("-%RETRY%Hello");
+            Validators.checkTopic("223-%RETRY%Hello");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+    }
+}
