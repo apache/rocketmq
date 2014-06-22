65ca708a4528ea1d27a6d8b5b47edc99e9835e31
shijia.wxr <shijia.wxr@taobao.com>
2014-06-22 19:17:14 +0800

    消息级别增加2个

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/config/MessageStoreConfig.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/config/MessageStoreConfig.java
@@ -125,7 +125,7 @@ public class MessageStoreConfig {
     // 同步刷盘超时时间
     private int syncFlushTimeout = 1000 * 5;
     // 定时消息相关
-    private String messageDelayLevel = "1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m";
+    private String messageDelayLevel = "1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h";
     private long flushDelayOffsetInterval = 1000 * 10;
     private int tranStateTableMapedFileSize = 2000000 * TransactionStateService.TSStoreUnitSize;
     private int tranRedoLogMapedFileSize = 2000000 * ConsumeQueue.CQStoreUnitSize;
