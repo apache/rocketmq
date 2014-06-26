32910b397d4a9988c45d60b9811d3eee39fe67f7
lansheng.zj <lansheng.zj@taobao.com>
2014-06-26 15:09:38 +0800

    文件校验工具

---
+++ rocketmq-tools/src/main/java/com/alibaba/rocketmq/tools/msgcheck/CheckMsg.java
@@ -0,0 +1,22 @@
+package com.alibaba.rocketmq.tools.msgcheck;
+
+/**
+ * @auther lansheng.zj
+ */
+public class CheckMsg {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.out
+                .println("need param:CommitLogStorePath CommitLogMapedFileSize ConsumeQueueStorePathParent ConsumeQueueMapedFileSize");
+            System.exit(-1);
+        }
+
+        Store store = new Store(args[0], Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]));
+        store.traval();
+    }
+
+}
