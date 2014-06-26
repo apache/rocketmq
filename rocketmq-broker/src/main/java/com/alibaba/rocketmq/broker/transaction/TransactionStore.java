786c0f9446c31a60b8ddaddf41beb59c7aaa8316
shijia.wxr <shijia.wxr@taobao.com>
2014-06-26 16:30:42 +0800

    重构事务存储模块

--- rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/transaction/TransactionStore.java
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/transaction/TransactionStore.java
@@ -1,5 +1,32 @@
 package com.alibaba.rocketmq.broker.transaction;
 
+import java.util.List;
+
+
+/**
+ * 事务存储接口，主要为分布式事务消息存储服务
+ */
 public interface TransactionStore {
-    public void load();
+    public void open();
+
+
+    public void close();
+
+
+    public boolean write(final TransactionRecord tr);
+
+
+    public void remove(final long pk);
+
+
+    public void remove(final List<Long> pks);
+
+
+    public List<TransactionRecord> traverse(final long pk, final int nums);
+
+
+    public long totalRecords();
+
+
+    public long maxOffset();
 }
