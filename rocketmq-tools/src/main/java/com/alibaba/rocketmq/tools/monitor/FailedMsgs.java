365d3e31a0a6226967b2757ed8c28c1f329195d2
shijia.wxr <shijia.wxr@taobao.com>
2014-07-05 22:48:49 +0800

    开发报警模块。

---
+++ rocketmq-tools/src/main/java/com/alibaba/rocketmq/tools/monitor/FailedMsgs.java
@@ -0,0 +1,37 @@
+package com.alibaba.rocketmq.tools.monitor;
+
+public class FailedMsgs {
+    private String consumerGroup;
+    private String topic;
+    private long failedMsgsTotalRecently;
+
+
+    public String getConsumerGroup() {
+        return consumerGroup;
+    }
+
+
+    public void setConsumerGroup(String consumerGroup) {
+        this.consumerGroup = consumerGroup;
+    }
+
+
+    public String getTopic() {
+        return topic;
+    }
+
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+
+    public long getFailedMsgsTotalRecently() {
+        return failedMsgsTotalRecently;
+    }
+
+
+    public void setFailedMsgsTotalRecently(long failedMsgsTotalRecently) {
+        this.failedMsgsTotalRecently = failedMsgsTotalRecently;
+    }
+}
