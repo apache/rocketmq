3c374e2b9297a8212b3f60ff8a4beb23f91d97cb
shijia.wxr <shijia.wxr@taobao.com>
2015-03-28 20:10:27 +0800

    准备将326版本发布到github

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/MQVersion.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/MQVersion.java
@@ -22,7 +22,7 @@ package com.alibaba.rocketmq.common;
  */
 public class MQVersion {
     // TODO 每次发布版本都要修改此处版本号
-    public static final int CurrentVersion = Version.V3_2_6_SNAPSHOT.ordinal();
+    public static final int CurrentVersion = Version.V3_2_6.ordinal();
 
 
     public static String getVersionDesc(int value) {
