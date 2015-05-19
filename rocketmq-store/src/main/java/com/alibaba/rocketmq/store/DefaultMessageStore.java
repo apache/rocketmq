d8d64e2c7cdfce602df2f8368d2913ca438d5fb5
manhong.yqd <manhong.yqd@alibaba-inc.com>
2015-05-19 19:36:53 +0800

    1. 备机位点纠正修复

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/DefaultMessageStore.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/DefaultMessageStore.java
@@ -443,6 +443,17 @@ public class DefaultMessageStore implements MessageStore {
     }
 
 
+    private long nextOffsetCorrection(long oldOffset, long newOffset) {
+        long nextOffset = oldOffset;
+        // 备机默认不纠正 pull offset
+        if (this.getMessageStoreConfig().getBrokerRole() != BrokerRole.SLAVE
+                || this.getMessageStoreConfig().isOffsetCheckInSlave()) {
+            nextOffset = newOffset;
+        }
+        return nextOffset;
+    }
+
+
     public GetMessageResult getMessage(final String group, final String topic, final int queueId,
             final long offset, final int maxMsgNums, final SubscriptionData subscriptionData) {
         if (this.shutdown) {
@@ -479,23 +490,23 @@ public class DefaultMessageStore implements MessageStore {
 
             if (maxOffset == 0) {
                 status = GetMessageStatus.NO_MESSAGE_IN_QUEUE;
-                nextBeginOffset = 0;
+                nextBeginOffset = nextOffsetCorrection(offset, 0);
             }
             else if (offset < minOffset) {
                 status = GetMessageStatus.OFFSET_TOO_SMALL;
-                nextBeginOffset = minOffset;
+                nextBeginOffset = nextOffsetCorrection(offset, minOffset);
             }
             else if (offset == maxOffset) {
                 status = GetMessageStatus.OFFSET_OVERFLOW_ONE;
-                nextBeginOffset = offset;
+                nextBeginOffset = nextOffsetCorrection(offset, offset);
             }
             else if (offset > maxOffset) {
                 status = GetMessageStatus.OFFSET_OVERFLOW_BADLY;
                 if (0 == minOffset) {
-                    nextBeginOffset = minOffset;
+                    nextBeginOffset = nextOffsetCorrection(offset, minOffset);
                 }
                 else {
-                    nextBeginOffset = maxOffset;
+                    nextBeginOffset = nextOffsetCorrection(offset, maxOffset);
                 }
             }
             else {
@@ -588,7 +599,7 @@ public class DefaultMessageStore implements MessageStore {
                 }
                 else {
                     status = GetMessageStatus.OFFSET_FOUND_NULL;
-                    nextBeginOffset = consumeQueue.rollNextFile(offset);
+                    nextBeginOffset = nextOffsetCorrection(offset, consumeQueue.rollNextFile(offset));
                     log.warn("consumer request topic: " + topic + "offset: " + offset + " minOffset: "
                             + minOffset + " maxOffset: " + maxOffset + ", but access logic queue failed.");
                 }
@@ -597,7 +608,7 @@ public class DefaultMessageStore implements MessageStore {
         // 请求的队列Id没有
         else {
             status = GetMessageStatus.NO_MATCHED_LOGIC_QUEUE;
-            nextBeginOffset = 0;
+            nextBeginOffset = nextOffsetCorrection(offset, 0);
         }
 
         if (GetMessageStatus.FOUND == status) {
@@ -610,15 +621,7 @@ public class DefaultMessageStore implements MessageStore {
         this.storeStatsService.setGetMessageEntireTimeMax(eclipseTime);
 
         getResult.setStatus(status);
-
-        // 备机默认不纠正 pull offset
-        if (this.getMessageStoreConfig().getBrokerRole() != BrokerRole.SLAVE
-                || this.getMessageStoreConfig().isOffsetCheckInSlave()) {
-            getResult.setNextBeginOffset(nextBeginOffset);
-        }
-        else {
-            getResult.setNextBeginOffset(offset);
-        }
+        getResult.setNextBeginOffset(nextBeginOffset);
         getResult.setMaxOffset(maxOffset);
         getResult.setMinOffset(minOffset);
         return getResult;
