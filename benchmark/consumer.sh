899dc968618e5e542ba3af55eeb6dbb4ad18d5a9
manhong.yqd <manhong.yqd@alibaba-inc.com>
2015-05-21 16:26:53 +0800

    修改脚本

--- benchmark/consumer.sh
+++ benchmark/consumer.sh
@@ -3,4 +3,4 @@
 #
 # $Id: consumer.sh 1831 2013-05-16 01:39:51Z shijia.wxr $
 #
-sh ./runclass.sh com.alibaba.rocketmq.example.benchmark.Consumer $@
+sh ./runclass.sh com.alibaba.rocketmq.example.benchmark.Consumer $@ &
