1d2ce2e7ce4279e3ff012bd403b4a6b0845b58be
manhong.yqd <manhong.yqd@alibaba-inc.com>
2016-03-11 06:22:51 +0800

    bug fix

--- rocketmq-broker/src/test/java/com/alibaba/rocketmq/broker/plugin/MockMessageStore.java
+++ rocketmq-broker/src/test/java/com/alibaba/rocketmq/broker/plugin/MockMessageStore.java
@@ -10,15 +10,11 @@ import java.util.Set;
 
 import com.alibaba.rocketmq.common.message.MessageExt;
 import com.alibaba.rocketmq.common.protocol.heartbeat.SubscriptionData;
-import com.alibaba.rocketmq.store.GetMessageResult;
-import com.alibaba.rocketmq.store.MessageExtBrokerInner;
-import com.alibaba.rocketmq.store.MessageStore;
-import com.alibaba.rocketmq.store.PutMessageResult;
-import com.alibaba.rocketmq.store.QueryMessageResult;
-import com.alibaba.rocketmq.store.SelectMapedBufferResult;
+import com.alibaba.rocketmq.store.*;
+
 
 /**
- * @author qinan.qn@taobao.com 2015Äê12ÔÂ12ÈÕ
+ * @author qinan.qn@taobao.com 2015ï¿½ï¿½12ï¿½ï¿½12ï¿½ï¿½
  */
 public class MockMessageStore implements MessageStore {
 
@@ -27,20 +23,24 @@ public class MockMessageStore implements MessageStore {
         return false;
     }
 
+
     @Override
     public void start() throws Exception {
 
     }
+
 
     @Override
     public void shutdown() {
 
     }
 
+
     @Override
     public void destroy() {
 
     }
+
 
     @Override
     public PutMessageResult putMessage(MessageExtBrokerInner msg) {
@@ -48,12 +48,14 @@ public class MockMessageStore implements MessageStore {
         return null;
     }
 
+
     @Override
-    public GetMessageResult getMessage(String group, String topic, int queueId, long offset,
-            int maxMsgNums, SubscriptionData subscriptionData) {
+    public GetMessageResult getMessage(String group, String topic, int queueId, long offset, int maxMsgNums,
+            SubscriptionData subscriptionData) {
 
         return null;
     }
+
 
     @Override
     public long getMaxOffsetInQuque(String topic, int queueId) {
@@ -61,11 +63,13 @@ public class MockMessageStore implements MessageStore {
         return 0;
     }
 
+
     @Override
     public long getMinOffsetInQuque(String topic, int queueId) {
 
         return 0;
     }
+
 
     @Override
     public long getCommitLogOffsetInQueue(String topic, int queueId, long cqOffset) {
@@ -73,11 +77,13 @@ public class MockMessageStore implements MessageStore {
         return 0;
     }
 
+
     @Override
     public long getOffsetInQueueByTime(String topic, int queueId, long timestamp) {
 
         return 0;
     }
+
 
     @Override
     public MessageExt lookMessageByOffset(long commitLogOffset) {
@@ -85,11 +91,13 @@ public class MockMessageStore implements MessageStore {
         return null;
     }
 
+
     @Override
     public SelectMapedBufferResult selectOneMessageByOffset(long commitLogOffset) {
 
         return null;
     }
+
 
     @Override
     public SelectMapedBufferResult selectOneMessageByOffset(long commitLogOffset, int msgSize) {
@@ -97,11 +105,13 @@ public class MockMessageStore implements MessageStore {
         return null;
     }
 
+
     @Override
     public String getRunningDataInfo() {
 
         return null;
     }
+
 
     @Override
     public HashMap<String, String> getRuntimeInfo() {
@@ -109,11 +119,13 @@ public class MockMessageStore implements MessageStore {
         return null;
     }
 
+
     @Override
     public long getMaxPhyOffset() {
 
         return 0;
     }
+
 
     @Override
     public long getMinPhyOffset() {
@@ -121,11 +133,13 @@ public class MockMessageStore implements MessageStore {
         return 0;
     }
 
+
     @Override
     public long getEarliestMessageTime(String topic, int queueId) {
 
         return 0;
     }
+
 
     @Override
     public long getMessageStoreTimeStamp(String topic, int queueId, long offset) {
@@ -133,11 +147,13 @@ public class MockMessageStore implements MessageStore {
         return 0;
     }
 
+
     @Override
     public long getMessageTotalInQueue(String topic, int queueId) {
 
         return 0;
     }
+
 
     @Override
     public SelectMapedBufferResult getCommitLogData(long offset) {
@@ -145,28 +161,32 @@ public class MockMessageStore implements MessageStore {
         return null;
     }
 
+
     @Override
     public boolean appendToCommitLog(long startOffset, byte[] data) {
 
         return false;
     }
 
+
     @Override
     public void excuteDeleteFilesManualy() {
 
     }
 
+
     @Override
-    public QueryMessageResult queryMessage(String topic, String key, int maxNum, long begin,
-            long end) {
+    public QueryMessageResult queryMessage(String topic, String key, int maxNum, long begin, long end) {
 
         return null;
     }
+
 
     @Override
     public void updateHaMasterAddress(String newAddr) {
 
     }
+
 
     @Override
     public long slaveFallBehindMuch() {
@@ -174,11 +194,13 @@ public class MockMessageStore implements MessageStore {
         return 0;
     }
 
+
     @Override
     public long now() {
 
         return 0;
     }
+
 
     @Override
     public int cleanUnusedTopic(Set<String> topics) {
@@ -186,17 +208,19 @@ public class MockMessageStore implements MessageStore {
         return 0;
     }
 
+
     @Override
     public void cleanExpiredConsumerQueue() {
 
     }
 
+
     @Override
-    public Map<String, Long> getMessageIds(String topic, int queueId, long minOffset,
-            long maxOffset, SocketAddress storeHost) {
+    public Map<String, Long> getMessageIds(String topic, int queueId, long minOffset, long maxOffset, SocketAddress storeHost) {
 
         return null;
     }
+
 
     @Override
     public boolean checkInDiskByConsumeOffset(String topic, int queueId, long consumeOffset) {
@@ -204,25 +228,30 @@ public class MockMessageStore implements MessageStore {
         return false;
     }
 
+
     @Override
     public long dispatchBehindBytes() {
 
         return 0;
     }
 
+
     @Override
     public long flush() {
         return 0;
     }
 
+
     @Override
-    public void resetWriteOffset(long phyOffset) {
-        
+    public boolean resetWriteOffset(long phyOffset) {
+        return true;
     }
+
 
     @Override
     public void setConfirmOffset(long phyOffset) {
     }
+
 
     @Override
     public long getConfirmOffset() {
