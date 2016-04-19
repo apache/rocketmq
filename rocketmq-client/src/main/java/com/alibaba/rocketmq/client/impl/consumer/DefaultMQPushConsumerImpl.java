c2ac164874c4d43bdc1b1b71187bc40e2155ec72
manhong.yqd <manhong.yqd@alibaba-inc.com>
2016-04-19 20:42:16 +0800

    Issue #50 消费位点重置，导致消息大量重复

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/consumer/DefaultMQPushConsumerImpl.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/consumer/DefaultMQPushConsumerImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2010-2013 Alibaba Group Holding Limited
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -466,7 +466,7 @@ public class DefaultMQPushConsumerImpl implements MQConsumerInner {
     public MessageExt queryMessageByUniqKey(String topic, String uniqKey) throws MQClientException,
             InterruptedException {
         return this.mQClientFactory.getMQAdminImpl().queryMessageByUniqKey(topic, uniqKey);
-     }
+    }
 
 
     public void registerMessageListener(MessageListener messageListener) {
@@ -475,6 +475,8 @@ public class DefaultMQPushConsumerImpl implements MQConsumerInner {
 
     public void resume() {
         this.pause = false;
+        // consumer 恢复之后,重新进行rebalance
+        doRebalance();
         log.info("resume this consumer, {}", this.defaultMQPushConsumer.getConsumerGroup());
     }
 
@@ -911,7 +913,7 @@ public class DefaultMQPushConsumerImpl implements MQConsumerInner {
 
     @Override
     public void doRebalance() {
-        if (this.rebalanceImpl != null) {
+        if (this.rebalanceImpl != null && !this.pause) {
             this.rebalanceImpl.doRebalance();
         }
     }
