8942e7dee4c8f598c17cd5e6790db33ed12b65c2
shijia.wxr <shijia.wxr@taobao.com>
2016-06-13 15:55:12 +0800

    Issue #67 com.rocketmq.remoting.frameMaxLength 由8M改为16M

--- rocketmq-remoting/src/main/java/com/alibaba/rocketmq/remoting/netty/NettyDecoder.java
+++ rocketmq-remoting/src/main/java/com/alibaba/rocketmq/remoting/netty/NettyDecoder.java
@@ -34,7 +34,7 @@ import java.nio.ByteBuffer;
 public class NettyDecoder extends LengthFieldBasedFrameDecoder {
     private static final Logger log = LoggerFactory.getLogger(RemotingHelper.RemotingLogName);
     private static final int FRAME_MAX_LENGTH = //
-            Integer.parseInt(System.getProperty("com.rocketmq.remoting.frameMaxLength", "8388608"));
+            Integer.parseInt(System.getProperty("com.rocketmq.remoting.frameMaxLength", "16777216"));
 
 
     public NettyDecoder() {
