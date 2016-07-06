0797a2d05162b04bb8432b798665eb2c5ef55f47
manhong.yqd <manhong.yqd@alibaba-inc.com>
2016-07-06 15:47:27 +0800

    Issue #78 顺序消息pullRequest死锁问题

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/consumer/RebalanceImpl.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/impl/consumer/RebalanceImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2010-2013 Alibaba Group Holding Limited
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -365,18 +365,22 @@ public abstract class RebalanceImpl {
         for (MessageQueue mq : mqSet) {
             if (!this.processQueueTable.containsKey(mq)) {
                 this.removeDirtyOffset(mq);
-                PullRequest pullRequest = new PullRequest();
-                pullRequest.setConsumerGroup(consumerGroup);
-                pullRequest.setMessageQueue(mq);
-                pullRequest.setProcessQueue(new ProcessQueue());
-
+                ProcessQueue pq = new ProcessQueue();
                 long nextOffset = this.computePullFromWhere(mq);
                 if (nextOffset >= 0) {
-                    pullRequest.setNextOffset(nextOffset);
-                    pullRequestList.add(pullRequest);
-                    changed = true;
-                    this.processQueueTable.put(mq, pullRequest.getProcessQueue());
-                    log.info("doRebalance, {}, add a new mq, {}", consumerGroup, mq);
+                    ProcessQueue pre = this.processQueueTable.putIfAbsent(mq, pq);
+                    if (pre != null) {
+                        log.info("doRebalance, {}, mq already exists, {}", consumerGroup, mq);
+                    } else {
+                        log.info("doRebalance, {}, add a new mq, {}", consumerGroup, mq);
+                        PullRequest pullRequest = new PullRequest();
+                        pullRequest.setConsumerGroup(consumerGroup);
+                        pullRequest.setNextOffset(nextOffset);
+                        pullRequest.setMessageQueue(mq);
+                        pullRequest.setProcessQueue(pq);
+                        pullRequestList.add(pullRequest);
+                        changed = true;
+                    }
                 } else {
                     log.warn("doRebalance, {}, add new mq failed, {}", consumerGroup, mq);
                 }
