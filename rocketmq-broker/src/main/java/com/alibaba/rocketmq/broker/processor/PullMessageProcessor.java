97985ccc68602bb5452d63bec50117dac06c04b2
manhong.yqd <manhong.yqd@alibaba-inc.com>
2016-07-13 11:51:09 +0800

    Issue #73 长轮询中去掉remotingcommand的引用

--- rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/processor/PullMessageProcessor.java
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/processor/PullMessageProcessor.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2010-2013 Alibaba Group Holding Limited
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -358,10 +358,12 @@ public class PullMessageProcessor implements NettyRequestProcessor {
                             pollingTimeMills = this.brokerController.getBrokerConfig().getShortPollingTimeMills();
                         }
 
+                        String topic = new String(requestHeader.getTopic());
+                        long offset = requestHeader.getQueueOffset();
+                        int queueId = requestHeader.getQueueId();
                         PullRequest pullRequest = new PullRequest(request, channel, pollingTimeMills,
-                                this.brokerController.getMessageStore().now(), requestHeader.getQueueOffset(), subscriptionData);
-                        this.brokerController.getPullRequestHoldService().suspendPullRequest(requestHeader.getTopic(),
-                                requestHeader.getQueueId(), pullRequest);
+                                this.brokerController.getMessageStore().now(), offset, subscriptionData);
+                        this.brokerController.getPullRequestHoldService().suspendPullRequest(topic, queueId, pullRequest);
                         response = null;
                         break;
                     }
