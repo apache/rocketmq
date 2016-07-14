bc17c8d50c9e0c1d97b4f8041a1a0ee3bcf695da
xinyuzhou.zxy <xinyuzhou.zxy@alibaba-inc.com>
2016-07-14 10:36:34 +0800

    Update copyright of the whole project

--- rocketmq-filtersrv/src/main/java/com/alibaba/rocketmq/filtersrv/filter/DynaCode.java
+++ rocketmq-filtersrv/src/main/java/com/alibaba/rocketmq/filtersrv/filter/DynaCode.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.alibaba.rocketmq.filtersrv.filter;
 
 import com.alibaba.common.lang.ArrayUtil;
@@ -18,18 +35,6 @@ import java.net.URLDecoder;
 import java.util.*;
 
 
-/**
- * <p/>
- * description: java动态编译类，创建该类时需要给定编译的java代码的text的code字符串
- * 再调用compileAndLoadClass方法进行编译，调用compileAndLoadClass之前可以通过相关的 SET方法来设置编译的一些参数
- * <p/>
- *
- * @author <a href="mailto:qihao@taobao.com">qihao</a>
- * @version 1.0
- * @{# DynaCode.java Create on Sep 22, 2011 11:34:10 AM
- * <p/>
- * Copyright (c) 2011 by qihao.
- */
 public class DynaCode {
     private static final Logger logger = LoggerFactory.getLogger(LoggerName.FiltersrvLoggerName);
 
