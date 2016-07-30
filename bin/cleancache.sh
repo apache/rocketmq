34f2dec948e268965b7acc2bb8773a9d65b326b2
shijia.wxr <shijia.wxr@taobao.com>
2016-07-30 13:36:53 +0800

    Issue #104 add clean cache shell file

---
+++ bin/cleancache.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+#
+# Execute Only Once
+#
+
+export PATH=$PATH:/sbin
+
+#
+# GB
+#
+function changeFreeCache()
+{
+    EXTRA=$1
+    MIN=$2
+    sysctl -w vm.extra_free_kbytes=${EXTRA}000000
+    sysctl -w vm.min_free_kbytes=${MIN}000000
+    sysctl -p
+}
+
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 extra_free_kbytes min_free_kbytes"
+    echo "Example: $0 3 1"
+    echo "Example: $0 3 10"
+    exit
+fi
+
+changeFreeCache $1 $2
+changeFreeCache $1 $2
