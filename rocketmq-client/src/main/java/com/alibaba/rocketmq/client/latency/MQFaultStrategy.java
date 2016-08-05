0a57b4a90526cd068f6c9a2f3c81b9d71c791ca0
shijia.wxr <shijia.wxr@taobao.com>
2016-08-05 17:44:12 +0800

    Issue #98 sendLatencyFaultEnable default false

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/latency/MQFaultStrategy.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/latency/MQFaultStrategy.java
@@ -26,7 +26,7 @@ import com.alibaba.rocketmq.common.message.MessageQueue;
 public class MQFaultStrategy {
     private final LatencyFaultTolerance<String> latencyFaultTolerance = new LatencyFaultToleranceImpl();
 
-    private boolean sendLatencyFaultEnable = true;
+    private boolean sendLatencyFaultEnable = false;
 
     private long[] latencyMax =             {50L,   100L,   550L,       1000L,  2000L,      3000L,      15000L};
     private long[] notAvailableDuration =   {0L,    0L,     30000L,     60000L, 120000L,    180000L,    600000L};
