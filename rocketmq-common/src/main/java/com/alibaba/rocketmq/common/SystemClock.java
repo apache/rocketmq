4649a4582f3f1e6a07e58d77fae284d8dda86318
xinyuzhou.zxy <xinyuzhou.zxy@alibaba-inc.com>
2016-08-31 19:21:51 +0800

    Issue #113 use system clock in SystemClock

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/SystemClock.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/SystemClock.java
@@ -16,53 +16,11 @@
  */
 package com.alibaba.rocketmq.common;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-
-
 /**
  * @author vintage.wang
  */
 public class SystemClock {
-
-    private final long precision;
-    private final AtomicLong now;
-
-
-    public SystemClock(long precision) {
-        this.precision = precision;
-        now = new AtomicLong(System.currentTimeMillis());
-        scheduleClockUpdating();
-    }
-
-
-    private void scheduleClockUpdating() {
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable runnable) {
-                Thread thread = new Thread(runnable, "System Clock");
-                thread.setDaemon(true);
-                return thread;
-            }
-        });
-        scheduler.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                now.set(System.currentTimeMillis());
-            }
-        }, precision, precision, TimeUnit.MILLISECONDS);
-    }
-
-
     public long now() {
-        return now.get();
-    }
-
-
-    public long precision() {
-        return precision;
+        return System.currentTimeMillis();
     }
 }
