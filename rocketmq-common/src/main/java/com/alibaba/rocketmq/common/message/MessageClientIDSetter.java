9158d3e637223332c71a92df2ebf582e2af3ad0c
manhong.yqd <manhong.yqd@alibaba-inc.com>
2016-10-28 10:55:59 +0800

    Issue #129 唯一消息 id 的产生出现大量的 block

--- rocketmq-common/src/main/java/com/alibaba/rocketmq/common/message/MessageClientIDSetter.java
+++ rocketmq-common/src/main/java/com/alibaba/rocketmq/common/message/MessageClientIDSetter.java
@@ -21,35 +21,19 @@ import com.alibaba.rocketmq.common.UtilAll;
 import java.nio.ByteBuffer;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MessageClientIDSetter {
     private static final String TOPIC_KEY_SPLITTER = "#";
-    // uniqID的固定长度
     private static final int len;
-    //本机ip, 进程id，classloader标识
     private static final String fixString;
     private static final AtomicInteger counter;
-
     private static long startTime;
-
     private static long nextStartTime;
-
     static {
-        //算法是这样的 ip 4 byte + pid 2 byte + classloader 4 byte + timestamp 4 byte + counter 2 byte
-        //其中，前3个4byte提前预置，
-        //timestamp 4个byte，存储当前时间到每个月1号0点0分0秒的毫秒差值
-        //counter 2 byte，可以计算到65535, 足够豪秒级别的自增了
-        //如果是一个进程多个容器，classloader可以区分
-        //如果是c++等语言调用，那么毫秒级别不可能启动关闭多个jvm，并有同样的pid
         len = 4 + 2 + 4 + 4 + 2;
-
-        //分配空间
         ByteBuffer tempBuffer = ByteBuffer.allocate(10);
-        //本机ip, 进程id，classloader标识
         tempBuffer.position(2);
         tempBuffer.putInt(UtilAll.getPid());//2
         tempBuffer.position(0);
@@ -61,22 +45,20 @@ public class MessageClientIDSetter {
         tempBuffer.position(6);
         tempBuffer.putInt(MessageClientIDSetter.class.getClassLoader().hashCode()); //4
         fixString = UtilAll.bytes2string(tempBuffer.array());
-        //以每月1号作为基准时间点计算差值
         setStartTime(System.currentTimeMillis());
-        //计数器
         counter = new AtomicInteger(0);
     }
 
     private synchronized static void setStartTime(long millis) {
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(millis);
-        cal.set(Calendar.DAY_OF_MONTH, 1);//1号0点0分0秒
+        cal.set(Calendar.DAY_OF_MONTH, 1);
         cal.set(Calendar.HOUR, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
         startTime = cal.getTimeInMillis();
-        cal.add(Calendar.MONTH, 1);//下个月1号0点0分0秒
+        cal.add(Calendar.MONTH, 1);
         nextStartTime = cal.getTimeInMillis();
     }
 
@@ -90,10 +72,9 @@ public class MessageClientIDSetter {
         buf.put(bytes, 10, 4);
         buf.position(0);
         long spanMS = buf.getLong();
-
         Calendar cal = Calendar.getInstance();
         long now = cal.getTimeInMillis();
-        cal.set(Calendar.DAY_OF_MONTH, 1);//1号0点0分0秒
+        cal.set(Calendar.DAY_OF_MONTH, 1);
         cal.set(Calendar.HOUR, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
@@ -128,44 +109,24 @@ public class MessageClientIDSetter {
 
 
     private static byte[] createUniqIDBuffer() {
-        //ip  + pid + classloaderid + counter + time
-        //4 bytes for ip, 4 bytes for pid, 4 bytes for  classloaderid, 2 bytes for counter, 4 bytes for timediff
         ByteBuffer buffer = ByteBuffer.allocate(4 + 2);
-        //连接正常唯一id
         long current = System.currentTimeMillis();
         if (current >= nextStartTime) {
             setStartTime(current);
         }
         buffer.position(0);
-        buffer.putInt((int) (System.currentTimeMillis() - startTime));//ms级别 4 byte
-        buffer.putShort((short) counter.getAndIncrement()); //覆盖timestamp前2byte，每ms 1个short 65535，应该足够计数了
+        buffer.putInt((int) (System.currentTimeMillis() - startTime));
+        buffer.putShort((short) counter.getAndIncrement());
         return buffer.array();
     }
 
-    /**
-     * 这个方法会在property中存储uniqkey在keys中的index,一旦后续这个index改变，程序会出问题，
-     * 因此后续只能向keys后面加入key，不能往前面插入key
-     *
-     * @param msg
-     */
     public static void setUniqID(final Message msg) {
-        //如果专有属性不存在时set，此时创造一个唯一ID，并且放入key中，然后将idx记入专有属性
-        //如果专有属性存在则不做任何操作
         if (msg.getProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX) == null) {
             msg.putProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX, createUniqID());
         }
     }
 
-    /**
-     * 客户端用来获取UniqID用, 由于查询的时候key必须配合topic查询，因此在这里Topic也是必须的
-     * 需要用splitter分割，一并返回
-     *
-     * @param msg
-     * @return
-     */
     public static String getUniqID(final Message msg) {
-        //如果专有属性不存在，则返回null
-        //然后根据专有属性，取出idx, 在key中取出对应的key
         return msg.getProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX);
     }
 
