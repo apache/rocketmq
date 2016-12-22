78e881af7ba627a635b9f79c32ca881df5a59e68
shtykh_roman <rshtykh@yahoo.com>
2016-12-22 11:40:35 +0900

    JIRA Issue: https://issues.apache.org/jira/browse/ROCKETMQ-2
    Added ASF header.

--- rocketmq-broker/src/test/java/com/alibaba/rocketmq/broker/BrokerControllerTest.java
+++ rocketmq-broker/src/test/java/com/alibaba/rocketmq/broker/BrokerControllerTest.java
@@ -1,10 +1,26 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.alibaba.rocketmq.broker;
 
 import com.alibaba.rocketmq.common.BrokerConfig;
 import com.alibaba.rocketmq.remoting.netty.NettyClientConfig;
 import com.alibaba.rocketmq.remoting.netty.NettyServerConfig;
 import com.alibaba.rocketmq.store.config.MessageStoreConfig;
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -21,7 +37,7 @@ public class BrokerControllerTest {
     @Test
     public void testRestart() throws Exception {
 
-        for (int i=0; i<RESTART_NUM; i++) {
+        for (int i = 0; i < RESTART_NUM; i++) {
             BrokerController brokerController = new BrokerController(//
                 new BrokerConfig(), //
                 new NettyServerConfig(), //
