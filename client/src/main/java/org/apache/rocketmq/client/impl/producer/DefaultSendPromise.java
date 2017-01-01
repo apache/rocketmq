0ddbc4471a6ed1305bade5bb5eb58c8182066990
thisisbaozi <thisisbaozi@gmail.com>
2017-01-01 11:39:44 +0800

    [ROCKETMQ-26]add a new way to send message in asynchronous

--- client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultSendPromise.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultSendPromise.java
@@ -1,5 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.rocketmq.client.impl.producer;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.apache.rocketmq.client.log.ClientLogger;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
@@ -8,7 +30,6 @@ import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
 
 /**
  * Created by stone
@@ -17,11 +38,11 @@ public class DefaultSendPromise extends CountDownLatch implements SendPromise {
 
     private final Logger logger = ClientLogger.getLog();
 
-    private Executor executor;
+    private final Executor executor;
     private Object callbacks;
     private volatile Object result;
 
-    public DefaultSendPromise(Executor executor) {
+    public DefaultSendPromise(final Executor executor) {
         super(1);
 
         if (executor == null) {
