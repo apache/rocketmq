1562bd0d11fbf4bbbca38921c6cc07ebe11ea15a
shroman <rshtykh@yahoo.com>
2017-01-09 16:28:10 +0800

    [ROCKETMQ-30] Fixed method signature for Message Filter example and class loading from resources, closes apache/incubator-rocketmq#27

--- example/src/main/java/org/apache/rocketmq/example/filter/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/filter/Consumer.java
@@ -16,6 +16,7 @@
  */
 package org.apache.rocketmq.example.filter;
 
+import java.io.File;
 import java.util.List;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
@@ -30,8 +31,11 @@ public class Consumer {
     public static void main(String[] args) throws InterruptedException, MQClientException {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ConsumerGroupNamecc4");
 
-        String filterCode = MixAll.file2String("/home/admin/MessageFilterImpl.java");
-        consumer.subscribe("TopicFilter7", "org.apache.rocketmq.example.filter.MessageFilterImpl",
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        File classFile = new File(classLoader.getResource("MessageFilterImpl.java").getFile());
+
+        String filterCode = MixAll.file2String(classFile);
+        consumer.subscribe("TopicTest", "org.apache.rocketmq.example.filter.MessageFilterImpl",
             filterCode);
 
         consumer.registerMessageListener(new MessageListenerConcurrently() {
