11f4362eae26a558ee3cbe5a2db976bc6495f2c3
芋艿酱 <zhijiantianya@gmail.com>
2017-03-26 15:24:36 +0800

    增加 broker 启动版本号，解决Producer无法发送的问题

--- broker/src/test2/java/org/apache/rocketmq/broker/yunai/BrokerControllerTest.java
+++ broker/src/test2/java/org/apache/rocketmq/broker/yunai/BrokerControllerTest.java
@@ -20,8 +20,10 @@ package org.apache.rocketmq.broker.yunai;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.rocketmq.broker.BrokerController;
 import org.apache.rocketmq.common.BrokerConfig;
+import org.apache.rocketmq.common.MQVersion;
 import org.apache.rocketmq.remoting.netty.NettyClientConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
+import org.apache.rocketmq.remoting.protocol.RemotingCommand;
 import org.apache.rocketmq.store.config.FlushDiskType;
 import org.apache.rocketmq.store.config.MessageStoreConfig;
 import org.junit.Test;
@@ -36,18 +38,22 @@ public class BrokerControllerTest {
      */
     @Test
     public void testBrokerRestart() throws Exception {
+        // 设置版本号
+        System.setProperty(RemotingCommand.REMOTING_VERSION_KEY, Integer.toString(MQVersion.CURRENT_VERSION));
+        //
         final NettyServerConfig nettyServerConfig = new NettyServerConfig();
         nettyServerConfig.setListenPort(10911);
-
+        //
         final BrokerConfig brokerConfig = new BrokerConfig();
         brokerConfig.setBrokerName("broker-a");
         brokerConfig.setNamesrvAddr("127.0.0.1:9876");
-
+        //
         final MessageStoreConfig messageStoreConfig = new MessageStoreConfig();
         messageStoreConfig.setDeleteWhen("04");
         messageStoreConfig.setFileReservedTime(48);
         messageStoreConfig.setFlushDiskType(FlushDiskType.ASYNC_FLUSH);
-
+//        BrokerPathConfigHelper.setBrokerConfigPath("/Users/yunai/百度云同步盘/开发/Javascript/Story/incubator-rocketmq/conf/broker.conf");
+        // broker 启动
         BrokerController brokerController = new BrokerController(//
                 brokerConfig, //
                 nettyServerConfig, //
@@ -55,9 +61,7 @@ public class BrokerControllerTest {
                 messageStoreConfig);
         brokerController.initialize();
         brokerController.start();
-
 //        brokerController.getTopicConfigManager().createTopicInSendMessageBackMethod()
-
         Thread.sleep(DateUtils.MILLIS_PER_DAY);
     }
 
