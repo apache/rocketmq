c60ac522eab2511c295a423427b19b80637c5c46
yukon <yukon@apache.org>
2017-04-10 12:37:08 +0800

    Add openmessaging module.

---
+++ openmessaging/src/main/java/io/openmessaging/rocketmq/MessagingAccessPointImpl.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openmessaging.rocketmq;
+
+import io.openmessaging.IterableConsumer;
+import io.openmessaging.KeyValue;
+import io.openmessaging.MessagingAccessPoint;
+import io.openmessaging.Producer;
+import io.openmessaging.PullConsumer;
+import io.openmessaging.PushConsumer;
+import io.openmessaging.ResourceManager;
+import io.openmessaging.SequenceProducer;
+import io.openmessaging.ServiceEndPoint;
+import io.openmessaging.observer.Observer;
+
+public class MessagingAccessPointImpl implements MessagingAccessPoint {
+    @Override
+    public Producer createProducer() {
+        return null;
+    }
+
+    @Override
+    public Producer createProducer(KeyValue properties) {
+        return null;
+    }
+
+    @Override
+    public SequenceProducer createSequenceProducer() {
+        return null;
+    }
+
+    @Override
+    public SequenceProducer createSequenceProducer(KeyValue properties) {
+        return null;
+    }
+
+    @Override
+    public PushConsumer createPushConsumer() {
+        return null;
+    }
+
+    @Override
+    public PushConsumer createPushConsumer(KeyValue properties) {
+        return null;
+    }
+
+    @Override
+    public PullConsumer createPullConsumer(String queueName) {
+        return null;
+    }
+
+    @Override
+    public PullConsumer createPullConsumer(String queueName, KeyValue properties) {
+        return null;
+    }
+
+    @Override
+    public IterableConsumer createIterableConsumer(String queueName) {
+        return null;
+    }
+
+    @Override
+    public IterableConsumer createIterableConsumer(String queueName, KeyValue properties) {
+        return null;
+    }
+
+    @Override
+    public ResourceManager createResourceManager() {
+        return null;
+    }
+
+    @Override
+    public ServiceEndPoint createServiceEndPoint() {
+        return null;
+    }
+
+    @Override
+    public ServiceEndPoint createServiceEndPoint(KeyValue properties) {
+        return null;
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void deleteObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void startup() {
+        //Ignore
+    }
+
+    @Override
+    public void shutdown() {
+        //Ignore
+    }
+}
