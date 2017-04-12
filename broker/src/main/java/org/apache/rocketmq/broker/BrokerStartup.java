7bcb3b3eae1e3c441861f2a3cd79ff54a8e691b9
Zhanhui Li <lizhanhui@apache.org>
2017-04-12 22:25:11 +0800

    BugFix: WS_DOMAIN_NAME, SUBGROUP default values override custom values passed by java options options

--- broker/src/main/java/org/apache/rocketmq/broker/BrokerStartup.java
+++ broker/src/main/java/org/apache/rocketmq/broker/BrokerStartup.java
@@ -232,12 +232,10 @@ public class BrokerStartup {
 
     private static void properties2SystemEnv(Properties properties) {
         if (properties == null) {
-            log.info("No properties to set system environment");
             return;
         }
-
-        String rmqAddressServerDomain = properties.getProperty("rmqAddressServerDomain", MixAll.DEFAULT_NAMESRV_ADDR_LOOKUP);
-        String rmqAddressServerSubGroup = properties.getProperty("rmqAddressServerSubGroup", "nsaddr");
+        String rmqAddressServerDomain = properties.getProperty("rmqAddressServerDomain", MixAll.WS_DOMAIN_NAME);
+        String rmqAddressServerSubGroup = properties.getProperty("rmqAddressServerSubGroup", MixAll.WS_DOMAIN_SUBGROUP);
         System.setProperty("rocketmq.namesrv.domain", rmqAddressServerDomain);
         System.setProperty("rocketmq.namesrv.domain.subgroup", rmqAddressServerSubGroup);
     }
