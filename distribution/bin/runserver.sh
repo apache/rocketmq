77e657f1addccedae4c21bc287d04fc2c9db71fd
dongeforever <zhendongliu92@yeah.net>
2017-04-21 17:36:31 +0800

    [ROCKETMQ-161] Update runbroker.sh and runserver.sh to support user defined jvm mem flag

--- distribution/bin/runserver.sh
+++ distribution/bin/runserver.sh
@@ -43,6 +43,7 @@ JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
 JAVA_OPT="${JAVA_OPT}  -XX:-UseLargePages"
 JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${BASE_DIR}/lib"
 #JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
+JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} -cp ${CLASSPATH}"
 
 $JAVA ${JAVA_OPT} $@
