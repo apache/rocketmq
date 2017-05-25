4d12d11249950468d2830cae31c0f8909ad14395
dongeforever <zhendongliu92@yeah.net>
2017-05-25 14:26:37 +0800

    [ROCKETMQ-161] Update runbroker.sh and runserver.sh to support user defined jvm mem flag closes apache/incubator-rocketmq#87

--- distribution/bin/runbroker.sh
+++ distribution/bin/runbroker.sh
@@ -46,6 +46,7 @@ JAVA_OPT="${JAVA_OPT} -XX:MaxDirectMemorySize=15g"
 JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages -XX:-UseBiasedLocking"
 JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${BASE_DIR}/lib"
 #JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
+JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} -cp ${CLASSPATH}"
 
 numactl --interleave=all pwd > /dev/null 2>&1
