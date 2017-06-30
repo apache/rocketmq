6ab4ad4553b70bfc39f7cacaf178ba9f01fd69f4
lindzh <linsony0@163.com>
2017-06-30 15:41:32 +0800

    fix read line

--- client/src/test/java/org/apache/rocketmq/client/log/ClientLogTest.java
+++ client/src/test/java/org/apache/rocketmq/client/log/ClientLogTest.java
@@ -34,22 +34,34 @@ public class ClientLogTest {
 
     @Test
     public void testLog4j2() throws IOException {
+        long seek = 0;
         boolean result = false;
+        File file = new File(LOG_DIR + File.separator + "rocketmq_client.log");
+        if (file.exists()) {
+            seek = file.length();
+        }
         Class logClass = ClientLogger.getLogClass();
         Assert.assertEquals("org.apache.logging.slf4j.Log4jLoggerFactory", logClass.getName());
         for (int i = 0; i < 10; i++) {
             ClientLogger.getLog().info("testcase testLog4j2 " + new Date());
         }
-        File file = new File(LOG_DIR + File.separator + "rocketmq_client.log");
-        FileInputStream fileInputStream = new FileInputStream(file);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
-        String line = reader.readLine();
+
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+        randomAccessFile.seek(seek);
+        String line = randomAccessFile.readLine();
+        int idx = 1;
         while (line != null) {
             if (line.contains("testcase testLog4j2")) {
                 result = true;
                 break;
             }
+            line = randomAccessFile.readLine();
+            idx++;
+            if (idx > 50) {
+                break;
+            }
         }
+        randomAccessFile.close();
         Assert.assertTrue(result);
     }
 }
