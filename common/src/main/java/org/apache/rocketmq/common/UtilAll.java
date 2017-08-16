776ddb918f15e8c17809f651ca83c2172c534bb7
mark.yky <mark.yky@alibaba-inc.com>
2017-08-16 16:45:00 +0800

    don’t need cal.setTimeInMillis(System.currentTimeMillis()) after Calendar.getInstance()

--- common/src/main/java/org/apache/rocketmq/common/UtilAll.java
+++ common/src/main/java/org/apache/rocketmq/common/UtilAll.java
@@ -113,7 +113,6 @@ public class UtilAll {
 
     public static long computNextMorningTimeMillis() {
         Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(System.currentTimeMillis());
         cal.add(Calendar.DAY_OF_MONTH, 1);
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
@@ -125,7 +124,6 @@ public class UtilAll {
 
     public static long computNextMinutesTimeMillis() {
         Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(System.currentTimeMillis());
         cal.add(Calendar.DAY_OF_MONTH, 0);
         cal.add(Calendar.HOUR_OF_DAY, 0);
         cal.add(Calendar.MINUTE, 1);
@@ -137,7 +135,6 @@ public class UtilAll {
 
     public static long computNextHourTimeMillis() {
         Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(System.currentTimeMillis());
         cal.add(Calendar.DAY_OF_MONTH, 0);
         cal.add(Calendar.HOUR_OF_DAY, 1);
         cal.set(Calendar.MINUTE, 0);
@@ -149,7 +146,6 @@ public class UtilAll {
 
     public static long computNextHalfHourTimeMillis() {
         Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(System.currentTimeMillis());
         cal.add(Calendar.DAY_OF_MONTH, 0);
         cal.add(Calendar.HOUR_OF_DAY, 1);
         cal.set(Calendar.MINUTE, 30);
