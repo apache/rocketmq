ae438d843e3c859c20b948c1a5ce4e5d2b7ec3d6
傅冲 <yubao.fyb@alibaba-inc.com>
2017-08-29 14:43:09 +0800

    #ROCKETMQ-281#  delete useless test

--- broker/src/test/java/org/apache/rocketmq/broker/BrokerStartupTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/BrokerStartupTest.java
@@ -17,12 +17,9 @@
 
 package org.apache.rocketmq.broker;
 
-import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.nio.channels.OverlappingFileLockException;
 import java.util.Properties;
-import org.apache.rocketmq.store.config.StorePathConfigHelper;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,27 +38,4 @@ public class BrokerStartupTest {
         method.invoke(null, properties);
         Assert.assertEquals("value", System.getProperty("rocketmq.namesrv.domain"));
     }
-
-
-    @Test
-    public void test_checkMQAlreadyStart() throws Exception {
-
-        File file = new File(StorePathConfigHelper.getLockFile(storePathRootDir));
-        if (file.exists()) {
-            file.delete();
-        }
-        BrokerStartup brokerStartup = new BrokerStartup();
-        Method method = brokerStartup.getClass().getDeclaredMethod("checkMQAlreadyStart", String.class);
-        method.setAccessible(true);
-
-        method.invoke(brokerStartup, storePathRootDir);
-
-        try {
-            method.invoke(brokerStartup, storePathRootDir);
-        } catch (Exception e) {
-            Assert.assertEquals(true, e.getCause() instanceof OverlappingFileLockException);
-        }
-
-    }
-
 }
