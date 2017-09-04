6bc0b58c04339c5f24eb306c60b25c6dc8ec626d
vsair <liuxuedee@gmail.com>
2017-09-04 15:09:54 +0800

    [ROCKETMQ-286] Add a switch to decide whether to wake up commitLogService when put message

--- store/src/main/java/org/apache/rocketmq/store/CommitLog.java
+++ store/src/main/java/org/apache/rocketmq/store/CommitLog.java
@@ -652,7 +652,9 @@ public class CommitLog {
             if (!this.defaultMessageStore.getMessageStoreConfig().isTransientStorePoolEnable()) {
                 flushCommitLogService.wakeup();
             } else {
-                commitLogService.wakeup();
+                if (this.defaultMessageStore.getMessageStoreConfig().isWakeCommitWhenPutMessage()) {
+                    commitLogService.wakeup();
+                }
             }
         }
     }
