7a6afd39c8676724727c3de4cc5032f04e12fdfa
lindzh <linsony0@163.com>
2017-12-13 15:36:08 +0800

    add test case for dlq default config

--- broker/src/test/java/org/apache/rocketmq/broker/BrokerControllerTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/BrokerControllerTest.java
@@ -17,10 +17,20 @@
 
 package org.apache.rocketmq.broker;
 
+import org.apache.rocketmq.broker.processor.SendMessageProcessor;
 import org.apache.rocketmq.common.BrokerConfig;
 import org.apache.rocketmq.common.UtilAll;
+import org.apache.rocketmq.common.constant.PermName;
+import org.apache.rocketmq.common.protocol.RequestCode;
+import org.apache.rocketmq.common.protocol.ResponseCode;
 import org.apache.rocketmq.remoting.netty.NettyClientConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
+import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+import org.apache.rocketmq.store.AppendMessageResult;
+import org.apache.rocketmq.store.AppendMessageStatus;
+import org.apache.rocketmq.store.MessageExtBrokerInner;
+import org.apache.rocketmq.store.PutMessageResult;
+import org.apache.rocketmq.store.PutMessageStatus;
 import org.apache.rocketmq.store.config.MessageStoreConfig;
 import org.junit.After;
 import org.junit.Test;
@@ -28,6 +38,8 @@ import org.junit.Test;
 import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class BrokerControllerTest {
     /**
@@ -45,6 +57,20 @@ public class BrokerControllerTest {
         assertThat(brokerController.initialize());
         brokerController.start();
         brokerController.shutdown();
+    }
+
+    @Test
+    public void testBrokerConfigDLQ() throws Exception {
+        BrokerConfig brokerConfig = new BrokerConfig();
+        brokerConfig.setDlqPerm(PermName.PERM_READ | PermName.PERM_WRITE);
+        brokerConfig.setDlqNumsPerGroup(8);
+        BrokerController brokerController = new BrokerController(
+            brokerConfig,
+            new NettyServerConfig(),
+            new NettyClientConfig(),
+            new MessageStoreConfig());
+        assertThat(brokerController.getBrokerConfig().getDlqPerm() == (PermName.PERM_WRITE | PermName.PERM_READ));
+        assertThat(brokerController.getBrokerConfig().getDlqNumsPerGroup() == 8);
     }
 
     @After
