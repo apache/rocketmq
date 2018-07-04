ae468f09390d3ab085513f6bd7ae0f82b5176b3d
shesheng <jungle.lwj@alibaba-inc.com>
2018-07-04 19:09:49 +0800

    modify some unit test

--- store/src/test/java/org/apache/rocketmq/store/ConsumeQueueTest.java
+++ store/src/test/java/org/apache/rocketmq/store/ConsumeQueueTest.java
@@ -213,7 +213,6 @@ public class ConsumeQueueTest {
             try {
                 putMsg(master);
                 // wait build consume queue
-                Thread.sleep(1000);
             } catch (Exception e) {
                 e.printStackTrace();
                 assertThat(Boolean.FALSE).isTrue();
