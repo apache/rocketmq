65e15ae37630b0ef64e4acad9086f0ad12f9cfa2
lirenzuo <97019752@qq.om>
2018-12-07 19:17:40 +0800

    m

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalanceImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalanceImpl.java
@@ -32,13 +32,13 @@ import org.apache.rocketmq.client.impl.FindBrokerResult;
 import org.apache.rocketmq.client.impl.factory.MQClientInstance;
 import org.apache.rocketmq.client.log.ClientLogger;
 import org.apache.rocketmq.common.MixAll;
+import org.apache.rocketmq.logging.InternalLogger;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.common.protocol.body.LockBatchRequestBody;
 import org.apache.rocketmq.common.protocol.body.UnlockBatchRequestBody;
 import org.apache.rocketmq.common.protocol.heartbeat.ConsumeType;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 import org.apache.rocketmq.common.protocol.heartbeat.SubscriptionData;
-import org.apache.rocketmq.logging.InternalLogger;
 
 /**
  * Base class for rebalance algorithm
