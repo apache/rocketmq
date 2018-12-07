9afd5ba18dadf80addb8233829d26ee138f8e9b8
jiangxinlingdu <97019752@qq.om>
2018-12-07 22:39:58 +0800

    修改还原！

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultMQPushConsumerImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultMQPushConsumerImpl.java
@@ -444,7 +444,7 @@ public class DefaultMQPushConsumerImpl implements MQConsumerInner {
         }
     }
 
-    public void executePullRequestLater(final PullRequest pullRequest, final long timeDelay) {
+    private void executePullRequestLater(final PullRequest pullRequest, final long timeDelay) {
         this.mQClientFactory.getPullMessageService().executePullRequestLater(pullRequest, timeDelay);
     }
 
