3a1fd0fb06d367c5c8cd13b68c3f1f7fc9d1a789
chengxiangwang <chengxiangwang@cmss.chinamobile.com>
2018-12-25 17:56:48 +0800

    add test case:testResolveDiscardMsg

--- broker/src/test/java/org/apache/rocketmq/broker/transaction/queue/DefaultTransactionalMessageCheckListenerTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/transaction/queue/DefaultTransactionalMessageCheckListenerTest.java
@@ -16,8 +16,10 @@
  */
 package org.apache.rocketmq.broker.transaction.queue;
 
+import java.net.InetSocketAddress;
 import org.apache.rocketmq.broker.BrokerController;
 import org.apache.rocketmq.common.BrokerConfig;
+import org.apache.rocketmq.common.MixAll;
 import org.apache.rocketmq.common.message.MessageAccessor;
 import org.apache.rocketmq.common.message.MessageConst;
 import org.apache.rocketmq.common.message.MessageExt;
@@ -37,14 +39,18 @@ public class DefaultTransactionalMessageCheckListenerTest {
     private DefaultTransactionalMessageCheckListener listener;
 
     @Spy
-    private BrokerController brokerController = new BrokerController(new BrokerConfig(), new NettyServerConfig(),
-        new NettyClientConfig(), new MessageStoreConfig());
+    private BrokerController brokerController = new BrokerController(new BrokerConfig(),
+            new NettyServerConfig(),
+            new NettyClientConfig(), new MessageStoreConfig());
 
 
     @Before
-    public void init() {
+    public void init() throws Exception {
         listener = new DefaultTransactionalMessageCheckListener();
         listener.setBrokerController(brokerController);
+        brokerController.initialize();
+        brokerController.start();
+
     }
 
     @Test
@@ -53,26 +59,51 @@ public class DefaultTransactionalMessageCheckListenerTest {
     }
 
     @Test
-    public void testSendCheckMessage() throws Exception{
+    public void testSendCheckMessage() throws Exception {
         MessageExt messageExt = createMessageExt();
         listener.sendCheckMessage(messageExt);
     }
 
     @Test
-    public void sendCheckMessage(){
+    public void sendCheckMessage() {
         listener.resolveDiscardMsg(createMessageExt());
     }
 
     private MessageExtBrokerInner createMessageExt() {
         MessageExtBrokerInner inner = new MessageExtBrokerInner();
-        MessageAccessor.putProperty(inner,MessageConst.PROPERTY_REAL_QUEUE_ID,"1");
-        MessageAccessor.putProperty(inner,MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX,"1234255");
-        MessageAccessor.putProperty(inner,MessageConst.PROPERTY_REAL_TOPIC,"realTopic");
-        inner.setTransactionId(inner.getProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX));
+        MessageAccessor.putProperty(inner, MessageConst.PROPERTY_REAL_QUEUE_ID, "1");
+        MessageAccessor
+                .putProperty(inner, MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX, "1234255");
+        MessageAccessor.putProperty(inner, MessageConst.PROPERTY_REAL_TOPIC, "realTopic");
+        inner.setTransactionId(
+                inner.getProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX));
         inner.setBody("check".getBytes());
         inner.setMsgId("12344567890");
         inner.setQueueId(0);
         return inner;
+    }
+
+    @Test
+    public void testResolveDiscardMsg() {
+        MessageExt messageExt = new MessageExt();
+        messageExt.setTopic(MixAll.RMQ_SYS_TRANS_HALF_TOPIC);
+        messageExt.setQueueId(0);
+        messageExt.setBody("test resolve discard msg".getBytes());
+        messageExt.setStoreHost(new InetSocketAddress("127.0.0.1",10911));
+        messageExt.setBornHost(new InetSocketAddress("127.0.0.1",54270));
+        MessageAccessor.putProperty(messageExt, MessageConst.PROPERTY_REAL_TOPIC, "test_topic");
+        MessageAccessor.putProperty(messageExt, MessageConst.PROPERTY_PRODUCER_GROUP,
+                "PID_TEST_DISCARD_MSG");
+        MessageAccessor.putProperty(messageExt, MessageConst.PROPERTY_TRANSACTION_PREPARED, "true");
+        MessageAccessor
+                .putProperty(messageExt, MessageConst.PROPERTY_TRANSACTION_CHECK_TIMES, "15");
+        MessageAccessor
+                .putProperty(messageExt, MessageConst.PROPERTY_REAL_QUEUE_ID, "2");
+        MessageAccessor
+                .putProperty(messageExt, MessageConst.PROPERTY_TAGS, "test_discard_msg");
+        MessageAccessor
+                .putProperty(messageExt, MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX, "AC14157E4F1C18B4AAC27EB1A0F30000");
+        listener.resolveDiscardMsg(messageExt);
     }
 
 }
