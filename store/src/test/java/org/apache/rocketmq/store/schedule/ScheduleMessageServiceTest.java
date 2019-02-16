2a6159b4174bd831772fd1dca4169eefaf9e56e4
ranqiqiang <qiqiang@ran.gmail.com>
2019-02-16 16:05:38 +0800

    1.fork with develop,and commit

--- store/src/test/java/org/apache/rocketmq/store/schedule/ScheduleMessageServiceTest.java
+++ store/src/test/java/org/apache/rocketmq/store/schedule/ScheduleMessageServiceTest.java
@@ -1,4 +1,4 @@
-package org.apache.rocketmq.store.schedule;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -15,7 +15,7 @@ package org.apache.rocketmq.store.schedule;/*
  * limitations under the License.
  */
 
-
+package org.apache.rocketmq.store.schedule;
 import org.apache.rocketmq.common.BrokerConfig;
 import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.message.MessageDecoder;
