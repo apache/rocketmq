19628e07d704b185c19d7bc16248f599fa76a5f5
芝麻谷 <woshihoujinxin@163.com>
2019-02-20 18:25:50 +0800

    validate serialized and deserialized data

--- common/src/test/java/org/apache/rocketmq/common/protocol/body/KVTableTest.java
+++ common/src/test/java/org/apache/rocketmq/common/protocol/body/KVTableTest.java
@@ -17,10 +17,13 @@
 
 package org.apache.rocketmq.common.protocol.body;
 
+import org.apache.rocketmq.remoting.protocol.RemotingSerializable;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class KVTableTest {
 
@@ -40,6 +43,23 @@ public class KVTableTest {
         Assert.assertTrue(kvTable.getTable().size() == 2);
         Assert.assertTrue(kvTable.getTable().equals(table) );
 
+    }
+
+    @Test
+    public void testFromJson() throws Exception {
+        HashMap<String, String> table = new HashMap<String, String>();
+        table.put("key1", "value1");
+        table.put("key2", "value2");
+
+        KVTable kvTable = new KVTable();
+        kvTable.setTable(table);
+
+        String json = RemotingSerializable.toJson(kvTable, true);
+        KVTable fromJson = RemotingSerializable.fromJson(json, KVTable.class);
+
+        assertThat(fromJson).isNotEqualTo(kvTable);
+        assertThat(fromJson.getTable().get("key1")).isEqualTo(kvTable.getTable().get("key1"));
+        assertThat(fromJson.getTable().get("key2")).isEqualTo(kvTable.getTable().get("key2"));
     }
 
 }
