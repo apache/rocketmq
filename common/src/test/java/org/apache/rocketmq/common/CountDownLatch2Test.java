153d4d346b029f13673bb9b4b4d52f0622be7b08
hubo <389528688@qq.com>
2019-02-21 22:06:15 +0800

    add CountDownLatch2Test

---
+++ common/src/test/java/org/apache/rocketmq/common/CountDownLatch2Test.java
@@ -0,0 +1,68 @@
+package org.apache.rocketmq.common;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * CountDownLatch2 Unit Test
+ * @see CountDownLatch2
+ *
+ * @since 2019-02-21
+ */
+public class CountDownLatch2Test {
+
+    /**
+     * test constructor with invalid init param
+     */
+    @Test
+    public void testConstructorError() {
+        int count = -1;
+        try {
+            CountDownLatch2 latch = new CountDownLatch2(count);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), is("count < 0"));
+        }
+    }
+
+    /**
+     * test constructor with valid init param
+     */
+    @Test
+    public void testConstructor() {
+        int count = 10;
+        CountDownLatch2 latch = new CountDownLatch2(count);
+        assertEquals("Expected equal", count, latch.getCount());
+        assertThat("Expected contain", latch.toString(), containsString("[Count = " + count + "]"));
+    }
+
+    @Test(timeout = 2000)
+    public void testCore() throws InterruptedException {
+        int count = 2;
+        CountDownLatch2 latch = new CountDownLatch2(count);
+        // test #countDown
+        latch.countDown();
+        assertEquals("Expected equal", count - 1, latch.getCount());
+        // test #reset
+        latch.reset();
+        assertEquals("Expected equal", count, latch.getCount());
+        //test #await timeout
+        latch.await(1, TimeUnit.SECONDS);
+        assertEquals("Expected equal", count, latch.getCount());
+        //test #await
+        latch.countDown();
+        latch.countDown();
+        latch.await();
+        assertEquals("Expected equal", 0, latch.getCount());
+        // coverage Sync#tryReleaseShared, c==0
+        latch.countDown();
+        assertEquals("Expected equal", 0, latch.getCount());
+    }
+}
