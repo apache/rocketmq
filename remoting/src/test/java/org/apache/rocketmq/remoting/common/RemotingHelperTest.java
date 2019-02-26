1d757ece1fb62522a93a25018263c24f334b48da
ssssssnake <157384081@qq.com>
2019-02-26 21:26:41 +0800

    add new test class for RemotingHelper

---
+++ remoting/src/test/java/org/apache/rocketmq/remoting/common/RemotingHelperTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.remoting.common;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RemotingHelperTest {
+
+    /**
+     * This unit test case ensures that {@link RemotingHelper#exceptionExactDesc(Throwable)} could return the root cause of the exception.
+     */
+    @Test
+    public void testExceptionExactDesc() {
+        String exceptionExactDesc = null;
+
+        try {
+            generateNumberFormatException();
+        } catch (Throwable t) {
+            exceptionExactDesc = RemotingHelper.exceptionExactDesc(t);
+        }
+
+        assertThat(exceptionExactDesc).contains("RemotingHelperTest.generateNumberFormatException");
+    }
+
+    /**
+     * Generate a {@link NumberFormatException}.
+     */
+    private static void generateNumberFormatException() {
+        String emptyStr = null;
+        Long.parseLong(emptyStr);
+    }
+
+}
