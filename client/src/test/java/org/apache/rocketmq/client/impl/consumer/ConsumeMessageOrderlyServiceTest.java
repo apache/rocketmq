13f88c798db9ceae9d9dc291560e6d7a1a8e6c5d
Dani <dani@egenie.cn>
2019-02-27 16:12:24 +0800

    delete code

--- client/src/test/java/org/apache/rocketmq/client/impl/consumer/ConsumeMessageOrderlyServiceTest.java
+++ client/src/test/java/org/apache/rocketmq/client/impl/consumer/ConsumeMessageOrderlyServiceTest.java
@@ -326,35 +326,6 @@ public class ConsumeMessageOrderlyServiceTest {
     }
 
 
-    private PullRequest createPullRequest() {
-        PullRequest pullRequest = new PullRequest();
-        pullRequest.setConsumerGroup(consumerGroup);
-        pullRequest.setNextOffset(1024);
-
-        MessageQueue messageQueue = new MessageQueue();
-        messageQueue.setBrokerName(broker);
-        messageQueue.setQueueId(0);
-        messageQueue.setTopic(topic);
-        pullRequest.setMessageQueue(messageQueue);
-        ProcessQueue processQueue = new ProcessQueue();
-        processQueue.setLocked(true);
-        processQueue.setLastLockTimestamp(System.currentTimeMillis());
-        pullRequest.setProcessQueue(processQueue);
-
-        return pullRequest;
-    }
-
-
-    private PullResultExt createPullResult(PullMessageRequestHeader requestHeader, PullStatus pullStatus,
-                                           List<MessageExt> messageExtList) throws Exception {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        for (MessageExt messageExt : messageExtList) {
-            outputStream.write(MessageDecoder.encode(messageExt, false));
-        }
-        return new PullResultExt(pullStatus, requestHeader.getQueueOffset() + messageExtList.size(), 123, 2048, messageExtList, 0, outputStream.toByteArray());
-    }
-
-
     private void runStartOrShutdown(boolean shutdown) throws Exception {
         pushConsumer.setMessageModel(MessageModel.BROADCASTING);
         ConsumeMessageOrderlyService serviceBroadcasting = new ConsumeMessageOrderlyService(pushConsumer.getDefaultMQPushConsumerImpl(), new MessageListenerOrderly() {
