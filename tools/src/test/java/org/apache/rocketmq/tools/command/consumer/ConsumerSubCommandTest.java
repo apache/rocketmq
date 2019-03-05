53b2695ec9a87dde55b9d5cdcb6e885ff032c0ff
Crazyowen <zhuweizhang@gmail.com>
2019-03-05 22:23:26 +0800

    add header for ConsumerSubCommandTest

--- tools/src/test/java/org/apache/rocketmq/tools/command/consumer/ConsumerSubCommandTest.java
+++ tools/src/test/java/org/apache/rocketmq/tools/command/consumer/ConsumerSubCommandTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.rocketmq.tools.command.consumer;
 
 import org.apache.commons.cli.CommandLine;
@@ -21,20 +37,15 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 
-/***
- *  added by owen.zhang
- */
 @RunWith(MockitoJUnitRunner.class)
 public class ConsumerSubCommandTest {
     private static DefaultMQAdminExt defaultMQAdminExt;
