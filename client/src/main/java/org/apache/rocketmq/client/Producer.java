e9e7a5734af4e88645d6badbd4914a22d6c2a8c0
cengfeng lzy <cengfeng.lzy@alibaba-inc.com>
2019-04-15 19:26:56 +0800

    [graal] Successfully run with main method

--- client/src/main/java/org/apache/rocketmq/client/Producer.java
+++ client/src/main/java/org/apache/rocketmq/client/Producer.java
@@ -8,13 +8,16 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import com.alibaba.fastjson.parser.ParserConfig;
+
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.logging.LogLevel;
 
 public class Producer {
     private static final LoggingHandler log = new LoggingHandler(LogLevel.DEBUG);
     public static void main(String[] args) throws MQClientException, InterruptedException {
-
+    	ParserConfig.global.setAsmEnable(false);
         DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
         producer.start();
 
