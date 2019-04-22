29982d0826efd0c6ff9bc9729af1b92cbbc01745
Liberxue <5831248+Liberxue@users.noreply.github.com>
2019-04-22 23:54:45 +0800

    remove  PermSize MaxPermSize elimination in JDK 8
    remove -XX:PermSize=128m -XX:MaxPermSize=128m

--- distribution/bin/tools.sh
+++ distribution/bin/tools.sh
@@ -36,7 +36,7 @@ export CLASSPATH=.:${BASE_DIR}/conf:${CLASSPATH}
 #===========================================================================================
 # JVM Configuration
 #===========================================================================================
-JAVA_OPT="${JAVA_OPT} -server -Xms1g -Xmx1g -Xmn256m -XX:PermSize=128m -XX:MaxPermSize=128m"
+JAVA_OPT="${JAVA_OPT} -server -Xms1g -Xmx1g -Xmn256m"
 JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${BASE_DIR}/lib:${JAVA_HOME}/jre/lib/ext"
 JAVA_OPT="${JAVA_OPT} -cp ${CLASSPATH}"
 
