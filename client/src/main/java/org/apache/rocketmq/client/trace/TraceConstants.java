604fae02781c68b69df1ec5992f8c447ec860340
duhenglucky <duheng0522@gmail.com>
2019-05-07 21:07:23 +0800

    Polish message trace default trace topic implementation

--- client/src/main/java/org/apache/rocketmq/client/trace/TraceConstants.java
+++ client/src/main/java/org/apache/rocketmq/client/trace/TraceConstants.java
@@ -16,10 +16,13 @@
  */
 package org.apache.rocketmq.client.trace;
 
+import org.apache.rocketmq.common.MixAll;
+
 public class TraceConstants {
 
     public static final String GROUP_NAME = "_INNER_TRACE_PRODUCER";
     public static final char CONTENT_SPLITOR = (char) 1;
     public static final char FIELD_SPLITOR = (char) 2;
     public static final String TRACE_INSTANCE_NAME = "PID_CLIENT_INNER_TRACE_PRODUCER";
+    public static final String traceTopic = MixAll.SYSTEM_TOPIC_PREFIX + "TRACE_DATA_";
 }
