7feddb9c46c342ddb210678b94c045fdeeff5d50
duhenglucky <duheng0522@gmail.com>
2019-05-17 10:29:18 +0800

    Add comment for AccessChannel class

--- client/src/main/java/org/apache/rocketmq/client/AccessChannel.java
+++ client/src/main/java/org/apache/rocketmq/client/AccessChannel.java
@@ -15,7 +15,19 @@
  * limitations under the License.
  */
 package org.apache.rocketmq.client;
+
+/**
+ * Used for set access channel, if need migrate the rocketmq service to cloud, it is We recommend set the value with
+ * "CLOUD". otherwise set with "LOCAL", especially used the message trace feature.
+ */
 public enum AccessChannel {
-    local,
-    cloud,
+    /**
+     * Means connect to private IDC cluster.
+     */
+    LOCAL,
+
+    /**
+     * Means connect to Cloud service.
+     */
+    CLOUD,
 }
