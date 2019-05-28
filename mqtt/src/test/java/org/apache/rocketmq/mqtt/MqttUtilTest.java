caf1439718334d0b97ad2982c1ff3207ee5736be
chengxiangwang <chengxiangwang@cmss.chinamobile.com>
2019-05-28 16:05:51 +0800

    add test case(unfinished)

---
+++ mqtt/src/test/java/org/apache/rocketmq/mqtt/MqttUtilTest.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.mqtt;
+
+import org.apache.rocketmq.mqtt.util.MqttUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MqttUtilTest {
+
+    @Test
+    public void test_getRootTopic() {
+
+        String rootTopic = MqttUtil.getRootTopic("/test/topic");
+        assertEquals("", rootTopic);
+
+        String rootTopic2 = MqttUtil.getRootTopic("test/topic");
+        assertEquals("test", rootTopic2);
+    }
+}
