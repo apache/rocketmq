24d43ed450584c30a3752363c24697ffd96bdfe9
Hu Zongtang <huzongtang@cmss.chinamobile.com>
2019-06-14 12:22:10 +0800

    [ISSUE #1156]new mqadmin API for ACL configuration (#1217)
    * [issue#1164]return the codes to original reput method part.
    
    * [issue#1164]fix issue that Consumer Instance can't consume message from slave when cluster is in the high level tps and master has been killed.
    
    * [issue#1164]if the broker is a master node,then modify reputFromOffset correctly.
    
    * [issue#1164]add some coding comments.
    
    * [ISSUE#1156]new mqadmin API for ACL configuration.Add dataVersion and write data to acl yaml config file in the acl module.
    
    * [ISSUE#1156]add unit test cases for new acl mqadmin API command which adding dataversion part.
    
    * [ISSUE#1156]implement update,delete and query Acl config version in new acl mqadmin API command.
    
    * [ISSUE#1156]polish and optimize the implementation for new acl mqadmin API command.
    
    * [ISSUE#1156]fix the issues that topicPerms and groupPerms can't be updated and there is NPE when querying acl config version.
    
    * [ISSUE#1156]fix small issue that specify wrong plainAccessConfig attribute,the correct one is AccessKey.
    
    * [ISSUE#1156]add updateGlobalWhiteAddr subcommand codes for mqadmin acl command.
    
    * [ISSUE#1156]adjust some codes for cluster acl config version list in the mqadmin acl commands.
    
    * [ISSUE#1156]add acl mqadmin command part in the acl user_guide docs.
    
    * [ISSUE#1156]polish and optimize some part of acl mqadmin command codes.
    
    * [ISSUE#1156]fix code comments issue that the first letter needs to be capitalized and adjust the contents of acl user_guide.

--- tools/src/test/java/org/apache/rocketmq/tools/command/acl/UpdateGlobalWhiteAddrSubCommandTest.java
+++ tools/src/test/java/org/apache/rocketmq/tools/command/acl/UpdateGlobalWhiteAddrSubCommandTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.rocketmq.tools.command.topic;
+package org.apache.rocketmq.tools.command.acl;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
@@ -24,29 +24,16 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UpdateTopicSubCommandTest {
+public class UpdateGlobalWhiteAddrSubCommandTest {
+
     @Test
     public void testExecute() {
-        UpdateTopicSubCommand cmd = new UpdateTopicSubCommand();
+        UpdateGlobalWhiteAddrSubCommand cmd = new UpdateGlobalWhiteAddrSubCommand();
         Options options = ServerUtil.buildCommandlineOptions(new Options());
-        String[] subargs = new String[] {
-            "-b 127.0.0.1:10911",
-            "-t unit-test",
-            "-r 8",
-            "-w 8",
-            "-p 6",
-            "-o false",
-            "-u false",
-            "-s false"};
+        String[] subargs = new String[] {"-g 10.10.103.*,192.168.0.*", "-c default-cluster"};
         final CommandLine commandLine =
             ServerUtil.parseCmdLine("mqadmin " + cmd.commandName(), subargs, cmd.buildCommandlineOptions(options), new PosixParser());
-        assertThat(commandLine.getOptionValue('b').trim()).isEqualTo("127.0.0.1:10911");
-        assertThat(commandLine.getOptionValue('r').trim()).isEqualTo("8");
-        assertThat(commandLine.getOptionValue('w').trim()).isEqualTo("8");
-        assertThat(commandLine.getOptionValue('t').trim()).isEqualTo("unit-test");
-        assertThat(commandLine.getOptionValue('p').trim()).isEqualTo("6");
-        assertThat(commandLine.getOptionValue('o').trim()).isEqualTo("false");
-        assertThat(commandLine.getOptionValue('u').trim()).isEqualTo("false");
-        assertThat(commandLine.getOptionValue('s').trim()).isEqualTo("false");
+        assertThat(commandLine.getOptionValue('g').trim()).isEqualTo("10.10.103.*,192.168.0.*");
+        assertThat(commandLine.getOptionValue('c').trim()).isEqualTo("default-cluster");
     }
 }
