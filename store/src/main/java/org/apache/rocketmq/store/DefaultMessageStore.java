b0b5dadfa2565de9bedadc8e029dd07d7dd1d3de
成旭 <chengxu@souche.com>
2019-06-26 10:13:03 +0800

    [RIP-10] add default message query test

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -1023,6 +1023,7 @@ public class DefaultMessageStore implements MessageStore {
         Map<String, Long> messageIds = new HashMap<String, Long>();
         if (this.shutdown) {
             return messageIds;
+
         }
 
         ConsumeQueue consumeQueue = findConsumeQueue(topic, queueId);
