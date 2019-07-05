372c732ff5c7eb002c8c5c132789bfe16ad6e197
StrigiformesCoder <254584819@qq.com>
2019-07-05 23:22:57 +0800

    a small optimization

--- broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFastFailure.java
+++ broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFastFailure.java
@@ -106,14 +106,14 @@ public class BrokerFastFailure {
                         break;
                     }
                     final RequestTask rt = castRunnable(runnable);
-                    if (rt == null || rt.isStopRun()) {
+                    if (rt == null || rt.isCancelled()) {
                         break;
                     }
 
                     final long behind = System.currentTimeMillis() - rt.getCreateTimestamp();
                     if (behind >= maxWaitTimeMillsInQueue) {
-                        if (blockingQueue.remove(runnable)) {
-                            rt.setStopRun(true);
+                        //Just try to cancel the task whit CAS here. When finding the task is cancelled, executor will skip it.
+                        if (rt.cancel()) {
                             rt.returnResponse(RemotingSysResponseCode.SYSTEM_BUSY, String.format("[TIMEOUT_CLEAN_QUEUE]broker busy, start flow control for a while, period in queue: %sms, size of queue: %d", behind, blockingQueue.size()));
                         }
                     } else {
--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/RequestTask.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/RequestTask.java
@@ -18,6 +18,7 @@
 package org.apache.rocketmq.remoting.netty;
 
 import io.netty.channel.Channel;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.rocketmq.remoting.protocol.RemotingCommand;
 
 public class RequestTask implements Runnable {
@@ -25,7 +26,12 @@ public class RequestTask implements Runnable {
     private final long createTimestamp = System.currentTimeMillis();
     private final Channel channel;
     private final RemotingCommand request;
-    private boolean stopRun = false;
+
+    private static final int INIT = 0;
+    private static final int RUNNING = 1;
+    private static final int DONE = 2;
+    private static final int CANCEL = -1;
+    private AtomicInteger status = new AtomicInteger(INIT);
 
     public RequestTask(final Runnable runnable, final Channel channel, final RemotingCommand request) {
         this.runnable = runnable;
@@ -39,7 +45,7 @@ public class RequestTask implements Runnable {
         result = 31 * result + (int) (getCreateTimestamp() ^ (getCreateTimestamp() >>> 32));
         result = 31 * result + (channel != null ? channel.hashCode() : 0);
         result = 31 * result + (request != null ? request.hashCode() : 0);
-        result = 31 * result + (isStopRun() ? 1 : 0);
+        result = 31 * result + (status.get());
         return result;
     }
 
@@ -54,7 +60,7 @@ public class RequestTask implements Runnable {
 
         if (getCreateTimestamp() != that.getCreateTimestamp())
             return false;
-        if (isStopRun() != that.isStopRun())
+        if (status.get() != that.status.get())
             return false;
         if (channel != null ? !channel.equals(that.channel) : that.channel != null)
             return false;
@@ -66,18 +72,20 @@ public class RequestTask implements Runnable {
         return createTimestamp;
     }
 
-    public boolean isStopRun() {
-        return stopRun;
+    public boolean cancel() {
+        return this.status.compareAndSet(INIT, CANCEL);
     }
 
-    public void setStopRun(final boolean stopRun) {
-        this.stopRun = stopRun;
+    public boolean isCancelled() {
+        return this.status.get() == CANCEL;
     }
 
     @Override
     public void run() {
-        if (!this.stopRun)
+        if (this.status.compareAndSet(INIT, RUNNING)) {
             this.runnable.run();
+            this.status.set(DONE);
+        }
     }
 
     public void returnResponse(int code, String remark) {
