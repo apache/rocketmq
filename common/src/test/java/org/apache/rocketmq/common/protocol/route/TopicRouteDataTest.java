3dcbd05292ca1c3fce0f8575162040c0b78fc107
Lie Bin Zheng <lebin_zheng@foxmail.com>
2019-07-18 09:29:10 +0800

    Fix the package of TopicRouteDataTest

--- common/src/test/java/org/apache/rocketmq/common/protocol/route/TopicRouteDataTest.java
+++ common/src/test/java/org/apache/rocketmq/common/protocol/route/TopicRouteDataTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.rocketmq.common.protocol;
+package org.apache.rocketmq.common.protocol.route;
 
 
 import org.apache.rocketmq.common.protocol.route.BrokerData;
