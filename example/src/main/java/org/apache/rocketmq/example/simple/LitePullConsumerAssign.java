cfca4401d5a0018f506aa655c452fb6a98ab5a76
King <794220751@qq.com>
2019-08-19 14:57:16 +0800

    Polish LitePullConsumer (#1395)
    * fix unsubscribe code
    
    * fix commit consumed offset
    
    * fix commit consumed offset
    
    * fix commit consumed offset
    
    * fix commit consumed offset
    
    * polish commit consumed offset
    
    * pass checkstyle
    
    * pass checkstyle
    
    * polish LiteMQPullConsumer
    
    * add flow control and polish commit logic
    
    * fix bug
    
    * polish code
    
    * fix commit consumed offset back
    
    * refactor litePullConsumer
    
    * development save
    
    * development save
    
    * Refactor DefaultLitePullConsumer and DefaultLitePullConsumerImpl.
    
    * Polish lite pull consumer
    
    * polish lite pull consumer
    
    * polish lite pull consumer
    
    * fix seek
    
    * fix seek function
    
    * polish lite pull consumer
    
    * add apache header
    
    * add test
    
    * polish test
    
    * Make broadcast model work for litePullConsumer
    
    * Revert example/broadcast/PushConsumer.java
    
    * Add delay time when no new message
    
    * Enable long polling mode
    
    * Fix subscribe bug when rebalance
    
    * Delete useless consumeMessageHook
    
    * Implement TopicMessageQueueChangeListener interface
    
    * Lite pull consumer support namespace
    
    * Make sql92 filter work
    
    * 1. Add javadoc for lite pull consumer.
    2. Polish lite pul consumer parameters
    
    * Modify lite pull consumer example
    
    * Add apache header for LitePullConsumerAssign example.
    
    * Add unit test for lite pull consumer
    
    * Fix typo

--- example/src/main/java/org/apache/rocketmq/example/simple/LitePullConsumerAssign.java
+++ example/src/main/java/org/apache/rocketmq/example/simple/LitePullConsumerAssign.java
@@ -18,31 +18,35 @@ package org.apache.rocketmq.example.simple;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import org.apache.rocketmq.client.consumer.DefaultLitePullConsumer;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.message.MessageQueue;
 
-public class LitePullConsumer {
+public class LitePullConsumerAssign {
+
+    public static volatile boolean running = true;
+
     public static void main(String[] args) throws Exception {
         DefaultLitePullConsumer litePullConsumer = new DefaultLitePullConsumer("please_rename_unique_group_name");
         litePullConsumer.setAutoCommit(false);
         litePullConsumer.start();
         Collection<MessageQueue> mqSet = litePullConsumer.fetchMessageQueues("TopicTest");
         List<MessageQueue> list = new ArrayList<>(mqSet);
-        Collection<MessageQueue> assginMq = Collections.singletonList(list.get(0));
-        litePullConsumer.assign(assginMq);
-        int size = 0;
-        litePullConsumer.seek(list.get(0), 26);
-
-        while (true) {
-            List<MessageExt> messageExts = litePullConsumer.poll();
-            if (messageExts != null) {
-                size += messageExts.size();
+        List<MessageQueue> assignList = new ArrayList<>();
+        for (int i = 0; i < list.size() / 2; i++) {
+            assignList.add(list.get(i));
+        }
+        litePullConsumer.assign(assignList);
+        litePullConsumer.seek(assignList.get(0), 10);
+        try {
+            while (running) {
+                List<MessageExt> messageExts = litePullConsumer.poll();
+                System.out.printf("%s %n", messageExts);
+                litePullConsumer.commitSync();
             }
-            litePullConsumer.commitSync();
-            System.out.printf("%s %d %n", messageExts, size);
+        } finally {
+            litePullConsumer.shutdown();
         }
 
     }
