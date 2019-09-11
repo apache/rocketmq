ba9ae0464cb75118f245def77f493051de629263
qqeasonchen <qqeasonchen@gmail.com>
2019-09-11 19:40:04 +0800

    add example of request-response model

--- client/src/main/java/org/apache/rocketmq/client/utils/MessageUtil.java
+++ client/src/main/java/org/apache/rocketmq/client/utils/MessageUtil.java
@@ -17,31 +17,31 @@
 
 package org.apache.rocketmq.client.utils;
 
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.MixAll;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageAccessor;
 import org.apache.rocketmq.common.message.MessageConst;
 
 public class MessageUtil {
-    public static Message createReplyMessage(final Message requestMessage) {
+    public static Message createReplyMessage(final Message requestMessage) throws MQClientException {
         if (requestMessage != null) {
             Message replyMessage = new Message();
             String cluster = requestMessage.getProperty(MessageConst.PROPERTY_CLUSTER);
             String replyTo = requestMessage.getProperty(MessageConst.PROPERTY_MESSAGE_REPLY_TO);
             String requestUniqId = requestMessage.getProperty(MessageConst.PROPERTY_REQUEST_UNIQ_ID);
             String ttl = requestMessage.getProperty(MessageConst.PROPERTY_MESSAGE_TTL);
-            if (cluster == null) {
+            if (cluster != null) {
+                String replyTopic = MixAll.getReplyTopic(cluster);
+                replyMessage.setTopic(replyTopic);
+                MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_MESSAGE_TYPE, MixAll.REPLY_MESSAGE_FLAG);
+                MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_REQUEST_UNIQ_ID, requestUniqId);
+                MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_MESSAGE_REPLY_TO, replyTo);
+                MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_MESSAGE_TTL, ttl);
 
+                return replyMessage;
             }
-            String replyTopic = MixAll.getReplyTopic(cluster);
-            replyMessage.setTopic(replyTopic);
-            MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_MESSAGE_TYPE, MixAll.REPLY_MESSAGE_FLAG);
-            MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_REQUEST_UNIQ_ID, requestUniqId);
-            MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_MESSAGE_REPLY_TO, replyTo);
-            MessageAccessor.putProperty(replyMessage, MessageConst.PROPERTY_MESSAGE_TTL, ttl);
-
-            return replyMessage;
         }
-        return null;
+        throw new MQClientException(-1, "create reply message fail.");
     }
 }
