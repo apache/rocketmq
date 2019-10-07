d120cad1c147478a56076c4b68bcfd3e420326db
Li Zhanhui <lizhanhui@apache.org>
2019-10-07 18:14:47 +0800

    Generate standard-compliant JSON

--- remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingSerializable.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingSerializable.java
@@ -17,6 +17,7 @@
 package org.apache.rocketmq.remoting.protocol;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import java.nio.charset.Charset;
 
 public abstract class RemotingSerializable {
@@ -24,14 +25,16 @@ public abstract class RemotingSerializable {
 
     public static byte[] encode(final Object obj) {
         final String json = toJson(obj, false);
-        if (json != null) {
-            return json.getBytes(CHARSET_UTF8);
-        }
-        return null;
+        return json.getBytes(CHARSET_UTF8);
     }
 
     public static String toJson(final Object obj, boolean prettyFormat) {
-        return JSON.toJSONString(obj, prettyFormat);
+        if (prettyFormat) {
+            return JSON.toJSONString(obj, SerializerFeature.PrettyFormat,
+                SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.BrowserCompatible);
+        }
+        return JSON.toJSONString(obj, SerializerFeature.DisableCircularReferenceDetect,
+            SerializerFeature.BrowserCompatible);
     }
 
     public static <T> T decode(final byte[] data, Class<T> classOfT) {
