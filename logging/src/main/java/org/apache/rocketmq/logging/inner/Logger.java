adac5a19549292832e7128a4ce67718b3d92e1b5
root <chinaloryu@gmail.com>
2019-10-25 10:07:27 +0800

    @20191025 add drone pipeline

--- logging/src/main/java/org/apache/rocketmq/logging/inner/Logger.java
+++ logging/src/main/java/org/apache/rocketmq/logging/inner/Logger.java
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * @20191025 add drone test
  */
 
 package org.apache.rocketmq.logging.inner;
