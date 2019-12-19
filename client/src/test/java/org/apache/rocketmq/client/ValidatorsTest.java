e2dffc6161e7c677cdf5a109fb4dd8a6447ab14c
Xu Haozhe <jamesxuhaozhe@gmail.com>
2019-12-19 11:53:40 +0800

    [ISSUE #1665] Add more unit test for Validators

--- client/src/test/java/org/apache/rocketmq/client/ValidatorsTest.java
+++ client/src/test/java/org/apache/rocketmq/client/ValidatorsTest.java
@@ -19,7 +19,9 @@ package org.apache.rocketmq.client;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.MixAll;
+import org.apache.rocketmq.common.message.Message;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -79,5 +81,118 @@ public class ValidatorsTest {
         } catch (MQClientException e) {
             assertThat(e).hasMessageStartingWith("The specified topic is longer than topic max length 255.");
         }
+    }
+
+    @Test
+    public void testCheckGroup_Success() throws MQClientException {
+        Validators.checkGroup("Group");
+        Validators.checkGroup("%TEST%Group");
+        Validators.checkGroup("_%TEST%Group");
+        Validators.checkGroup("-%TEST%Group");
+        Validators.checkGroup("223-%TEST%Group");
+    }
+
+    @Test
+    public void testCheckGroup_BlankGroup() {
+        String blankGroup = "";
+        try {
+            Validators.checkGroup(blankGroup);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageStartingWith("the specified group is blank");
+        }
+    }
+
+    @Test
+    public void testCheckGroup_HasIllegalCharacters() {
+        String illegalGroup = "GROUP&*^";
+        try {
+            Validators.checkGroup(illegalGroup);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageStartingWith(String.format("the specified group[%s] contains illegal characters, allowing only %s", illegalGroup, Validators.VALID_PATTERN_STR));
+        }
+    }
+
+    @Test
+    public void testCheckGroup_TooLongGroup() {
+        String tooLongGroup = StringUtils.rightPad("TooLongGroup", Validators.CHARACTER_MAX_LENGTH + 1, "_");
+        assertThat(tooLongGroup.length()).isGreaterThan(Validators.CHARACTER_MAX_LENGTH);
+        try {
+            Validators.checkGroup(tooLongGroup);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageStartingWith("the specified group is longer than group max length 255.");
+        }
+    }
+
+    @Test
+    public void testCheckMessage_Success() throws MQClientException {
+        Message message = new Message();
+        message.setTopic("hello");
+        message.setBody("test msg body".getBytes());
+
+        DefaultMQProducer producer = new DefaultMQProducer();
+        Validators.checkMessage(message, producer);
+    }
+
+    @Test
+    public void testCheckMessage_NullMessage() {
+        Message nullMessage = null;
+        try {
+            Validators.checkMessage(nullMessage, null);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageContaining("the message is null");
+        }
+    }
+
+    @Test
+    public void testCheckMessage_NullMessageBody() {
+        Message messageWithNullBody = new Message();
+        messageWithNullBody.setTopic("hello");
+        messageWithNullBody.setBody(null);
+        try {
+            Validators.checkMessage(messageWithNullBody, null);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageContaining("the message body is null");
+        }
+    }
+
+    @Test
+    public void testCheckMessage_ZeroMessageBodyLen() {
+        Message messageWithZeroBodyLen = new Message();
+        messageWithZeroBodyLen.setTopic("hello");
+        messageWithZeroBodyLen.setBody(new byte[0]);
+        try {
+            Validators.checkMessage(messageWithZeroBodyLen, null);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageContaining("the message body length is zero");
+        }
+    }
+
+    @Test
+    public void testCheckMessage_TooLongMessageBodyLen() {
+        DefaultMQProducer producer = new DefaultMQProducer();
+
+        Message messageWithTooLongBodyLen = new Message();
+        messageWithTooLongBodyLen.setTopic("hello");
+        messageWithTooLongBodyLen.setBody(new byte[producer.getMaxMessageSize() + 1]);
+
+        assertThat(messageWithTooLongBodyLen.getBody().length).isGreaterThan(producer.getMaxMessageSize());
+
+        try {
+            Validators.checkMessage(messageWithTooLongBodyLen, producer);
+            failBecauseExceptionWasNotThrown(MQClientException.class);
+        } catch (MQClientException e) {
+            assertThat(e).hasMessageContaining(String.format("the message body size over max value, MAX: %d", producer.getMaxMessageSize()));
+        }
+    }
+
+    @Test
+    public void testRegularExpressionMatcher_NullPattern() {
+        assertThat(Validators.regularExpressionMatcher("testOrigin", null)).isTrue();
     }
 }
