d63f56958ddfd75ee367c102c8c448fd88a348ff
rushsky518 <rushsky158@163.com>
2020-01-26 15:29:04 +0800

    add a trace demo

---
+++ example/src/main/java/org/apache/rocketmq/example/tracemessage/TraceInfoConsumer.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.example.tracemessage;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.trace.TraceBean;
+import org.apache.rocketmq.client.trace.TraceContext;
+import org.apache.rocketmq.client.trace.TraceDataEncoder;
+import org.apache.rocketmq.common.MixAll;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class TraceInfoConsumer {
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("C_TRACE");
+        // use the default message track trace topic name
+        consumer.subscribe(MixAll.RMQ_SYS_TRACE_TOPIC, "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                if (msgs.size() > 0) for(int i = 0; i < msgs.size(); i++) {
+                    List<TraceContext> traceContexts = TraceDataEncoder.decoderFromTraceDataString(new String(msgs.get(i).getBody()));
+                    for (TraceContext tc: traceContexts) {
+                        for (TraceBean tb: tc.getTraceBeans()) {
+                            StringBuilder sb = new StringBuilder();
+                            sb.append(tb.getClientHost()).append(" ")
+                                    .append(tc.getGroupName()).append(" ")
+                                    .append(tc.getTimeStamp()).append(" ")
+                                    .append(tc.getTraceType()).append(" ")
+                                    .append(tb.getTopic()).append(" ")
+                                    .append(tb.getMsgId()).append(" ")
+                                    .append(tb.getMsgType()).append(" ");
+                            System.out.println(sb.toString());
+                        }
+                    }
+                }
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
