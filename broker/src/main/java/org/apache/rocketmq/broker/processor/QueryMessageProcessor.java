df174322dcb7afc0ee93e0563461327e979aa651
thenx-wei <thenx-wei@thenx.org>
2020-01-27 13:35:34 +0800

    In the QueryMessageProcessor class, the equals method of Object is prone to null pointer exceptions and should be invoked using constants or objects that determine values

--- broker/src/main/java/org/apache/rocketmq/broker/processor/QueryMessageProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/QueryMessageProcessor.java
@@ -80,7 +80,7 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
         response.setOpaque(request.getOpaque());
 
         String isUniqueKey = request.getExtFields().get(MixAll.UNIQUE_MSG_QUERY_FLAG);
-        if (isUniqueKey != null && isUniqueKey.equals("true")) {
+        if ("true".equals(isUniqueKey)) {
             requestHeader.setMaxNum(this.brokerController.getMessageStoreConfig().getDefaultQueryMaxNum());
         }
 
