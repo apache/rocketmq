804c1898a29002a1176b3a7783eefe4769dc3297
lich0079 <lich0079@gmail.com>
2020-02-01 14:50:10 +0800

    Use thread safe List for registerBrokerResultList due to need of multi thread registerBrokerResultList.add()

--- broker/src/main/java/org/apache/rocketmq/broker/out/BrokerOuterAPI.java
+++ broker/src/main/java/org/apache/rocketmq/broker/out/BrokerOuterAPI.java
@@ -16,7 +16,6 @@
  */
 package org.apache.rocketmq.broker.out;
 
-import com.google.common.collect.Lists;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
@@ -123,7 +122,7 @@ public class BrokerOuterAPI {
         final int timeoutMills,
         final boolean compressed) {
 
-        final List<RegisterBrokerResult> registerBrokerResultList = Lists.newArrayList();
+        final List<RegisterBrokerResult> registerBrokerResultList = new CopyOnWriteArrayList<>();
         List<String> nameServerAddressList = this.remotingClient.getNameServerAddressList();
         if (nameServerAddressList != null && nameServerAddressList.size() > 0) {
 
