b8bb434e8302f72b09578b0dbbe7c1a9e30ed84c
zero.xu <zero.xu@bytedance.com>
2020-03-27 14:38:57 +0800

    feat(store): remove extra flush of StoreCheckpoint

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -314,7 +314,6 @@ public class DefaultMessageStore implements MessageStore {
             this.reputMessageService.shutdown();
             this.flushConsumeQueueService.shutdown();
             this.allocateMappedFileService.shutdown();
-            this.storeCheckpoint.flush();
             this.storeCheckpoint.shutdown();
 
             if (this.runningFlags.isWriteable() && dispatchBehindBytes() == 0) {
