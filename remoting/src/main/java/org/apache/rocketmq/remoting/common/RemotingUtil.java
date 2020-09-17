5f704bba9fd0eafe993d56fc043bff42708c1809
nullhuangyiming <nullhuangyiming@didiglobale.com>
2020-09-17 18:16:09 +0800

    get Selector don't need to judge by programming #2306

--- remoting/src/main/java/org/apache/rocketmq/remoting/common/RemotingUtil.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/common/RemotingUtil.java
@@ -56,34 +56,7 @@ public class RemotingUtil {
     }
 
     public static Selector openSelector() throws IOException {
-        Selector result = null;
-
-        if (isLinuxPlatform()) {
-            try {
-                final Class<?> providerClazz = Class.forName("sun.nio.ch.EPollSelectorProvider");
-                if (providerClazz != null) {
-                    try {
-                        final Method method = providerClazz.getMethod("provider");
-                        if (method != null) {
-                            final SelectorProvider selectorProvider = (SelectorProvider) method.invoke(null);
-                            if (selectorProvider != null) {
-                                result = selectorProvider.openSelector();
-                            }
-                        }
-                    } catch (final Exception e) {
-                        log.warn("Open ePoll Selector for linux platform exception", e);
-                    }
-                }
-            } catch (final Exception e) {
-                // ignore
-            }
-        }
-
-        if (result == null) {
-            result = Selector.open();
-        }
-
-        return result;
+        return Selector.open();
     }
 
     public static boolean isLinuxPlatform() {
