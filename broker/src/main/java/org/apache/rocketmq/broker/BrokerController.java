922b36e031f5e08cf7d33f2dce60eefc651188b4
horizonzy <1060026287@qq.com>
2020-12-30 12:26:11 +0800

    checkstyle fix

--- broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
+++ broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
@@ -31,7 +31,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import org.apache.rocketmq.acl.AccessValidator;
 import org.apache.rocketmq.broker.client.ClientHousekeepingService;
