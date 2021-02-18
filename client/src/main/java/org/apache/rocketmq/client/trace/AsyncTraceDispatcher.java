21b558cbf7c7e1e7dab9b337636a7d45c4acfca2
张玻 <zhangbo@mydataway.com>
2021-02-18 09:41:50 +0800

    update the method name to incrementAndGet

--- client/src/main/java/org/apache/rocketmq/client/trace/AsyncTraceDispatcher.java
+++ client/src/main/java/org/apache/rocketmq/client/trace/AsyncTraceDispatcher.java
@@ -387,7 +387,7 @@ public class AsyncTraceDispatcher implements TraceDispatcher {
                                     filterMqs.add(queue);
                                 }
                             }
-                            int index = sendWhichQueue.getAndIncrement();
+                            int index = sendWhichQueue.incrementAndGet();
                             int pos = Math.abs(index) % filterMqs.size();
                             if (pos < 0) {
                                 pos = 0;
