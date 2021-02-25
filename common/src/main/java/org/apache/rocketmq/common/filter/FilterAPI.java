a1dea52a7c333d38b944930e64e514325f667767
sunxi <sunxi_mail@163.com>
2021-02-25 10:04:04 +0800

    fix

--- common/src/main/java/org/apache/rocketmq/common/filter/FilterAPI.java
+++ common/src/main/java/org/apache/rocketmq/common/filter/FilterAPI.java
@@ -36,7 +36,7 @@ public class FilterAPI {
         return simple;
     }
 
-    public static SubscriptionData buildSubscriptionData( String topic, String subString) throws Exception {
+    public static SubscriptionData buildSubscriptionData(String topic, String subString) throws Exception {
         SubscriptionData subscriptionData = new SubscriptionData();
         subscriptionData.setTopic(topic);
         subscriptionData.setSubString(subString);
