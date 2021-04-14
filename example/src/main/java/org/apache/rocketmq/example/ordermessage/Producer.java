0ff882bd4d1d0054ce9d261abef21fa6da3bcfe5
zhangjidi2016 <zhangjidi@cmss.chinamobile.com>
2021-04-14 11:16:25 +0800

    [ISSUE #2801]Fix NamesrvAddr connot set in Producer

--- example/src/main/java/org/apache/rocketmq/example/ordermessage/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/ordermessage/Producer.java
@@ -32,7 +32,7 @@ import org.apache.rocketmq.remoting.exception.RemotingException;
 public class Producer {
     public static void main(String[] args) throws UnsupportedEncodingException {
         try {
-            MQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+            DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
             producer.start();
 
             String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
