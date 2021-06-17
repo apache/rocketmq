a950093af277ceff4c811141f7984f7d197d7e9c
wuzh <wuzh@bosera.com>
2021-06-17 13:52:02 +0800

    optimize imports

--- example/src/main/java/org/apache/rocketmq/example/benchmark/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/benchmark/Producer.java
@@ -37,7 +37,11 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.TimerTask;
-import java.util.concurrent.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Producer {
