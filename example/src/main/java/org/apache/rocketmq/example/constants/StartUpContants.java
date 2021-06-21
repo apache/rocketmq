ceaea9b53f09903d69d82f580b8a8ae3fa221065
Steven Liang <1634074538@qq.com>
2021-06-21 23:12:20 +0800

    add: 吧原本设置namesrv地址的写法从consumer.setNamesrvAddr("127.0.0.1:9876");变成        consumer.setNamesrvAddr(StartUpContants.DEFAULT_NAMESRV_ADDRESS);，方便统一修改地址

--- example/src/main/java/org/apache/rocketmq/example/constants/StartUpContants.java
+++ example/src/main/java/org/apache/rocketmq/example/constants/StartUpContants.java
@@ -2,7 +2,6 @@ package org.apache.rocketmq.example.constants;
 
 import javax.xml.bind.annotation.XmlType;
 
-
 public class StartUpContants {
     public static final String DEFAULT_NAMESRV_ADDRESS="127.0.0.1:9876";
 }
