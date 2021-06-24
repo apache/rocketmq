74144e27763d61dd6d8d30f1999a3215f931e9da
Steven Liang <1634074538@qq.com>
2021-06-24 21:59:00 +0800

    add: add the Dynamic flow control of Default DefaultMQPushConsumer,and give a use sample
    add: add the Dynamic flow control of Default DefaultMQPushConsumer,and give a sample
    
    add: add the Dynamic flow control of Default DefaultMQPushConsumer,and give a sample
    
    add: add the test of ReactiveAdjustFlow
    
    add: add the lisence
    
    change:change the location of license
    
    change:change the location of license
    
    change: 原地址使用localhost报错（why？），改为127.0.0.1
    
    change: 为了不影响之前测试，将单元测试单独抽离一个文件
    
    change: 为去掉多余的桩
    
    change: 由于之前的example中部分sample未加namesrc的地址导致在本地运行时出现connect to null的错误，而部分加地址直接使用        producer.setNamesrvAddr("127.0.0.1")的方法，不方便统一修改，目前统一使用StartUpConstants存放地址，为所有example加上使其可以本地运行并且方便修改;
    
    change: 回滚

---
+++ example/src/main/java/org/apache/rocketmq/example/adjustflow/Producer.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.example.adjustflow;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * This class demonstrates how to send messages to brokers using provided {@link DefaultMQProducer}.
+ */
+public class Producer {
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+
+
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+
+        for (int i = 0; i < 12000; i++) {
+            try {
+
+                Message msg = new Message("TopicTest01" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+                );
+
+                SendResult sendResult = producer.send(msg);
+
+                System.out.printf("%s%n", sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
+        producer.shutdown();
+    }
+}
