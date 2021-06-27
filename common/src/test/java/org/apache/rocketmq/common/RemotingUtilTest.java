09669a8c3e5d9539903863b3e9b5bdb9b91fe49b
sunbufu <sunyoubufu@163.com>
2021-06-27 15:24:41 +0800

    add convert2IPString method in RemotingUtil

--- common/src/test/java/org/apache/rocketmq/common/RemotingUtilTest.java
+++ common/src/test/java/org/apache/rocketmq/common/RemotingUtilTest.java
@@ -28,4 +28,10 @@ public class RemotingUtilTest {
         assertThat(localAddress).isNotNull();
         assertThat(localAddress.length()).isGreaterThan(0);
     }
+
+    @Test
+    public void testConvert2IpString() {
+        String result = RemotingUtil.convert2IpString("localhost:9876");
+        assertThat(result).isEqualTo("127.0.0.1:9876");
+    }
 }
