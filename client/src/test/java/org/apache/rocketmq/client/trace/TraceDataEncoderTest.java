2cac86626b12fd92e47e9fa12a25ca891214e82a
StyleTang <styletang.me@gmail.com>
2021-08-28 00:04:55 +0800

    [ISSUE #3136] TraceDataEncoder add SubAfter trace bean timestamp (#3137)
    * [ISSUE #3136] TraceDataEncoder add SubAfter trace bean timestamp
    
    * add SubAfter TraceBean TimeStamp and GroupName back

--- client/src/test/java/org/apache/rocketmq/client/trace/TraceDataEncoderTest.java
+++ client/src/test/java/org/apache/rocketmq/client/trace/TraceDataEncoderTest.java
@@ -189,6 +189,8 @@ public class TraceDataEncoderTest {
         subAfterContext.setRequestId("3455848576927");
         subAfterContext.setCostTime(20);
         subAfterContext.setSuccess(true);
+        subAfterContext.setTimeStamp(1625883640000L);
+        subAfterContext.setGroupName("GroupName-test");
         subAfterContext.setContextCode(98623046);
         TraceBean bean = new TraceBean();
         bean.setMsgId("AC1415116D1418B4AAC217FE1B4E0000");
@@ -200,7 +202,7 @@ public class TraceDataEncoderTest {
         String transData = traceTransferBean.getTransData();
         Assert.assertNotNull(transData);
         String[] items = transData.split(String.valueOf(TraceConstants.CONTENT_SPLITOR));
-        Assert.assertEquals(7, items.length);
+        Assert.assertEquals(9, items.length);
 
     }
 
