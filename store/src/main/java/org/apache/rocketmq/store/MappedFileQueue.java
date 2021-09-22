7d4e5cd659adccac6ac15471c800913471c4fae6
Jiang Haiting <jianghaiting@didichuxing.com>
2021-09-22 19:48:27 +0800

    fix compile error

--- store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
+++ store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
