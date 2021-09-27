ba46253462850a7ceb94711191fb780048beb486
liuxu <928525166@qq.com>
2021-09-27 15:33:41 +0800

    修复RocketMQ的RPC调用生成的JSON数据的key不符合json定义规范的问题

--- remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingSerializable.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingSerializable.java
@@ -24,8 +24,8 @@ import java.nio.charset.Charset;
 public abstract class RemotingSerializable {
     private final static Charset CHARSET_UTF8 = Charset.forName("UTF-8");
 
-    static{
-        JSON.DEFAULT_GENERATE_FEATURE|= SerializerFeature.WriteNonStringKeyAsString.getMask();
+    static {
+        JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.WriteNonStringKeyAsString.getMask();
     }
 
     public static byte[] encode(final Object obj) {
