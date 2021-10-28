d84d9f97ca1a9166fd35591d53fa17b76396d84f
BingCoke <2486773105@qq.com>
2021-10-28 12:08:44 +0800

    Validators.checkMessage() is used twice.The other is used in sendDefaultImpl

--- client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
+++ client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
@@ -331,7 +331,6 @@ public class DefaultMQProducer extends ClientConfig implements MQProducer {
     @Override
     public SendResult send(
         Message msg) throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
-        Validators.checkMessage(msg, this);
         msg.setTopic(withNamespace(msg.getTopic()));
         return this.defaultMQProducerImpl.send(msg);
     }
