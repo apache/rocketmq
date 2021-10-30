1e50d2b8b86d734e7b77f7742b93c13039e6b808
BJ89081ML <bj89081ml@BJ89081MLdeMacBook-Pro.local>
2021-10-30 14:52:07 +0800

    fix: #3434

---
+++ client/src/test/java/org/apache/rocketmq/client/consumer/HeartbeatServiceTest.java
@@ -0,0 +1,42 @@
+package org.apache.rocketmq.client.consumer;
+
+import org.apache.rocketmq.client.ClientConfig;
+import org.apache.rocketmq.client.impl.consumer.HeartbeatService;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HeartbeatServiceTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        ClientConfig clientConfig = new ClientConfig();
+        HeartbeatService service = new HeartbeatService(clientConfig.getHeartbeatBrokerInterval());
+        service.start();
+        int n = 100;
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        for (int i = 0; i < n; i++) {
+            int finalI = i;
+            service.addHeartbeatTask(() -> {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (finalI == n - 1) {
+                    countDownLatch.countDown();
+                }
+            });
+            Thread.sleep(1);
+        }
+        countDownLatch.await(1, TimeUnit.SECONDS);
+        service.shutdown();
+        long count = countDownLatch.getCount();
+        assertThat(count).isEqualTo(0);
+    }
+}
