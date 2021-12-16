e0c9670beaf8bd111190d70a60788823011eab07
zh378814 <wb-zh378814@alibaba-inc.com>
2021-12-16 10:10:13 +0800

    fix statsall column asymmetry

--- tools/src/main/java/org/apache/rocketmq/tools/command/stats/StatsAllSubCommand.java
+++ tools/src/main/java/org/apache/rocketmq/tools/command/stats/StatsAllSubCommand.java
@@ -108,7 +108,7 @@ public class StatsAllSubCommand implements SubCommand {
         } else {
             if (!activeTopic || (inMsgCntToday > 0)) {
 
-                System.out.printf("%-64s  %-32s %12d %11.2f %11s %14d %14s%n",
+                System.out.printf("%-64s  %-64s %12d %11.2f %11s %14d %14s%n",
                     UtilAll.frontStringAtLeast(topic, 64),
                     "",
                     0,
