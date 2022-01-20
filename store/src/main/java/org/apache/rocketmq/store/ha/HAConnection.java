84cde5f5beddfc16621f6b5dba31883796d2c216
makabakaboom <402879834@qq.com>
2022-01-20 13:37:01 +0800

    #3015 when slaveAckOffset greater than local commitLog offset, break connection and return (#3016)
    when slaveAckOffset greater than local commitLog offset, break connection and return

--- store/src/main/java/org/apache/rocketmq/store/ha/HAConnection.java
+++ store/src/main/java/org/apache/rocketmq/store/ha/HAConnection.java
@@ -168,6 +168,12 @@ public class HAConnection {
                             if (HAConnection.this.slaveRequestOffset < 0) {
                                 HAConnection.this.slaveRequestOffset = readOffset;
                                 log.info("slave[" + HAConnection.this.clientAddr + "] request offset " + readOffset);
+                            } else if (HAConnection.this.slaveAckOffset > HAConnection.this.haService.getDefaultMessageStore().getMaxPhyOffset()){
+                                log.warn("slave[{}] request offset={} greater than local commitLog offset={}. ",
+                                        HAConnection.this.clientAddr,
+                                        HAConnection.this.slaveAckOffset,
+                                        HAConnection.this.haService.getDefaultMessageStore().getMaxPhyOffset());
+                                return false;
                             }
 
                             HAConnection.this.haService.notifyTransferSome(HAConnection.this.slaveAckOffset);
