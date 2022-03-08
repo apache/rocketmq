a7fe863e6958e93aef04e69eba6f6cc0e008059f
zhangjidi <zhangjidi@cmss.chinamobile.com>
2022-03-08 16:08:08 +0800

    [ISSUE #3940]Optimize AllocateMessageQueueStrategy

--- client/src/main/java/org/apache/rocketmq/client/consumer/rebalance/AllocateMessageQueueByConfig.java
+++ client/src/main/java/org/apache/rocketmq/client/consumer/rebalance/AllocateMessageQueueByConfig.java
@@ -16,16 +16,20 @@
  */
 package org.apache.rocketmq.client.consumer.rebalance;
 
+import java.util.ArrayList;
 import java.util.List;
-import org.apache.rocketmq.client.consumer.AllocateMessageQueueStrategy;
 import org.apache.rocketmq.common.message.MessageQueue;
 
-public class AllocateMessageQueueByConfig implements AllocateMessageQueueStrategy {
+public class AllocateMessageQueueByConfig extends AbstractAllocateMessageQueueStrategy {
     private List<MessageQueue> messageQueueList;
 
     @Override
     public List<MessageQueue> allocate(String consumerGroup, String currentCID, List<MessageQueue> mqAll,
         List<String> cidAll) {
+        List<MessageQueue> result = new ArrayList<MessageQueue>();
+        if (!check(consumerGroup, currentCID, mqAll, cidAll)) {
+            return result;
+        }
         return this.messageQueueList;
     }
 
