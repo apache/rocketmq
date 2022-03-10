57a549a95840b1bf7a621450b1cb705da7f71f50
zhouxiang <zhouxiang.zzx@alibaba-inc.com>
2022-03-10 19:24:44 +0800

    [ISSUE #3949] Add interface for GrpcService

--- proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/ClusterGrpcService.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/ClusterGrpcService.java
@@ -17,12 +17,43 @@
 
 package org.apache.rocketmq.proxy.grpc.service;
 
+import apache.rocketmq.v1.AckMessageRequest;
+import apache.rocketmq.v1.AckMessageResponse;
+import apache.rocketmq.v1.ChangeInvisibleDurationRequest;
+import apache.rocketmq.v1.ChangeInvisibleDurationResponse;
+import apache.rocketmq.v1.EndTransactionRequest;
+import apache.rocketmq.v1.EndTransactionResponse;
+import apache.rocketmq.v1.ForwardMessageToDeadLetterQueueRequest;
+import apache.rocketmq.v1.ForwardMessageToDeadLetterQueueResponse;
 import apache.rocketmq.v1.HealthCheckRequest;
 import apache.rocketmq.v1.HealthCheckResponse;
-import com.google.rpc.Code;
-import java.util.concurrent.CompletableFuture;
+import apache.rocketmq.v1.HeartbeatRequest;
+import apache.rocketmq.v1.HeartbeatResponse;
+import apache.rocketmq.v1.NackMessageRequest;
+import apache.rocketmq.v1.NackMessageResponse;
+import apache.rocketmq.v1.NotifyClientTerminationRequest;
+import apache.rocketmq.v1.NotifyClientTerminationResponse;
+import apache.rocketmq.v1.PollCommandRequest;
+import apache.rocketmq.v1.PollCommandResponse;
+import apache.rocketmq.v1.PullMessageRequest;
+import apache.rocketmq.v1.PullMessageResponse;
+import apache.rocketmq.v1.QueryAssignmentRequest;
+import apache.rocketmq.v1.QueryAssignmentResponse;
+import apache.rocketmq.v1.QueryOffsetRequest;
+import apache.rocketmq.v1.QueryOffsetResponse;
+import apache.rocketmq.v1.QueryRouteRequest;
+import apache.rocketmq.v1.QueryRouteResponse;
+import apache.rocketmq.v1.ReceiveMessageRequest;
+import apache.rocketmq.v1.ReceiveMessageResponse;
+import apache.rocketmq.v1.ReportMessageConsumptionResultRequest;
+import apache.rocketmq.v1.ReportMessageConsumptionResultResponse;
+import apache.rocketmq.v1.ReportThreadStackTraceRequest;
+import apache.rocketmq.v1.ReportThreadStackTraceResponse;
+import apache.rocketmq.v1.SendMessageRequest;
+import apache.rocketmq.v1.SendMessageResponse;
+import io.grpc.Context;
+import io.netty.util.concurrent.CompleteFuture;
 import org.apache.rocketmq.common.constant.LoggerName;
-import org.apache.rocketmq.proxy.grpc.common.ResponseBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,5 +62,80 @@ public class ClusterGrpcService implements GrpcService {
 
     public ClusterGrpcService() {
 
+    }
+
+    @Override public CompleteFuture<QueryRouteResponse> queryRoute(Context ctx, QueryRouteRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<HeartbeatResponse> heartbeat(Context ctx, HeartbeatRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<HealthCheckResponse> healthCheck(Context ctx, HealthCheckRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<SendMessageResponse> sendMessage(Context ctx, SendMessageRequest request) {
+        return null;
+    }
+
+    @Override
+    public CompleteFuture<QueryAssignmentResponse> queryAssignment(Context ctx, QueryAssignmentRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<ReceiveMessageResponse> receiveMessage(Context ctx, ReceiveMessageRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<AckMessageResponse> ackMessage(Context ctx, AckMessageRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<NackMessageResponse> nackMessage(Context ctx, NackMessageRequest request) {
+        return null;
+    }
+
+    @Override
+    public CompleteFuture<ForwardMessageToDeadLetterQueueResponse> forwardMessageToDeadLetterQueue(Context ctx,
+        ForwardMessageToDeadLetterQueueRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<EndTransactionResponse> endTransaction(Context ctx, EndTransactionRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<QueryOffsetResponse> queryOffset(Context ctx, QueryOffsetRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<PullMessageResponse> pullMessage(Context ctx, PullMessageRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<PollCommandResponse> pollCommand(Context ctx, PollCommandRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<ReportThreadStackTraceResponse> reportThreadStackTrace(Context ctx,
+        ReportThreadStackTraceRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<ReportMessageConsumptionResultResponse> reportMessageConsumptionResult(Context ctx,
+        ReportMessageConsumptionResultRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<NotifyClientTerminationResponse> notifyClientTermination(Context ctx,
+        NotifyClientTerminationRequest request) {
+        return null;
+    }
+
+    @Override public CompleteFuture<ChangeInvisibleDurationResponse> changeInvisibleDuration(Context ctx,
+        ChangeInvisibleDurationRequest request) {
+        return null;
     }
 }
