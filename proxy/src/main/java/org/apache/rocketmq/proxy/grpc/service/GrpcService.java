af2342887748dd9887aadda4fb9a0fd1788f9e1e
zhouxiang <zhouxiang.zzx@alibaba-inc.com>
2022-03-10 19:24:44 +0800

    [ISSUE #3949] Add interface for GrpcService

--- proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/GrpcService.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/GrpcService.java
@@ -17,9 +17,80 @@
 
 package org.apache.rocketmq.proxy.grpc.service;
 
+import apache.rocketmq.v1.AckMessageRequest;
+import apache.rocketmq.v1.AckMessageResponse;
+import apache.rocketmq.v1.ChangeInvisibleDurationRequest;
+import apache.rocketmq.v1.ChangeInvisibleDurationResponse;
+import apache.rocketmq.v1.EndTransactionRequest;
+import apache.rocketmq.v1.EndTransactionResponse;
+import apache.rocketmq.v1.ForwardMessageToDeadLetterQueueRequest;
+import apache.rocketmq.v1.ForwardMessageToDeadLetterQueueResponse;
 import apache.rocketmq.v1.HealthCheckRequest;
 import apache.rocketmq.v1.HealthCheckResponse;
-import java.util.concurrent.CompletableFuture;
+import apache.rocketmq.v1.HeartbeatRequest;
+import apache.rocketmq.v1.HeartbeatResponse;
+import apache.rocketmq.v1.NackMessageRequest;
+import apache.rocketmq.v1.NackMessageResponse;
+import apache.rocketmq.v1.NotifyClientTerminationRequest;
+import apache.rocketmq.v1.NotifyClientTerminationResponse;
+import apache.rocketmq.v1.PollCommandRequest;
+import apache.rocketmq.v1.PollCommandResponse;
+import apache.rocketmq.v1.PullMessageRequest;
+import apache.rocketmq.v1.PullMessageResponse;
+import apache.rocketmq.v1.QueryAssignmentRequest;
+import apache.rocketmq.v1.QueryAssignmentResponse;
+import apache.rocketmq.v1.QueryOffsetRequest;
+import apache.rocketmq.v1.QueryOffsetResponse;
+import apache.rocketmq.v1.QueryRouteRequest;
+import apache.rocketmq.v1.QueryRouteResponse;
+import apache.rocketmq.v1.ReceiveMessageRequest;
+import apache.rocketmq.v1.ReceiveMessageResponse;
+import apache.rocketmq.v1.ReportMessageConsumptionResultRequest;
+import apache.rocketmq.v1.ReportMessageConsumptionResultResponse;
+import apache.rocketmq.v1.ReportThreadStackTraceRequest;
+import apache.rocketmq.v1.ReportThreadStackTraceResponse;
+import apache.rocketmq.v1.SendMessageRequest;
+import apache.rocketmq.v1.SendMessageResponse;
+import io.grpc.Context;
+import io.netty.util.concurrent.CompleteFuture;
 
 public interface GrpcService {
+    CompleteFuture<QueryRouteResponse> queryRoute(Context ctx, QueryRouteRequest request);
+
+    CompleteFuture<HeartbeatResponse> heartbeat(Context ctx, HeartbeatRequest request);
+
+    CompleteFuture<HealthCheckResponse> healthCheck(Context ctx, HealthCheckRequest request);
+
+    CompleteFuture<SendMessageResponse> sendMessage(Context ctx, SendMessageRequest request);
+
+    CompleteFuture<QueryAssignmentResponse> queryAssignment(Context ctx, QueryAssignmentRequest request);
+
+    CompleteFuture<ReceiveMessageResponse> receiveMessage(Context ctx, ReceiveMessageRequest request);
+
+    CompleteFuture<AckMessageResponse> ackMessage(Context ctx, AckMessageRequest request);
+
+    CompleteFuture<NackMessageResponse> nackMessage(Context ctx, NackMessageRequest request);
+
+    CompleteFuture<ForwardMessageToDeadLetterQueueResponse> forwardMessageToDeadLetterQueue(Context ctx,
+        ForwardMessageToDeadLetterQueueRequest request);
+
+    CompleteFuture<EndTransactionResponse> endTransaction(Context ctx, EndTransactionRequest request);
+
+    CompleteFuture<QueryOffsetResponse> queryOffset(Context ctx, QueryOffsetRequest request);
+
+    CompleteFuture<PullMessageResponse> pullMessage(Context ctx, PullMessageRequest request);
+
+    CompleteFuture<PollCommandResponse> pollCommand(Context ctx, PollCommandRequest request);
+
+    CompleteFuture<ReportThreadStackTraceResponse> reportThreadStackTrace(Context ctx,
+        ReportThreadStackTraceRequest request);
+
+    CompleteFuture<ReportMessageConsumptionResultResponse> reportMessageConsumptionResult(Context ctx,
+        ReportMessageConsumptionResultRequest request);
+
+    CompleteFuture<NotifyClientTerminationResponse> notifyClientTermination(Context ctx,
+        NotifyClientTerminationRequest request);
+
+    CompleteFuture<ChangeInvisibleDurationResponse> changeInvisibleDuration(Context ctx,
+        ChangeInvisibleDurationRequest request);
 }
