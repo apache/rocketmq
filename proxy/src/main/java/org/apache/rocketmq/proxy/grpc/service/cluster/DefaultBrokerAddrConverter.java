10a7b3c9a28349d163046638e0c5aacd6475e537
kaiyi.lk <kaiyi.lk@alibaba-inc.com>
2022-03-16 19:49:56 +0800

    [ISSUE #3949] ack, nack for cluster mode

---
+++ proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/cluster/DefaultBrokerAddrConverter.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.proxy.grpc.service.cluster;
+
+import io.grpc.Context;
+import org.apache.rocketmq.proxy.connector.route.TopicRouteCache;
+import org.apache.rocketmq.proxy.grpc.common.ParameterConverter;
+
+public class DefaultBrokerAddrConverter implements ParameterConverter<String /* addr */, String /* brokerName */> {
+
+    private final TopicRouteCache topicRouteCache;
+
+    public DefaultBrokerAddrConverter(TopicRouteCache topicRouteCache) {
+        this.topicRouteCache = topicRouteCache;
+    }
+
+    @Override
+    public String convert(Context ctx, String parameter) throws Throwable {
+        return this.topicRouteCache.getBrokerAddr(parameter);
+    }
+}
