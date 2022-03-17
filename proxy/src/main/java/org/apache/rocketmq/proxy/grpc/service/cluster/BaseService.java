1150aa99451d729d9f367abae4d311914c27f2e1
kaiyi.lk <kaiyi.lk@alibaba-inc.com>
2022-03-17 11:31:47 +0800

    [ISSUE #3949] pull message for cluster mode and code refactor

--- proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/cluster/BaseService.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/grpc/service/cluster/BaseService.java
@@ -16,7 +16,12 @@
  */
 package org.apache.rocketmq.proxy.grpc.service.cluster;
 
+import com.google.rpc.Code;
+import io.grpc.Context;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.rocketmq.common.consumer.ReceiptHandle;
 import org.apache.rocketmq.proxy.connector.ConnectorManager;
+import org.apache.rocketmq.proxy.grpc.common.ProxyException;
 
 public class BaseService {
 
@@ -24,5 +29,24 @@ public class BaseService {
 
     public BaseService(ConnectorManager connectorManager) {
         this.connectorManager = connectorManager;
+    }
+
+    protected ReceiptHandle resolveReceiptHandle(Context ctx, String receiptHandleStr) {
+        ReceiptHandle receiptHandle = ReceiptHandle.decode(receiptHandleStr);
+        if (receiptHandle.isExpired()) {
+            throw new ProxyException(Code.INVALID_ARGUMENT, "handle has expired");
+        }
+        return receiptHandle;
+    }
+
+    protected String getBrokerAddr(Context ctx, String brokerName) throws Exception {
+        if (StringUtils.isBlank(brokerName)) {
+            throw new ProxyException(Code.INVALID_ARGUMENT, "broker name is empty");
+        }
+        String addr = this.connectorManager.getTopicRouteCache().getBrokerAddr(brokerName);
+        if (StringUtils.isBlank(addr)) {
+            throw new ProxyException(Code.NOT_FOUND, brokerName + " not exist");
+        }
+        return addr;
     }
 }
