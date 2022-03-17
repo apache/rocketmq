bb4f5e4ec71197ec80bded81062f30b7b1aae328
zhouxiang <zhouxiang.zzx@alibaba-inc.com>
2022-03-17 19:13:23 +0800

    [ISSUE #3949] Implement pull and stream

---
+++ proxy/src/main/java/org/apache/rocketmq/proxy/grpc/adapter/handler/PullMessageResponseHandler.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.proxy.grpc.adapter.handler;
+
+import apache.rocketmq.v1.PullMessageRequest;
+import apache.rocketmq.v1.PullMessageResponse;
+import java.nio.ByteBuffer;
+import java.util.List;
+import org.apache.rocketmq.common.message.MessageDecoder;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.ResponseCode;
+import org.apache.rocketmq.common.protocol.header.PullMessageResponseHeader;
+import org.apache.rocketmq.proxy.grpc.adapter.InvocationContext;
+import org.apache.rocketmq.proxy.grpc.common.Converter;
+import org.apache.rocketmq.proxy.grpc.common.ResponseBuilder;
+import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+
+public class PullMessageResponseHandler implements ResponseHandler<PullMessageRequest, PullMessageResponse> {
+    @Override public void handle(RemotingCommand responseCommand,
+        InvocationContext<PullMessageRequest, PullMessageResponse> context) {
+        try {
+            PullMessageResponseHeader responseHeader = (PullMessageResponseHeader) responseCommand.readCustomHeader();
+            PullMessageResponse.Builder builder = PullMessageResponse.newBuilder();
+            if (responseCommand.getCode() == ResponseCode.SUCCESS) {
+                ByteBuffer byteBuffer = ByteBuffer.wrap(responseCommand.getBody());
+                List<MessageExt> msgFoundList = MessageDecoder.decodes(byteBuffer);
+                for (MessageExt messageExt : msgFoundList) {
+                    builder.addMessages(Converter.buildMessage(messageExt));
+                }
+            }
+            PullMessageResponse response = builder.setCommon(ResponseBuilder.buildCommon(responseCommand.getCode(), responseCommand.getRemark()))
+                .setMinOffset(responseHeader.getMinOffset())
+                .setNextOffset(responseHeader.getNextBeginOffset())
+                .setMaxOffset(responseHeader.getMaxOffset())
+                .build();
+            context.getResponse().complete(response);
+        } catch (Exception e) {
+            context.getResponse().completeExceptionally(e);
+        }
+    }
+}
