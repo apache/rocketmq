1a2f3704bde92f690a4491c8972367edc412b76c
ScarbWin <jjhfen00@163.com>
2022-04-06 22:56:37 +0800

    [ISSUE #4048] Add assertions for DeleteExpiredCommitLogSubCommandTest

--- tools/src/test/java/org/apache/rocketmq/tools/command/broker/DeleteExpiredCommitLogSubCommandTest.java
+++ tools/src/test/java/org/apache/rocketmq/tools/command/broker/DeleteExpiredCommitLogSubCommandTest.java
@@ -16,17 +16,39 @@
  */
 package org.apache.rocketmq.tools.command.broker;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.PosixParser;
 import org.apache.rocketmq.srvutil.ServerUtil;
 import org.apache.rocketmq.tools.command.SubCommandException;
 import org.apache.rocketmq.tools.command.server.ServerResponseMocker;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DeleteExpiredCommitLogSubCommandTest extends ServerResponseMocker {
 
     private static final int PORT = 45678;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @Override
     protected int getPort() {
@@ -46,5 +68,7 @@ public class DeleteExpiredCommitLogSubCommandTest extends ServerResponseMocker {
         final CommandLine commandLine = ServerUtil.parseCmdLine("mqadmin " + cmd.commandName(), subargs,
             cmd.buildCommandlineOptions(options), new PosixParser());
         cmd.execute(commandLine, options, null);
+        Assert.assertTrue(outContent.toString().startsWith("success"));
+        Assert.assertEquals("", errContent.toString());
     }
 }
