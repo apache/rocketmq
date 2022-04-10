049abc24342d8bc10c1b4e842b948c777235e72d
dugenkui03 <dugk@foxmail.com>
2022-04-10 11:33:11 +0800

    remove checked Exception which will never be thrown from method signature

--- srvutil/src/main/java/org/apache/rocketmq/srvutil/FileWatchService.java
+++ srvutil/src/main/java/org/apache/rocketmq/srvutil/FileWatchService.java
@@ -23,7 +23,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
