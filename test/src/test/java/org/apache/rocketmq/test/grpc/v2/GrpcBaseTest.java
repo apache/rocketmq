5a58f2cb10ff42775b26b0f391a8e30117ce92a4
kaiyi.lk <kaiyi.lk@alibaba-inc.com>
2022-04-12 20:27:02 +0800

    [ISSUE #3949] v2 test cases

--- test/src/test/java/org/apache/rocketmq/test/grpc/v2/GrpcBaseTest.java
+++ test/src/test/java/org/apache/rocketmq/test/grpc/v2/GrpcBaseTest.java
@@ -19,11 +19,14 @@ package org.apache.rocketmq.test.grpc.v2;
 
 import apache.rocketmq.v2.AckMessageRequest;
 import apache.rocketmq.v2.AckMessageResponse;
+import apache.rocketmq.v2.ClientOverwrittenSettings;
+import apache.rocketmq.v2.ClientSettings;
 import apache.rocketmq.v2.Code;
-import apache.rocketmq.v2.Endpoints;
 import apache.rocketmq.v2.Message;
 import apache.rocketmq.v2.MessageQueue;
 import apache.rocketmq.v2.MessagingServiceGrpc;
+import apache.rocketmq.v2.NackMessageRequest;
+import apache.rocketmq.v2.NackMessageResponse;
 import apache.rocketmq.v2.QueryRouteRequest;
 import apache.rocketmq.v2.QueryRouteResponse;
 import apache.rocketmq.v2.ReceiveMessageRequest;
@@ -32,10 +35,12 @@ import apache.rocketmq.v2.Resource;
 import apache.rocketmq.v2.SendMessageRequest;
 import apache.rocketmq.v2.SendMessageResponse;
 import apache.rocketmq.v2.SystemProperties;
+import apache.rocketmq.v2.TelemetryCommand;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Duration;
 import com.google.protobuf.Timestamp;
 import io.grpc.Channel;
+import io.grpc.Metadata;
 import io.grpc.ServerInterceptors;
 import io.grpc.ServerServiceDefinition;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
@@ -43,16 +48,22 @@ import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import io.grpc.netty.shaded.io.netty.handler.ssl.ApplicationProtocolConfig;
 import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
 import io.grpc.netty.shaded.io.netty.handler.ssl.SslProvider;
+import io.grpc.stub.MetadataUtils;
+import io.grpc.stub.StreamObserver;
 import io.grpc.testing.GrpcCleanupRule;
 import io.netty.handler.ssl.ApplicationProtocolNames;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import java.io.IOException;
 import java.security.cert.CertificateException;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import javax.net.ssl.SSLException;
 import org.apache.rocketmq.proxy.config.ConfigurationManager;
 import org.apache.rocketmq.proxy.grpc.interceptor.ContextInterceptor;
 import org.apache.rocketmq.proxy.grpc.interceptor.HeaderInterceptor;
+import org.apache.rocketmq.proxy.grpc.interceptor.InterceptorConstants;
 import org.apache.rocketmq.test.base.BaseConf;
 import org.junit.Rule;
 
@@ -65,9 +76,52 @@ public class GrpcBaseTest extends BaseConf {
     @Rule
     public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
+    protected final Metadata header = new Metadata();
+
     private static final int defaultQueueNums = 8;
 
-    protected Channel setUpServer(MessagingServiceGrpc.MessagingServiceImplBase serverImpl,
+    public void setUp() throws Exception {
+        header.put(InterceptorConstants.CLIENT_ID, "client-id" + UUID.randomUUID());
+    }
+
+    protected MessagingServiceGrpc.MessagingServiceStub createStub(Channel channel) {
+        MessagingServiceGrpc.MessagingServiceStub stub = MessagingServiceGrpc.newStub(channel);
+        return MetadataUtils.attachHeaders(stub, header);
+    }
+
+    protected MessagingServiceGrpc.MessagingServiceBlockingStub createBlockingStub(Channel channel) {
+        MessagingServiceGrpc.MessagingServiceBlockingStub stub = MessagingServiceGrpc.newBlockingStub(channel);
+        return MetadataUtils.attachHeaders(stub, header);
+    }
+
+    protected CompletableFuture<ClientOverwrittenSettings> sendClientSettings(MessagingServiceGrpc.MessagingServiceStub stub, ClientSettings clientSettings) {
+        CompletableFuture<ClientOverwrittenSettings> future = CompletableFuture.completedFuture(ClientOverwrittenSettings.getDefaultInstance());
+        StreamObserver<TelemetryCommand> requestStreamObserver = stub.telemetry(new StreamObserver<TelemetryCommand>() {
+            @Override
+            public void onNext(TelemetryCommand value) {
+                TelemetryCommand.CommandCase commandCase = value.getCommandCase();
+                if (TelemetryCommand.CommandCase.CLIENT_OVERWRITTEN_SETTINGS.equals(commandCase)) {
+                    future.complete(value.getClientOverwrittenSettings());
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        });
+        requestStreamObserver.onNext(TelemetryCommand.newBuilder()
+            .setClientSettings(clientSettings)
+            .build());
+        return future;
+    }
+
+    protected void setUpServer(MessagingServiceGrpc.MessagingServiceImplBase serverImpl,
         int port, boolean enableInterceptor) throws IOException, CertificateException {
         SelfSignedCertificate selfSignedCertificate = new SelfSignedCertificate();
         ServerServiceDefinition serviceDefinition = ServerInterceptors.intercept(serverImpl);
@@ -81,7 +135,9 @@ public class GrpcBaseTest extends BaseConf {
             .useTransportSecurity(selfSignedCertificate.certificate(), selfSignedCertificate.privateKey())
             .build()
             .start());
-        // Create a client channel and register for automatic graceful shutdown.
+    }
+
+    protected Channel createChannel(int port) throws SSLException {
         return grpcCleanup.register(NettyChannelBuilder.forAddress("127.0.0.1", port)
             .directExecutor()
             .sslContext(SslContextBuilder
@@ -99,10 +155,6 @@ public class GrpcBaseTest extends BaseConf {
     }
 
     public QueryRouteRequest buildQueryRouteRequest(String topic) {
-        return buildQueryRouteRequest(topic, Endpoints.getDefaultInstance());
-    }
-
-    public QueryRouteRequest buildQueryRouteRequest(String topic, Endpoints endpoints) {
         return QueryRouteRequest.newBuilder()
             .setTopic(Resource.newBuilder()
                 .setName(topic)
@@ -112,7 +164,7 @@ public class GrpcBaseTest extends BaseConf {
 
     public SendMessageRequest buildSendMessageRequest(String topic, String messageId) {
         return SendMessageRequest.newBuilder()
-            .setMessages(0, Message.newBuilder()
+            .addMessages(Message.newBuilder()
                 .setTopic(Resource.newBuilder()
                     .setName(topic)
                     .build())
@@ -158,6 +210,21 @@ public class GrpcBaseTest extends BaseConf {
             .build();
     }
 
+    public NackMessageRequest buildNackMessageRequest(String group, String topic, String messageId, String receiptHandle,
+        int deliveryAttempt) {
+        return NackMessageRequest.newBuilder()
+            .setDeliveryAttempt(deliveryAttempt)
+            .setMessageId(messageId)
+            .setReceiptHandle(receiptHandle)
+            .setTopic(Resource.newBuilder()
+                .setName(topic)
+                .build())
+            .setGroup(Resource.newBuilder()
+                .setName(group)
+                .build())
+            .build();
+    }
+
     public void assertQueryRoute(QueryRouteResponse response, int brokerSize) {
         assertThat(response.getStatus().getCode()).isEqualTo(Code.OK);
         assertThat(response.getMessageQueuesList().size()).isEqualTo(brokerSize * defaultQueueNums);
@@ -182,5 +249,9 @@ public class GrpcBaseTest extends BaseConf {
     public void assertAck(AckMessageResponse response) {
         assertThat(response.getStatus()
             .getCode()).isEqualTo(Code.OK);
+    }
+
+    public void assertNackMessageResponse(NackMessageResponse response) {
+        assertThat(response.getStatus().getCode()).isEqualTo(Code.OK);
     }
 }
