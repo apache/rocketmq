524febb49555ba2fb155f801da021fa419353ed5
yuz10 <845238369@qq.com>
2022-04-25 14:03:00 +0800

    [ISSUE #4107] rocketmq-tools should not depend on rocketmq-store (#4108)

---
+++ common/src/main/java/org/apache/rocketmq/common/stats/Stats.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.common.stats;
+
+public class Stats {
+
+    public static final String QUEUE_PUT_NUMS = "QUEUE_PUT_NUMS";
+    public static final String QUEUE_PUT_SIZE = "QUEUE_PUT_SIZE";
+    public static final String QUEUE_GET_NUMS = "QUEUE_GET_NUMS";
+    public static final String QUEUE_GET_SIZE = "QUEUE_GET_SIZE";
+    public static final String TOPIC_PUT_NUMS = "TOPIC_PUT_NUMS";
+    public static final String TOPIC_PUT_SIZE = "TOPIC_PUT_SIZE";
+    public static final String GROUP_GET_NUMS = "GROUP_GET_NUMS";
+    public static final String GROUP_GET_SIZE = "GROUP_GET_SIZE";
+    public static final String SNDBCK_PUT_NUMS = "SNDBCK_PUT_NUMS";
+    public static final String BROKER_PUT_NUMS = "BROKER_PUT_NUMS";
+    public static final String BROKER_GET_NUMS = "BROKER_GET_NUMS";
+    public static final String GROUP_GET_FROM_DISK_NUMS = "GROUP_GET_FROM_DISK_NUMS";
+    public static final String GROUP_GET_FROM_DISK_SIZE = "GROUP_GET_FROM_DISK_SIZE";
+    public static final String BROKER_GET_FROM_DISK_NUMS = "BROKER_GET_FROM_DISK_NUMS";
+    public static final String BROKER_GET_FROM_DISK_SIZE = "BROKER_GET_FROM_DISK_SIZE";
+    public static final String COMMERCIAL_SEND_TIMES = "COMMERCIAL_SEND_TIMES";
+    public static final String COMMERCIAL_SNDBCK_TIMES = "COMMERCIAL_SNDBCK_TIMES";
+    public static final String COMMERCIAL_RCV_TIMES = "COMMERCIAL_RCV_TIMES";
+    public static final String COMMERCIAL_RCV_EPOLLS = "COMMERCIAL_RCV_EPOLLS";
+    public static final String COMMERCIAL_SEND_SIZE = "COMMERCIAL_SEND_SIZE";
+    public static final String COMMERCIAL_RCV_SIZE = "COMMERCIAL_RCV_SIZE";
+    public static final String COMMERCIAL_PERM_FAILURES = "COMMERCIAL_PERM_FAILURES";
+
+    public static final String GROUP_GET_FALL_SIZE = "GROUP_GET_FALL_SIZE";
+    public static final String GROUP_GET_FALL_TIME = "GROUP_GET_FALL_TIME";
+    public static final String GROUP_GET_LATENCY = "GROUP_GET_LATENCY";
+}
