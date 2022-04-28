0be7653899d51b8228f610eb454e8c3cc02b7ba4
wangfan <wangfan8@xiaomi.com>
2022-04-28 10:19:52 +0800

    [ISSUE #4203] Support zstd/lz4 compression to improve send/receive performance

--- common/src/main/java/org/apache/rocketmq/common/MixAll.java
+++ common/src/main/java/org/apache/rocketmq/common/MixAll.java
@@ -50,6 +50,7 @@ public class MixAll {
     public static final String ROCKETMQ_HOME_PROPERTY = "rocketmq.home.dir";
     public static final String NAMESRV_ADDR_ENV = "NAMESRV_ADDR";
     public static final String NAMESRV_ADDR_PROPERTY = "rocketmq.namesrv.addr";
+    public static final String MESSAGE_COMPRESS_TYPE = "rocketmq.message.compressType";
     public static final String MESSAGE_COMPRESS_LEVEL = "rocketmq.message.compressLevel";
     public static final String DEFAULT_NAMESRV_ADDR_LOOKUP = "jmenv.tbsite.net";
     public static final String WS_DOMAIN_NAME = System.getProperty("rocketmq.namesrv.domain", DEFAULT_NAMESRV_ADDR_LOOKUP);
