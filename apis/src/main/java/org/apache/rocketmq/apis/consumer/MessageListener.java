bcce3d32ce7ed53fd1f2851fe14c16f6e68c0082
rongtong.jrt <rongtong.jrt@alibaba-inc.com>
2022-04-29 16:51:42 +0800

    Fix check style to paas CI

--- apis/src/main/java/org/apache/rocketmq/apis/consumer/MessageListener.java
+++ apis/src/main/java/org/apache/rocketmq/apis/consumer/MessageListener.java
@@ -24,33 +24,31 @@ import org.apache.rocketmq.apis.message.MessageView;
  * synchronously.
  *
  * <p>PushConsumer will fetch messages from brokers and dispatch them to an
- * embedded thread pool in form of <code>Runnable</code> tasks to achieve
- * desirable processing concurrency.
+ * embedded thread pool in form of <code>Runnable</code> tasks to achieve desirable processing concurrency.
  *
  * <p>Refer to {@link PushConsumer} for more further specs.
  *
  * <p>
- *     <strong>Thread Safety</strong>
- *     This class may be called concurrently by multiple threads. Implementation should be thread safe.
+ * <strong>Thread Safety</strong>
+ * This class may be called concurrently by multiple threads. Implementation should be thread safe.
  * </p>
  */
 public interface MessageListener {
 
-  /**
-   * Callback interface to handle incoming messages.
-   *
-   * Application developers are expected to implement this interface to fulfill
-   * business requirements through processing <code>message</code> and return
-   * <code>ConsumeResult</code> accordingly.
-   *
-   * PushConsumer will, on behalf of its group, acknowledge the message to
-   * broker on success; In case of failure or unexpected exceptions
-   * were raised, it will negatively acknowledge <code>message</code>, which
-   * would potentially get re-delivered after the configured back off period.
-   *
-   * @param message The message passed to the listener.
-   * @return {@link ConsumeResult#SUCCESS} if <code>message</code> is properly processed; {@link ConsumeResult#FAILURE}
-   *         otherwise.
-   */
-  ConsumeResult onMessage(MessageView message);
+    /**
+     * Callback interface to handle incoming messages.
+     *
+     * Application developers are expected to implement this interface to fulfill business requirements through
+     * processing <code>message</code> and return
+     * <code>ConsumeResult</code> accordingly.
+     *
+     * PushConsumer will, on behalf of its group, acknowledge the message to broker on success; In case of failure or
+     * unexpected exceptions were raised, it will negatively acknowledge <code>message</code>, which would potentially
+     * get re-delivered after the configured back off period.
+     *
+     * @param message The message passed to the listener.
+     * @return {@link ConsumeResult#SUCCESS} if <code>message</code> is properly processed; {@link
+     * ConsumeResult#FAILURE} otherwise.
+     */
+    ConsumeResult onMessage(MessageView message);
 }
