3d3edfe3a5ed6b0cfcdfd98a60cefbeb53ce10df
hzh0425 <642256541@qq.com>
2022-05-02 16:34:52 +0800

    style: review

--- common/src/main/java/org/apache/rocketmq/common/utils/CheckpointFile.java
+++ common/src/main/java/org/apache/rocketmq/common/utils/CheckpointFile.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.rocketmq.common.utils;
 
 import java.io.BufferedReader;
@@ -48,8 +65,7 @@ public class CheckpointFile<T> {
         }
         synchronized (this) {
             final FileOutputStream fos = new FileOutputStream(this.path);
-            final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos, StandardCharsets.UTF_8));
-            try {
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos, StandardCharsets.UTF_8))) {
                 // Write size
                 writer.write(entries.size() + "");
                 writer.newLine();
@@ -65,8 +81,6 @@ public class CheckpointFile<T> {
 
                 writer.flush();
                 fos.getFD().sync();
-            } finally {
-                writer.close();
             }
         }
     }
@@ -76,7 +90,7 @@ public class CheckpointFile<T> {
      */
     public List<T> read() throws IOException {
         final ArrayList<T> result = new ArrayList<>();
-        synchronized(this) {
+        synchronized (this) {
             final File file = new File(this.path);
             if (!file.exists()) {
                 return result;
