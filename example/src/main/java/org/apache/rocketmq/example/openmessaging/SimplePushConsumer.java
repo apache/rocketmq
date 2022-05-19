ce6fbdd5c8160f767725809acb5c32d7e71ce18e
lixiaoshuang <644968328@qq.com>
2022-05-19 23:35:22 +0800

    Optimized openmessaging example code

--- example/src/main/java/org/apache/rocketmq/example/openmessaging/SimplePushConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/openmessaging/SimplePushConsumer.java
@@ -24,9 +24,15 @@ import io.openmessaging.consumer.MessageListener;
 import io.openmessaging.consumer.PushConsumer;
 
 public class SimplePushConsumer {
+
+    public static final String URL = "oms:rocketmq://localhost:9876/default:default";
+    public static final String QUEUE = "OMS_HELLO_TOPIC";
+
     public static void main(String[] args) {
+        // You need to set the environment variable OMS_RMQ_DIRECT_NAME_SRV=true
+
         final MessagingAccessPoint messagingAccessPoint = OMS
-            .getMessagingAccessPoint("oms:rocketmq://localhost:9876/default:default");
+            .getMessagingAccessPoint(URL);
 
         final PushConsumer consumer = messagingAccessPoint.
             createPushConsumer(OMS.newKeyValue().put(OMSBuiltinKeys.CONSUMER_ID, "OMS_CONSUMER"));
@@ -34,20 +40,14 @@ public class SimplePushConsumer {
         messagingAccessPoint.startup();
         System.out.printf("MessagingAccessPoint startup OK%n");
 
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                consumer.shutdown();
-                messagingAccessPoint.shutdown();
-            }
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            consumer.shutdown();
+            messagingAccessPoint.shutdown();
         }));
 
-        consumer.attachQueue("OMS_HELLO_TOPIC", new MessageListener() {
-            @Override
-            public void onReceived(Message message, Context context) {
-                System.out.printf("Received one message: %s%n", message.sysHeaders().getString(Message.BuiltinKeys.MESSAGE_ID));
-                context.ack();
-            }
+        consumer.attachQueue(QUEUE, (message, context) -> {
+            System.out.printf("Received one message: %s%n", message.sysHeaders().getString(Message.BuiltinKeys.MESSAGE_ID));
+            context.ack();
         });
 
         consumer.startup();
