2b192a0eb72dc0a9a94aecad25974b88ff742e92
lixiaoshuang <644968328@qq.com>
2022-05-23 20:57:15 +0800

    update annotation

--- example/src/main/java/org/apache/rocketmq/example/schedule/ScheduledMessageConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/schedule/ScheduledMessageConsumer.java
@@ -31,7 +31,7 @@ public class ScheduledMessageConsumer {
         // Instantiate message consumer
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
 
-        // If the debugging source code can open comments, you need to set the namesrvAddr to the address of the local namesrvAddr
+        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
 //        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
 
         // Subscribe topics
