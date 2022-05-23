0c2d0de3f71d0608f1bc72b83c1b2a2de45998b9
lixiaoshuang <644968328@qq.com>
2022-05-23 21:01:25 +0800

    update annotation

--- example/src/main/java/org/apache/rocketmq/example/ordermessage/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/ordermessage/Consumer.java
@@ -36,7 +36,7 @@ public class Consumer {
     public static void main(String[] args) throws MQClientException {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
 
-        // If the debugging source code can open comments, you need to set the namesrvAddr to the address of the local namesrvAddr
+        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
 //        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 
