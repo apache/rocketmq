5eaeeabf51618d7f31e19816017eaafa2f287a2f
hzh0425 <642256541@qq.com>
2022-06-12 16:01:08 +0800

    [Summer of code] Let broker register to controller again when master not existed. (#4450)
    * register to controller if master is null
    
    * stop electmaster when the old master is active
    
    * review
    
    * fix test bugs for brokerAlivePredicate

--- broker/src/main/java/org/apache/rocketmq/broker/hacontroller/ReplicasManager.java
+++ broker/src/main/java/org/apache/rocketmq/broker/hacontroller/ReplicasManager.java
@@ -300,18 +300,23 @@ public class ReplicasManager {
                 final long brokerId = info.getBrokerId();
                 synchronized (this) {
                     // Check if master changed
-                    if (StringUtils.isNoneEmpty(newMasterAddress) && newMasterEpoch > this.masterEpoch) {
-                        if (StringUtils.equals(newMasterAddress, this.localAddress)) {
-                            changeToMaster(newMasterEpoch, syncStateSet.getSyncStateSetEpoch());
-                        } else {
-                            if (brokerId > 0) {
-                                changeToSlave(newMasterAddress, newMasterEpoch, brokerId);
-                            } else if (brokerId < 0) {
-                                // If the brokerId is no existed, we should try register again.
-                                registerBrokerToController();
+                    if (newMasterEpoch > this.masterEpoch) {
+                        if (StringUtils.isNoneEmpty(newMasterAddress)) {
+                            if (StringUtils.equals(newMasterAddress, this.localAddress)) {
+                                changeToMaster(newMasterEpoch, syncStateSet.getSyncStateSetEpoch());
+                            } else {
+                                if (brokerId > 0) {
+                                    changeToSlave(newMasterAddress, newMasterEpoch, brokerId);
+                                } else if (brokerId < 0) {
+                                    // If the brokerId is no existed, we should try register again.
+                                    registerBrokerToController();
+                                }
                             }
+                        } else {
+                            // In this case, the master in controller is null, try register to controller again, this will trigger the electMasterEvent in controller.
+                            registerBrokerToController();
                         }
-                    } else {
+                    } else if (newMasterEpoch == this.masterEpoch) {
                         // Check if sync state set changed
                         if (isMasterState()) {
                             changeSyncStateSet(syncStateSet.getSyncStateSet(), syncStateSet.getSyncStateSetEpoch());
