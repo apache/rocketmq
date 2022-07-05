5668b579db9de0126f43050ec39b6d54059f7256
diaoq <diaoqiang2006@163.com>
2022-07-05 14:01:16 +0800

    PushConsumer example code remove UnusedImports

--- example/src/main/java/org/apache/rocketmq/example/broadcast/PushConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/broadcast/PushConsumer.java
@@ -16,14 +16,11 @@
  */
 package org.apache.rocketmq.example.broadcast;
 
-import java.util.List;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
-import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 public class PushConsumer {
