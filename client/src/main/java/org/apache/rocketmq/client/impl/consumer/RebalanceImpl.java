e45166c2fe5f3c6741c48426672dd9926afa35d3
灼华 <1015773611@qq.com>
2022-07-14 17:43:38 +0800

    Fix conflicts

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalanceImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalanceImpl.java
@@ -518,12 +518,6 @@ public abstract class RebalanceImpl {
         List<PullRequest> pullRequestList = new ArrayList<PullRequest>();
         for (MessageQueue mq : mqSet) {
             if (!this.processQueueTable.containsKey(mq)) {
-                if (isOrder && !this.lock(mq)) {
-                    log.warn("doRebalance, {}, add a new mq failed, {}, because lock failed", consumerGroup, mq);
-                    allMQLocked = false;
-                    continue;
-                }
-
                 this.removeDirtyOffset(mq);
                 ProcessQueue pq = createProcessQueue(topic);
                 pq.setLocked(true);
@@ -533,6 +527,11 @@ public abstract class RebalanceImpl {
                     if (pre != null) {
                         log.info("doRebalance, {}, mq already exists, {}", consumerGroup, mq);
                     } else {
+                        if (isOrder && !this.lock(mq)) {
+                            log.warn("doRebalance, {}, add a new mq failed, {}, because lock failed", consumerGroup, mq);
+                            allMQLocked = false;
+                            continue;
+                        }
                         log.info("doRebalance, {}, add a new mq, {}", consumerGroup, mq);
                         PullRequest pullRequest = new PullRequest();
                         pullRequest.setConsumerGroup(consumerGroup);
