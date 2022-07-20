a87d20cb4a37363f70c724598f213560f4ab2b39
Oliver <wqdyxnbd@163.com>
2022-07-20 13:51:45 +0800

    [Issue #4585] Add unit test

--- common/src/main/java/org/apache/rocketmq/common/PlainAccessConfig.java
+++ common/src/main/java/org/apache/rocketmq/common/PlainAccessConfig.java
@@ -99,4 +99,18 @@ public class PlainAccessConfig {
     public void setGroupPerms(List<String> groupPerms) {
         this.groupPerms = groupPerms;
     }
+
+    @Override
+    public String toString() {
+        return "PlainAccessConfig{" +
+            "accessKey='" + accessKey + '\'' +
+            ", secretKey='" + secretKey + '\'' +
+            ", whiteRemoteAddress='" + whiteRemoteAddress + '\'' +
+            ", admin=" + admin +
+            ", defaultTopicPerm='" + defaultTopicPerm + '\'' +
+            ", defaultGroupPerm='" + defaultGroupPerm + '\'' +
+            ", topicPerms=" + topicPerms +
+            ", groupPerms=" + groupPerms +
+            '}';
+    }
 }
