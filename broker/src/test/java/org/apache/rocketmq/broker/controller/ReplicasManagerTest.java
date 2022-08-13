d58951d70cae7e37e1f9012be01f3392634007e3
TheR1sing3un <87409330+TheR1sing3un@users.noreply.github.com>
2022-08-13 22:35:09 +0800

    fix wrong method call in ReplicasManagerTest#before
    1.fix wrong method call in ReplicasManagerTest#before

--- broker/src/test/java/org/apache/rocketmq/broker/controller/ReplicasManagerTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/controller/ReplicasManagerTest.java
@@ -121,7 +121,7 @@ public class ReplicasManagerTest {
         when(brokerController.getBrokerOuterAPI()).thenReturn(brokerOuterAPI);
         when(brokerController.getBrokerAddr()).thenReturn(OLD_MASTER_ADDRESS);
         when(brokerOuterAPI.getControllerMetaData(any())).thenReturn(getMetaDataResponseHeader);
-        when(brokerOuterAPI.registerBrokerToController(any(), any(), any(), any())).thenReturn(registerBrokerToControllerResponseHeader);
+        when(brokerOuterAPI.registerBrokerToController(any(), any(), any(), any(), any())).thenReturn(registerBrokerToControllerResponseHeader);
         when(brokerOuterAPI.getReplicaInfo(any(), any(), any())).thenReturn(result);
         replicasManager = new ReplicasManager(brokerController);
         autoSwitchHAService.init(defaultMessageStore);
