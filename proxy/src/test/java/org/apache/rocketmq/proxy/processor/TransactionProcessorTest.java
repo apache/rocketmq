e3baf8fb39b3be3c6a79c2da43c6333521f103be
RongtongJin <jinrongtong16@mails.ucas.ac.cn>
2022-09-02 17:31:37 +0800

    Remove useless import in proxy module

--- proxy/src/test/java/org/apache/rocketmq/proxy/processor/TransactionProcessorTest.java
+++ proxy/src/test/java/org/apache/rocketmq/proxy/processor/TransactionProcessorTest.java
@@ -30,7 +30,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
