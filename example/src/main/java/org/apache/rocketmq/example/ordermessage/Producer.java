4b02d52b87c61f3cbdb75213886ab1cff1074051
alexcao <35793943+alexcao2018@users.noreply.github.com>
2022-09-22 09:10:21 +0800

    Update Producer.java
    fixed for compilation exception.

--- example/src/main/java/org/apache/rocketmq/example/ordermessage/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/ordermessage/Producer.java
@@ -22,7 +22,6 @@ import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageQueue;
-import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 import java.util.List;
 
@@ -37,7 +36,7 @@ public class Producer {
                 int orderId = i % 10;
                 Message msg =
                     new Message("TopicTestjjj", tags[i % tags.length], "KEY" + i,
-                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                        ("Hello RocketMQ " + i).getBytes(StandardCharsets.UTF_8));
                 SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
                     @Override
                     public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
