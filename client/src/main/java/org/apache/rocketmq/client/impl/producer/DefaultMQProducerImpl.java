58bd4a75824f03f447da94edcbecea456e558024
Aaron Ai <yangkun.ayk@gmail.com>
2022-10-08 11:43:44 +0800

    Update client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultMQProducerImpl.java
    Co-authored-by: lizhimins <707364882@qq.com>

--- client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultMQProducerImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultMQProducerImpl.java
@@ -349,7 +349,7 @@ public class DefaultMQProducerImpl implements MQProducerInner {
                         if (transactionCheckListener != null) {
                             localTransactionState = transactionCheckListener.checkLocalTransactionState(message);
                         } else {
-                            log.warn("transactionListener is null, producerGroup={}", group);
+                            log.warn("TransactionCheckListener is null, used new check API, producerGroup={}", group);
                             localTransactionState = transactionListener.checkLocalTransaction(message);
                         }
                     } catch (Throwable e) {
