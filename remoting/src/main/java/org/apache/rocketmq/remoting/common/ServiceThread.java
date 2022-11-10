767d05075a3c11b535c7e0e83e5a65112287f298
SSpirits <admin@lv5.moe>
2022-11-10 21:35:43 +0800

    revert delete class ServiceThread because of introduced by dledger

---
+++ remoting/src/main/java/org/apache/rocketmq/remoting/common/ServiceThread.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.remoting.common;
+
+
+import org.apache.rocketmq.logging.InternalLogger;
+import org.apache.rocketmq.logging.InternalLoggerFactory;
+
+/**
+ * Base class for background thread
+ */
+public abstract class ServiceThread implements Runnable {
+    private static final InternalLogger log = InternalLoggerFactory.getLogger(RemotingHelper.ROCKETMQ_REMOTING);
+
+    private static final long JOIN_TIME = 90 * 1000;
+    protected final Thread thread;
+    protected volatile boolean hasNotified = false;
+    protected volatile boolean stopped = false;
+
+    public ServiceThread() {
+        this.thread = new Thread(this, this.getServiceName());
+    }
+
+    public abstract String getServiceName();
+
+    public void start() {
+        this.thread.start();
+    }
+
+    public void shutdown() {
+        this.shutdown(false);
+    }
+
+    public void shutdown(final boolean interrupt) {
+        this.stopped = true;
+        log.info("shutdown thread " + this.getServiceName() + " interrupt " + interrupt);
+        synchronized (this) {
+            if (!this.hasNotified) {
+                this.hasNotified = true;
+                this.notify();
+            }
+        }
+
+        try {
+            if (interrupt) {
+                this.thread.interrupt();
+            }
+
+            long beginTime = System.currentTimeMillis();
+            this.thread.join(this.getJointime());
+            long elapsedTime = System.currentTimeMillis() - beginTime;
+            log.info("join thread " + this.getServiceName() + " elapsed time(ms) " + elapsedTime + " "
+                + this.getJointime());
+        } catch (InterruptedException e) {
+            log.error("Interrupted", e);
+        }
+    }
+
+    public long getJointime() {
+        return JOIN_TIME;
+    }
+
+    public boolean isStopped() {
+        return stopped;
+    }
+}
