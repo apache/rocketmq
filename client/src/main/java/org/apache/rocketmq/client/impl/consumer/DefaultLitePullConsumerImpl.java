80b63ab1d7c0dc04e60f84c6b2586ec0d4a942bc
rongtong <jinrongtong5@163.com>
2022-12-02 16:27:05 +0800

    Persist consumer offset should not be disabled when auto commit is turned off (#5635)
    Co-authored-by: SSpirits <admin@lv5.moe>

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultLitePullConsumerImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultLitePullConsumerImpl.java
@@ -1111,10 +1111,6 @@ public class DefaultLitePullConsumerImpl implements MQConsumerInner {
 
     @Override
     public void persistConsumerOffset() {
-        // this method will be called by MQInstance schedule task, commit offset depends on autocommit config
-        if (!this.defaultLitePullConsumer.isAutoCommit()) {
-            return;
-        }
         try {
             checkServiceState();
             Set<MessageQueue> mqs = new HashSet<>();
