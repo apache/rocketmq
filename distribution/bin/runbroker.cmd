178553eeac35f271c2e5fa7d0f6fec7a04cf8898
mxsm <ljbmxsm@gmail.com>
2022-12-12 09:15:33 +0800

    [ISSUE #5673] Add the windows platform script as the controller quick start (#5674)

--- distribution/bin/runbroker.cmd
+++ distribution/bin/runbroker.cmd
@@ -37,6 +37,6 @@ set "JAVA_OPT=%JAVA_OPT% -XX:+AlwaysPreTouch"
 set "JAVA_OPT=%JAVA_OPT% -XX:MaxDirectMemorySize=15g"
 set "JAVA_OPT=%JAVA_OPT% -XX:-UseLargePages -XX:-UseBiasedLocking"
 set "JAVA_OPT=%JAVA_OPT% -Drocketmq.client.logUseSlf4j=true"
-set "JAVA_OPT=%JAVA_OPT% -cp %CLASSPATH%"
+set "JAVA_OPT=%JAVA_OPT% %JAVA_OPT_EXT% -cp %CLASSPATH%"
 
 "%JAVA%" %JAVA_OPT% %*
