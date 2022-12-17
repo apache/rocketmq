5e8ccd1ec07670558d52ae5541f3de5d476705bf
RongtongJin <jinrongtong16@mails.ucas.ac.cn>
2022-12-17 17:23:01 +0800

    Remove the useless import

--- store/src/main/java/org/apache/rocketmq/store/TransientStorePool.java
+++ store/src/main/java/org/apache/rocketmq/store/TransientStorePool.java
@@ -24,7 +24,6 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import org.apache.rocketmq.common.constant.LoggerName;
 import org.apache.rocketmq.logging.org.slf4j.Logger;
 import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
-import org.apache.rocketmq.store.config.MessageStoreConfig;
 import org.apache.rocketmq.store.util.LibC;
 import sun.nio.ch.DirectBuffer;
 
