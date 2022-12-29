e98653472868555e3b6235861eb7c24fe61a50d3
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2022-12-29 15:17:58 +0800

    [ISSUE #5699] remove duplicate code definition of UtilAll.isBlank (#5700)
    * [ISSUE #5699] remove duplicate code definition of UtilAll.isBlank
    
    * [ISSUE #5699] remove duplicate code definition of UtilAll.isBlank

--- remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RocketMQSerializable.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RocketMQSerializable.java
@@ -241,17 +241,4 @@ public class RocketMQSerializable {
         }
         return map;
     }
-
-    public static boolean isBlank(String str) {
-        int strLen;
-        if (str == null || (strLen = str.length()) == 0) {
-            return true;
-        }
-        for (int i = 0; i < strLen; i++) {
-            if (!Character.isWhitespace(str.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
