9384f43d21f1957a4985e40a4524c7ed6b6223b9
hzh0425 <642256541@qq.com>
2023-01-02 19:36:20 +0800

    Add empty snapshot check test

--- controller/src/test/java/org/apache/rocketmq/controller/impl/controller/impl/manager/ReplicasInfoManagerTest.java
+++ controller/src/test/java/org/apache/rocketmq/controller/impl/controller/impl/manager/ReplicasInfoManagerTest.java
@@ -49,6 +49,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class ReplicasInfoManagerTest {
@@ -335,6 +336,14 @@ public class ReplicasInfoManagerTest {
 
     }
 
+    @Test
+    public void testEmptySnapshot() {
+        byte[] metadata = this.replicasInfoManager.encodeMetadata();
+        assertNotNull(metadata);
+        assertTrue(metadata.length > 0);
+
+        assertTrue(this.replicasInfoManager.loadMetadata(metadata));
+    }
 
     @Test
     public void testSnapshot() {
