78a715e1e93a81bff444da50f6c4b4fee52d0b4d
mahaitao617 <mahaitao617@mahaitao617deMacBook-Pro.local>
2023-01-17 19:54:12 +0800

    feat:fix

--- example/src/main/java/org/apache/rocketmq/example/simple/PopConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/simple/PopConsumer.java
@@ -37,51 +37,16 @@ public class PopConsumer {
     public static final String TOPIC = "TopicTest";
 
     public static void main(String[] args) throws Exception {
-
         switchPop();
-        /*
-         * Instantiate with specified consumer group name.
-         */
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
-
-        /*
-         * Specify name server addresses.
-         * <p/>
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         * consumer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
-         */
-        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-//        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-
-        /*
-         * Specify where to start in case the specific consumer group is a brand-new one.
-         */
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-
-        /*
-         * Subscribe one more topic to consume.
-         */
         consumer.subscribe(TOPIC, "*");
-//        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-        /*
-         *  Register callback to execute on arrival of messages fetched from brokers.
-         */
         consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
             System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
         consumer.setClientRebalance(false);
-
-        /*
-         *  Launch the consumer instance.
-         */
         consumer.start();
-
         System.out.printf("Consumer Started.%n");
     }
 
