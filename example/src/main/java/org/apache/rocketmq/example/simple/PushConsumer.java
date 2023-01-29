0b0c39c9b054b2f1b3ae104e7415d71ccffb4a0f
mahaitao617 <mahaitao617@mahaitao617deMacBook-Pro.local>
2023-01-29 10:04:19 +0800

    feat:fix

--- example/src/main/java/org/apache/rocketmq/example/simple/PushConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/simple/PushConsumer.java
@@ -40,6 +40,7 @@ public class PushConsumer {
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
         //wrong time format 2017_0422_221800
         consumer.setConsumeTimestamp("20181109221800");
+        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
