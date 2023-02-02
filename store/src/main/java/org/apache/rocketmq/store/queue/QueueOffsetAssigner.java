7ab3080f5a19926de3b974fb506eb84562f4a80e
pingww <pingw002@gmail.com>
2023-02-02 18:01:47 +0800

    [ISSUE #5965] Fix lmqTopicQueueTable initialization

--- store/src/main/java/org/apache/rocketmq/store/queue/QueueOffsetAssigner.java
+++ store/src/main/java/org/apache/rocketmq/store/queue/QueueOffsetAssigner.java
@@ -17,7 +17,6 @@
 
 package org.apache.rocketmq.store.queue;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
