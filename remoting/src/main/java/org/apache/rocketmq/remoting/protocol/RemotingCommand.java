b4638b20a19f17b95d7adfc164410d36e267a146
weihu <weihubeats@163.com>
2023-02-06 15:15:37 +0800

    Formatting Code

--- remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingCommand.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/protocol/RemotingCommand.java
@@ -44,6 +44,7 @@ import org.apache.rocketmq.remoting.exception.RemotingCommandException;
 public class RemotingCommand {
     public static final String SERIALIZE_TYPE_PROPERTY = "rocketmq.serialize.type";
     public static final String SERIALIZE_TYPE_ENV = "ROCKETMQ_SERIALIZE_TYPE";
+    
     public static final String REMOTING_VERSION_KEY = "rocketmq.remoting.version";
     static final Logger log = LoggerFactory.getLogger(RemotingHelper.ROCKETMQ_REMOTING);
     private static final int RPC_TYPE = 0; // 0, REQUEST_COMMAND
