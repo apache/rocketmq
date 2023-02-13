081f20bb9f587f9f8aaf021b4e206ea8033ccd3c
nowinkey <nowinkey@tom.com>
2023-02-13 21:04:07 +0800

    Put batchDispatchRequestThreadPoolNums config to MessageStoreConfig.java

--- common/src/main/java/org/apache/rocketmq/common/BrokerConfig.java
+++ common/src/main/java/org/apache/rocketmq/common/BrokerConfig.java
@@ -392,8 +392,6 @@ public class BrokerConfig extends BrokerIdentity {
     private long channelExpiredTimeout = 1000 * 120;
     private long subscriptionExpiredTimeout = 1000 * 60 * 10;
 
-    private int batchDispatchRequestThreadPoolNums = 16;
-
     /**
      * Estimate accumulation or not when subscription filter type is tag and is not SUB_ALL.
      */
@@ -1638,13 +1636,5 @@ public class BrokerConfig extends BrokerIdentity {
 
     public void setEstimateAccumulation(boolean estimateAccumulation) {
         this.estimateAccumulation = estimateAccumulation;
-    }
-
-    public int getBatchDispatchRequestThreadPoolNums() {
-        return batchDispatchRequestThreadPoolNums;
-    }
-
-    public void setBatchDispatchRequestThreadPoolNums(int batchDispatchRequestThreadPoolNums) {
-        this.batchDispatchRequestThreadPoolNums = batchDispatchRequestThreadPoolNums;
     }
 }
