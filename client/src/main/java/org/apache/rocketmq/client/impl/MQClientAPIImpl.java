7b47e19b13bf64fa7b6a3812f960ecf0236fa3b8
zhouyunpeng <2474138779@qq.com>
2023-02-25 19:55:35 +0800

    remove invalid null check code

--- client/src/main/java/org/apache/rocketmq/client/impl/MQClientAPIImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/MQClientAPIImpl.java
@@ -897,7 +897,6 @@ public class MQClientAPIImpl implements NameServerUpdateCallback {
                         } else {
                             ackResult.setStatus(AckStatus.NO_EXIST);
                         }
-                        assert ackResult != null;
                         ackCallback.onSuccess(ackResult);
                     } catch (Exception e) {
                         ackCallback.onException(e);
@@ -943,7 +942,6 @@ public class MQClientAPIImpl implements NameServerUpdateCallback {
                         } else {
                             ackResult.setStatus(AckStatus.NO_EXIST);
                         }
-                        assert ackResult != null;
                         ackCallback.onSuccess(ackResult);
                     } catch (Exception e) {
                         ackCallback.onException(e);
