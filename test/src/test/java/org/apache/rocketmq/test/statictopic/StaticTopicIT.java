10692d2d4d6fb32f7a081b2e3f4a8d0f80353609
zhouyunpeng <2474138779@qq.com>
2023-02-28 00:41:11 +0800

    modify Collections.sort usage

--- test/src/test/java/org/apache/rocketmq/test/statictopic/StaticTopicIT.java
+++ test/src/test/java/org/apache/rocketmq/test/statictopic/StaticTopicIT.java
@@ -21,8 +21,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
