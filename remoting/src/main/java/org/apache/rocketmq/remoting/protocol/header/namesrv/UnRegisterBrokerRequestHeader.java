5cb4c7674be857a09d9e3b3904dd14078d1102a0
xdshen <xdshent@gmail.com>
2023-02-27 17:02:45 +0800

    style: code style fix

--- remoting/src/main/java/org/apache/rocketmq/remoting/protocol/header/namesrv/UnRegisterBrokerRequestHeader.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/protocol/header/namesrv/UnRegisterBrokerRequestHeader.java
@@ -74,10 +74,10 @@ public class UnRegisterBrokerRequestHeader implements CommandCustomHeader {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o){
+        if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()){
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         UnRegisterBrokerRequestHeader that = (UnRegisterBrokerRequestHeader) o;
