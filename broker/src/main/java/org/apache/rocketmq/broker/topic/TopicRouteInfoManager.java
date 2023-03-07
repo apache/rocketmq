ec8cab57f37b4a248fb4e57c530fd8331d78ff27
zhouyunpeng <2474138779@qq.com>
2023-03-07 22:15:44 +0800

    remove useless import

--- broker/src/main/java/org/apache/rocketmq/broker/topic/TopicRouteInfoManager.java
+++ broker/src/main/java/org/apache/rocketmq/broker/topic/TopicRouteInfoManager.java
@@ -25,7 +25,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
@@ -61,7 +61,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
