6c5f62be451b38ce5808a4be9228f1ecf833db4a
WSYwsy22 <2914883754@qq.com>
2023-03-13 15:46:42 +0800

    fix bazel test

--- proxy/src/test/java/org/apache/rocketmq/proxy/config/ConfigurationManagerTest.java
+++ proxy/src/test/java/org/apache/rocketmq/proxy/config/ConfigurationManagerTest.java
@@ -25,24 +25,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ConfigurationManagerTest extends InitConfigTest {
 
     @Test
-    public void testInitEnv() {
-        // configure proxy home by system env.
-        assertThat(ConfigurationManager.getProxyHome()).isEqualTo(mockProxyHome);
-    }
-
-    @Test
     public void testIntConfig() {
         assertThat(ConfigurationManager.getProxyConfig()).isNotNull();
         assertThat(ConfigurationManager.getProxyConfig().getProxyMode()).isEqualToIgnoringCase(ProxyMode.CLUSTER.toString());
 
         String brokerConfig = ConfigurationManager.getProxyConfig().getBrokerConfigPath();
         assertThat(brokerConfig).isEqualTo(ConfigurationManager.getProxyHome() + "/conf/broker.conf");
-    }
-
-    @Test
-    public void testGetProxyHome() {
-        // test configured proxy home
-        assertThat(ConfigurationManager.getProxyHome()).isEqualTo(mockProxyHome);
     }
 
     @Test
