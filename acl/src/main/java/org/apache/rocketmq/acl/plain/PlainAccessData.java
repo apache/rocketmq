d758d33e112fa21c9db40db2b9f0fd665f50de06
yuz10 <845238369@qq.com>
2023-03-14 14:43:29 +0800

    implements Serializable

--- acl/src/main/java/org/apache/rocketmq/acl/plain/PlainAccessData.java
+++ acl/src/main/java/org/apache/rocketmq/acl/plain/PlainAccessData.java
@@ -18,13 +18,17 @@ package org.apache.rocketmq.acl.plain;
 
 import org.apache.rocketmq.common.PlainAccessConfig;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class PlainAccessData {
-    List<String> globalWhiteRemoteAddresses = new ArrayList<>();
-    List<PlainAccessConfig> accounts = new ArrayList<>();
-    List<DataVersion> dataVersion = new ArrayList<>();
+public class PlainAccessData implements Serializable {
+    private static final long serialVersionUID = -7971775135605117152L;
+
+    private List<String> globalWhiteRemoteAddresses = new ArrayList<>();
+    private List<PlainAccessConfig> accounts = new ArrayList<>();
+    private List<DataVersion> dataVersion = new ArrayList<>();
 
     public List<String> getGlobalWhiteRemoteAddresses() {
         return globalWhiteRemoteAddresses;
@@ -50,7 +54,8 @@ public class PlainAccessData {
         this.dataVersion = dataVersion;
     }
 
-    public static class DataVersion {
+    public static class DataVersion implements Serializable {
+        private static final long serialVersionUID = 6437361970079056954L;
         private long timestamp;
         private long counter;
 
@@ -69,5 +74,31 @@ public class PlainAccessData {
         public void setCounter(long counter) {
             this.counter = counter;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            DataVersion that = (DataVersion) o;
+            return timestamp == that.timestamp && counter == that.counter;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(timestamp, counter);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlainAccessData that = (PlainAccessData) o;
+        return Objects.equals(globalWhiteRemoteAddresses, that.globalWhiteRemoteAddresses) && Objects.equals(accounts, that.accounts) && Objects.equals(dataVersion, that.dataVersion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(globalWhiteRemoteAddresses, accounts, dataVersion);
     }
 }
