1b2a7b7b358d289b448e898457743477550e1d13
zfj <fujian.zfj@alibaba-inc.com>
2023-03-27 10:14:03 +0800

    quickstart consumer test revert

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -67,7 +67,7 @@ public class Consumer {
          */
         consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
             System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
-            return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
         /*
