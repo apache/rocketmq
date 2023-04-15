ede0da8878b449bdef69797c0f2552d95bb50f26
Abhijeet Mishra <abhijeetmishra498@gmail.com>
2023-04-15 15:51:03 +0530

    [ISSUE #6525]Make ConsumeQueueInterface extends from FileQueueLifeCycle (#6534)
    * [#6525] Make ConsumeQueueInterface extends from FileQueueLifeCycle
    
    * [apache#6525] forced typecast is removed
    
    * [#6525] remove all the forced convert

--- store/src/main/java/org/apache/rocketmq/store/queue/ConsumeQueueStore.java
+++ store/src/main/java/org/apache/rocketmq/store/queue/ConsumeQueueStore.java
@@ -79,7 +79,7 @@ public class ConsumeQueueStore {
     }
 
     private FileQueueLifeCycle getLifeCycle(String topic, int queueId) {
-        return (FileQueueLifeCycle) findOrCreateConsumeQueue(topic, queueId);
+        return findOrCreateConsumeQueue(topic, queueId);
     }
 
     public long rollNextFile(ConsumeQueueInterface consumeQueue, final long offset) {
@@ -220,7 +220,7 @@ public class ConsumeQueueStore {
                     FutureTask<Boolean> futureTask = new FutureTask<>(() -> {
                         boolean ret = true;
                         try {
-                            ((FileQueueLifeCycle) logic).recover();
+                            logic.recover();
                         } catch (Throwable e) {
                             ret = false;
                             log.error("Exception occurs while recover consume queue concurrently, " +
