b22a90b793b0c25a648ab083f9e3a88abba7c153
TheR1sing3un <ther1sing3un@163.com>
2023-04-17 23:16:56 +0800

    fix(tieredstorage): fix unmatched config attributes in brokerS3.conf
    1. fix unmatched config attributes in brokerS3.conf

--- tieredstore/src/main/java/org/apache/rocketmq/tieredstore/common/TieredMessageStoreConfig.java
+++ tieredstore/src/main/java/org/apache/rocketmq/tieredstore/common/TieredMessageStoreConfig.java
@@ -117,20 +117,6 @@ public class TieredMessageStoreConfig {
 
     private String tieredStoreFilepath = "";
 
-    // only for oss storage provider
-//    private String ossEndpoint = "";
-//    private String ossBucket = "";
-//    private String ossAccessKey = "";
-//    private String ossSecretKey = "";
-//
-//    private String s3Region = "";
-//
-//    private String s3Bucket = "";
-//
-//    private String s3AccessKey = "";
-//
-//    private String s3SecretKey = "";
-
     private String objectStoreRegion = "";
 
     private String objectStoreBucket = "";
