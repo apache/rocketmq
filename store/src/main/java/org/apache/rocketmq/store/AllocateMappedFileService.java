39e9c5d1b329ba210bbebe6fc43e352584f72e1b
winglechen <winglechen@gmail.com>
2023-08-01 14:51:52 +0800

    refactor: simplify AllocateMappedFileService.putRequestAndReturnMappedFile

--- store/src/main/java/org/apache/rocketmq/store/AllocateMappedFileService.java
+++ store/src/main/java/org/apache/rocketmq/store/AllocateMappedFileService.java
@@ -52,12 +52,14 @@ public class AllocateMappedFileService extends ServiceThread {
 
     public MappedFile putRequestAndReturnMappedFile(String nextFilePath, String nextNextFilePath, int fileSize) {
         int canSubmitRequests = 2;
-        if (this.messageStore.isTransientStorePoolEnable()) {
-            if (this.messageStore.getMessageStoreConfig().isFastFailIfNoBufferInStorePool()
-                && BrokerRole.SLAVE != this.messageStore.getMessageStoreConfig().getBrokerRole()) { //if broker is slave, don't fast fail even no buffer in pool
-                canSubmitRequests = this.messageStore.getTransientStorePool().availableBufferNums() - this.requestQueue.size();
-            }
-        }
+
+       if (this.messageStore.isTransientStorePoolEnable()
+               && this.messageStore.getMessageStoreConfig().isFastFailIfNoBufferInStorePool()
+               && BrokerRole.SLAVE != this.messageStore.getMessageStoreConfig().getBrokerRole()) {
+
+           //if broker is slave, don't fast fail even no buffer in pool
+           canSubmitRequests = this.messageStore.getTransientStorePool().availableBufferNums() - this.requestQueue.size();
+       }
 
         AllocateRequest nextReq = new AllocateRequest(nextFilePath, fileSize);
         boolean nextPutOK = this.requestTable.putIfAbsent(nextFilePath, nextReq) == null;
