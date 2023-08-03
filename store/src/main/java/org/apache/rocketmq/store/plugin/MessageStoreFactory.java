c72a4561577873649d1a6162879617ae15d396cd
winglechen <winglechen@gmail.com>
2023-08-03 14:06:46 +0800

    refactor: simplify MessageStoreFactory.build

--- store/src/main/java/org/apache/rocketmq/store/plugin/MessageStoreFactory.java
+++ store/src/main/java/org/apache/rocketmq/store/plugin/MessageStoreFactory.java
@@ -22,22 +22,22 @@ import java.lang.reflect.Constructor;
 import org.apache.rocketmq.store.MessageStore;
 
 public final class MessageStoreFactory {
-    public static MessageStore build(MessageStorePluginContext context,
-        MessageStore messageStore) throws IOException {
+    public static MessageStore build(MessageStorePluginContext context, MessageStore messageStore) throws IOException {
         String plugin = context.getBrokerConfig().getMessageStorePlugIn();
-        if (plugin != null && plugin.trim().length() != 0) {
-            String[] pluginClasses = plugin.split(",");
-            for (int i = pluginClasses.length - 1; i >= 0; --i) {
-                String pluginClass = pluginClasses[i];
-                try {
-                    @SuppressWarnings("unchecked")
-                    Class<AbstractPluginMessageStore> clazz = (Class<AbstractPluginMessageStore>) Class.forName(pluginClass);
-                    Constructor<AbstractPluginMessageStore> construct = clazz.getConstructor(MessageStorePluginContext.class, MessageStore.class);
-                    AbstractPluginMessageStore pluginMessageStore = construct.newInstance(context, messageStore);
-                    messageStore = pluginMessageStore;
-                } catch (Throwable e) {
-                    throw new RuntimeException("Initialize plugin's class: " + pluginClass + " not found!", e);
-                }
+        if (plugin == null || plugin.trim().length() == 0) {
+            return messageStore;
+        }
+
+        String[] pluginClasses = plugin.split(",");
+        for (int i = pluginClasses.length - 1; i >= 0; --i) {
+            String pluginClass = pluginClasses[i];
+            try {
+                @SuppressWarnings("unchecked")
+                Class<AbstractPluginMessageStore> clazz = (Class<AbstractPluginMessageStore>) Class.forName(pluginClass);
+                Constructor<AbstractPluginMessageStore> construct = clazz.getConstructor(MessageStorePluginContext.class, MessageStore.class);
+                messageStore = construct.newInstance(context, messageStore);
+            } catch (Throwable e) {
+                throw new RuntimeException("Initialize plugin's class: " + pluginClass + " not found!", e);
             }
         }
         return messageStore;
