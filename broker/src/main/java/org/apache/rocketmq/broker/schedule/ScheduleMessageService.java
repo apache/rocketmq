3a6ef0400c8f3dc420b8781c619e66d47d1c4336
fujian-zfj <2573259572@qq.com>
2023-08-05 00:32:11 +0800

    [ISSUE #7064] [RIP-66-1] Support KV(RocksDB) Storage for Metadata (#7092)
    * typo int readme[ecosystem]
    
    * rocksdb metadata
    
    * add unit test
    
    * fix testOffsetPersistInMemory
    
    * fix unit test
    
    * fix unit test
    
    * remove unused import
    
    * move RocksDBOffsetSerialize to broker moudle
    
    * Fix bazel build scripts
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    
    * Flag QueryMsgByKeyIT as flaky as it fails at frequency: 5 out of 32
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    
    * change public to private of some inner method
    
    ---------
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    Co-authored-by: Li Zhanhui <lizhanhui@gmail.com>

--- broker/src/main/java/org/apache/rocketmq/broker/schedule/ScheduleMessageService.java
+++ broker/src/main/java/org/apache/rocketmq/broker/schedule/ScheduleMessageService.java
@@ -92,7 +92,7 @@ public class ScheduleMessageService extends ConfigManager {
         this.brokerController = brokerController;
         this.enableAsyncDeliver = brokerController.getMessageStoreConfig().isEnableScheduleAsyncDeliver();
         scheduledPersistService = new ScheduledThreadPoolExecutor(1,
-                new ThreadFactoryImpl("ScheduleMessageServicePersistThread", true, brokerController.getBrokerConfig()));
+            new ThreadFactoryImpl("ScheduleMessageServicePersistThread", true, brokerController.getBrokerConfig()));
     }
 
     public static int queueId2DelayLevel(final int queueId) {
@@ -169,7 +169,7 @@ public class ScheduleMessageService extends ConfigManager {
         ThreadUtils.shutdown(scheduledPersistService);
     }
 
-    public void stop() {
+    public boolean stop() {
         if (this.started.compareAndSet(true, false) && null != this.deliverExecutorService) {
             this.deliverExecutorService.shutdown();
             try {
@@ -193,6 +193,7 @@ public class ScheduleMessageService extends ConfigManager {
 
             this.persist();
         }
+        return true;
     }
 
     public boolean isStarted() {
