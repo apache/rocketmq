b21fe06e3bcc9dad238c80d4cbf3a5646767f13d
ShuangxiDing <dingshuangxi888@gmail.com>
2023-08-10 15:42:11 +0800

    Add a switch for MIXED message type when creating a Topic in the Broker.

--- common/src/main/java/org/apache/rocketmq/common/BrokerConfig.java
+++ common/src/main/java/org/apache/rocketmq/common/BrokerConfig.java
@@ -392,6 +392,8 @@ public class BrokerConfig extends BrokerIdentity {
      */
     private boolean enableSingleTopicRegister = false;
 
+    private boolean enableMixedMessageType = false;
+
     public long getMaxPopPollingSize() {
         return maxPopPollingSize;
     }
@@ -1702,5 +1704,13 @@ public class BrokerConfig extends BrokerIdentity {
 
     public void setEnableSingleTopicRegister(boolean enableSingleTopicRegister) {
         this.enableSingleTopicRegister = enableSingleTopicRegister;
+    }
+
+    public boolean isEnableMixedMessageType() {
+        return enableMixedMessageType;
+    }
+
+    public void setEnableMixedMessageType(boolean enableMixedMessageType) {
+        this.enableMixedMessageType = enableMixedMessageType;
     }
 }
