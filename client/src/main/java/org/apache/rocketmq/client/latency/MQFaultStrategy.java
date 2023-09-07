d7c6ba77af6fbdcfd707d16d203e15bc7b9aab80
juntao <juntao.jjt@alibaba-inc.com>
2023-09-07 15:04:26 +0800

    remove useless class.

--- client/src/main/java/org/apache/rocketmq/client/latency/MQFaultStrategy.java
+++ client/src/main/java/org/apache/rocketmq/client/latency/MQFaultStrategy.java
@@ -20,7 +20,6 @@ package org.apache.rocketmq.client.latency;
 import org.apache.rocketmq.client.ClientConfig;
 import org.apache.rocketmq.client.impl.producer.TopicPublishInfo;
 import org.apache.rocketmq.client.impl.producer.TopicPublishInfo.QueueFilter;
-import org.apache.rocketmq.common.ConfigManager;
 import org.apache.rocketmq.common.message.MessageQueue;
 
 public class MQFaultStrategy {
