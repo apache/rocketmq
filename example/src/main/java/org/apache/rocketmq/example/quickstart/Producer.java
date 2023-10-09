9ce99b609cc62c03eb55ad300bc88bebd4f8ae49
yulangz <53958801+yulangz@users.noreply.github.com>
2023-10-09 15:54:45 +0800

    Update Producer.java

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -30,7 +30,7 @@ public class Producer {
     /**
      * The number of produced messages.
      */
-    public static final int MESSAGE_COUNT = 10;
+    public static final int MESSAGE_COUNT = 1000;
     public static final String PRODUCER_GROUP = "please_rename_unique_group_name";
     public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
     public static final String TOPIC = "TopicTest";
