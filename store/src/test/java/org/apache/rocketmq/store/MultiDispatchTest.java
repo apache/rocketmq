16914d8890e82656ca78b60f81e9da122eca85c2
Dongyuan Pan <dongyuanpan0@gmail.com>
2023-11-01 11:58:20 +0800

    fix MultiDispatchTest

--- store/src/test/java/org/apache/rocketmq/store/MultiDispatchTest.java
+++ store/src/test/java/org/apache/rocketmq/store/MultiDispatchTest.java
@@ -34,7 +34,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.rocksdb.RocksDBException;
 
-import static org.apache.rocketmq.store.config.StorePathConfigHelper.getStorePathConsumeQueue;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
