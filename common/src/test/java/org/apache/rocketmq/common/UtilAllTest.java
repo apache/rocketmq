56e886bf70669befd7b9e7380e68751fe67f05b2
YASH PATEL <121890726+yp969803@users.noreply.github.com>
2023-11-29 10:02:38 +0530

    [ISSUE #7592] testCleanBuffer unit test modifies, changed non-direct … (#7593)
    * [ISSUE #7592] testCleanBuffer unit test modifies, changed non-direct to direct buffer allocation
    
    * fix: consolidate UtilAll#cleanBuffer by checking if the given buffer is direct or not
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    
    ---------
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    Co-authored-by: Li Zhanhui <lizhanhui@gmail.com>

--- common/src/test/java/org/apache/rocketmq/common/UtilAllTest.java
+++ common/src/test/java/org/apache/rocketmq/common/UtilAllTest.java
@@ -215,8 +215,9 @@ public class UtilAllTest {
     @Test
     public void testCleanBuffer() {
         UtilAll.cleanBuffer(null);
+        UtilAll.cleanBuffer(ByteBuffer.allocateDirect(10));
+        UtilAll.cleanBuffer(ByteBuffer.allocateDirect(0));
         UtilAll.cleanBuffer(ByteBuffer.allocate(10));
-        UtilAll.cleanBuffer(ByteBuffer.allocate(0));
     }
 
     @Test
