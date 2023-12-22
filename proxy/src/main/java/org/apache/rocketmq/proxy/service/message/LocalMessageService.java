4bb4d78f1d5a8d920b85675ef9628a75b2a86f98
dingshuangxi888 <dingshuangxi888@gmail.com>
2023-12-22 15:55:00 +0800

    [ISSUE #7686] The bornTime is not set when using the popMessage API in cluster mode. (#7687)
    * The bornTime is not set when using the popMessage API in cluster mode.
    
    ---------
    
    Co-authored-by: ShuangxiDing <shuangxi.dsx@alibaba-inc.com>

--- proxy/src/main/java/org/apache/rocketmq/proxy/service/message/LocalMessageService.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/service/message/LocalMessageService.java
@@ -195,7 +195,6 @@ public class LocalMessageService implements MessageService {
     @Override
     public CompletableFuture<PopResult> popMessage(ProxyContext ctx, AddressableMessageQueue messageQueue,
         PopMessageRequestHeader requestHeader, long timeoutMillis) {
-        requestHeader.setBornTime(System.currentTimeMillis());
         RemotingCommand request = LocalRemotingCommand.createRequestCommand(RequestCode.POP_MESSAGE, requestHeader, ctx.getLanguage());
         CompletableFuture<RemotingCommand> future = new CompletableFuture<>();
         SimpleChannel channel = channelManager.createInvocationChannel(ctx);
