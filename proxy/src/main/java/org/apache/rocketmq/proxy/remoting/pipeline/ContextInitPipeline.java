9cc2333c0bffb6c8d1715d48e57f8b6c97c4eb6d
ShuangxiDing <shuangxi.dsx@alibaba-inc.com>
2024-01-09 11:33:05 +0800

    fix conflicts with develop.

--- proxy/src/main/java/org/apache/rocketmq/proxy/remoting/pipeline/ContextInitPipeline.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/remoting/pipeline/ContextInitPipeline.java
@@ -18,13 +18,13 @@ package org.apache.rocketmq.proxy.remoting.pipeline;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import java.util.Optional;
 import org.apache.rocketmq.common.MQVersion;
 import org.apache.rocketmq.common.utils.NetworkUtil;
 import org.apache.rocketmq.proxy.common.ProxyContext;
 import org.apache.rocketmq.proxy.processor.channel.ChannelProtocolType;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.apache.rocketmq.remoting.netty.AttributeKeys;
+import org.apache.rocketmq.remoting.protocol.LanguageCode;
 import org.apache.rocketmq.remoting.protocol.RemotingCommand;
 
 public class ContextInitPipeline implements RequestPipeline {
@@ -32,17 +32,22 @@ public class ContextInitPipeline implements RequestPipeline {
     @Override
     public void execute(ChannelHandlerContext ctx, RemotingCommand request, ProxyContext context) throws Exception {
         Channel channel = ctx.channel();
-        context.setAction(RemotingHelper.getRequestCodeDesc(request.getCode()))
-            .setProtocolType(ChannelProtocolType.REMOTING.getName())
-            .setChannel(channel)
-            .setLocalAddress(NetworkUtil.socketAddress2String(ctx.channel().localAddress()))
-            .setRemoteAddress(RemotingHelper.parseChannelRemoteAddr(ctx.channel()));
-
-        Optional.ofNullable(RemotingHelper.getAttributeValue(AttributeKeys.LANGUAGE_CODE_KEY, channel))
-            .ifPresent(language -> context.setLanguage(language.name()));
-        Optional.ofNullable(RemotingHelper.getAttributeValue(AttributeKeys.CLIENT_ID_KEY, channel))
-            .ifPresent(context::setClientID);
-        Optional.ofNullable(RemotingHelper.getAttributeValue(AttributeKeys.VERSION_KEY, channel))
-            .ifPresent(version -> context.setClientVersion(MQVersion.getVersionDesc(version)));
+        LanguageCode languageCode = RemotingHelper.getAttributeValue(AttributeKeys.LANGUAGE_CODE_KEY, channel);
+        String clientId = RemotingHelper.getAttributeValue(AttributeKeys.CLIENT_ID_KEY, channel);
+        Integer version = RemotingHelper.getAttributeValue(AttributeKeys.VERSION_KEY, channel);
+        context = context.withAction(RemotingHelper.getRequestCodeDesc(request.getCode()))
+            .withProtocolType(ChannelProtocolType.REMOTING.getName())
+            .withChannel(channel)
+            .withLocalAddress(NetworkUtil.socketAddress2String(ctx.channel().localAddress()))
+            .withRemoteAddress(RemotingHelper.parseChannelRemoteAddr(ctx.channel()));
+        if (languageCode != null) {
+            context.withLanguage(languageCode.name());
+        }
+        if (clientId != null) {
+            context.withClientID(clientId);
+        }
+        if (version != null) {
+            context.withClientVersion(MQVersion.getVersionDesc(version));
+        }
     }
 }
