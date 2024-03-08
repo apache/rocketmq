05f0abbdf627164db33716220a1ea62b08ecd1eb
Focus-rth <ruantihong@gmail.com>
2024-03-08 14:45:07 +0800

    [ISSUE #7890] fix PullMessageProcessor processRequest Response is null because of use asynchronous return

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingAbstract.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingAbstract.java
@@ -195,7 +195,7 @@ public abstract class NettyRemotingAbstract {
     }
 
     public void doAfterRpcHooks(String addr, RemotingCommand request, RemotingCommand response) {
-        if (rpcHooks.size() > 0) {
+        if (rpcHooks.size() > 0 && response != null) {
             for (RPCHook rpcHook : rpcHooks) {
                 rpcHook.doAfterResponse(addr, request, response);
             }
