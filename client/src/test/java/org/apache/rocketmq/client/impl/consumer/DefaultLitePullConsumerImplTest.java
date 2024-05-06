d656b4c8ffa68377e5b6233a4a7eb66195a0fc14
Willhow <65004897+Willhow-Gao@users.noreply.github.com>
2024-05-06 16:14:50 +0800

    Modify code style

--- client/src/test/java/org/apache/rocketmq/client/impl/consumer/DefaultLitePullConsumerImplTest.java
+++ client/src/test/java/org/apache/rocketmq/client/impl/consumer/DefaultLitePullConsumerImplTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.rocketmq.client.impl.consumer;
 
 import org.apache.rocketmq.client.consumer.DefaultLitePullConsumer;
@@ -54,7 +70,7 @@ public class DefaultLitePullConsumerImplTest {
      * The two null sets should be equal
      */
     @Test
-    public void testIsSetEqual3() throws InvocationTargetException, IllegalAccessException{
+    public void testIsSetEqual3() throws InvocationTargetException, IllegalAccessException {
         Set<MessageQueue> set1 = null;
         Set<MessageQueue> set2 = null;
         boolean equalResult = (boolean) isSetEqualMethod.invoke(consumer, set1, set2);
@@ -62,7 +78,7 @@ public class DefaultLitePullConsumerImplTest {
     }
 
     @Test
-    public void testIsSetEqual4() throws InvocationTargetException, IllegalAccessException{
+    public void testIsSetEqual4() throws InvocationTargetException, IllegalAccessException {
         Set<MessageQueue> set1 = null;
         Set<MessageQueue> set2 = new HashSet<>();
         boolean equalResult = (boolean) isSetEqualMethod.invoke(consumer, set1, set2);
@@ -70,7 +86,7 @@ public class DefaultLitePullConsumerImplTest {
     }
 
     @Test
-    public void testIsSetEqual5() throws InvocationTargetException, IllegalAccessException{
+    public void testIsSetEqual5() throws InvocationTargetException, IllegalAccessException {
         Set<MessageQueue> set1 = new HashSet<>();
         set1.add(new MessageQueue("testTopic","testBroker",111));
         Set<MessageQueue> set2 = new HashSet<>();
