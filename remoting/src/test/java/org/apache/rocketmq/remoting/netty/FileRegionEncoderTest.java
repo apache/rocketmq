db163b431ea9366d1121d04ded4b660fd8a31624
yuz10 <845238369@qq.com>
2024-05-30 13:52:09 +0800

    Revert "[ISSUE #7757] Use `CompositeByteBuf` to prevent memory copy. (#7694)" (#8209)
    This reverts commit 7a36d4d736ae8d6d92658e3bdb18f1cd5c0afdb0.

--- remoting/src/test/java/org/apache/rocketmq/remoting/netty/FileRegionEncoderTest.java
+++ remoting/src/test/java/org/apache/rocketmq/remoting/netty/FileRegionEncoderTest.java
@@ -21,15 +21,14 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.DefaultFileRegion;
 import io.netty.channel.FileRegion;
 import io.netty.channel.embedded.EmbeddedChannel;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 import java.util.UUID;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class FileRegionEncoderTest {
 
