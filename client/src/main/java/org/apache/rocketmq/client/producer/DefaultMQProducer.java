10f5dda43b7ae5bfc0a7c4fd517235934675aa97
wanghuaiyuan <3424672656@qq.com>
2024-06-04 20:34:27 +0800

    optimize code typo

--- client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
+++ client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
@@ -23,7 +23,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.rocketmq.client.ClientConfig;
 import org.apache.rocketmq.client.QueryResult;
 import org.apache.rocketmq.client.Validators;
