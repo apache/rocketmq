32f0e8481d1f5672b8d494de7607fe5b846ffa51
YanYunyang <313169664@qq.com>
2024-07-05 15:42:03 +0800

    [ISSUE #8366] When determining if `ChannelWrapper` is the wrapper for a channel, no longer acquire a read lock.

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
@@ -463,12 +463,10 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
                     for (Map.Entry<String, ChannelWrapper> entry : channelTables.entrySet()) {
                         String key = entry.getKey();
                         ChannelWrapper prev = entry.getValue();
-                        if (prev.getChannel() != null) {
-                            if (prev.getChannel() == channel) {
-                                prevCW = prev;
-                                addrRemote = key;
-                                break;
-                            }
+                        if (prev.isWrapperOf(channel)) {
+                            prevCW = prev;
+                            addrRemote = key;
+                            break;
                         }
                     }
 
@@ -1020,6 +1018,13 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
 
         public boolean isWritable() {
             return getChannel().isWritable();
+        }
+
+        public boolean isWrapperOf(Channel channel) {
+            if (this.channelFuture.channel() != null && this.channelFuture.channel() == channel) {
+                return true;
+            }
+            return false;
         }
 
         private Channel getChannel() {
