85ccb0cabc316cfa5c06faaa3509650a5ca5cf05
ziiyee <ziiyee@outlook.com>
2024-08-03 17:39:04 +0800

    Add more test case for BrokerMetricsManager. Includes: - check the topic is retry or dlq topic - check the group is system group or not - check the topic and the group belongs to system or not

--- broker/src/test/java/org/apache/rocketmq/broker/metrics/BrokerMetricsManagerTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/metrics/BrokerMetricsManagerTest.java
@@ -20,6 +20,8 @@ package org.apache.rocketmq.broker.metrics;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder;
+import org.apache.rocketmq.common.MixAll;
+import org.apache.rocketmq.common.topic.TopicValidator;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -65,4 +67,100 @@ public class BrokerMetricsManagerTest {
         assertThat(attributes.get(AttributeKey.stringKey("a"))).isEqualTo("b");
         assertThat(attributes.get(AttributeKey.stringKey("customized"))).isEqualTo("value");
     }
+
+
+    @Test
+    public void testIsRetryOrDlqTopicWithRetryTopic() {
+        String topic = MixAll.RETRY_GROUP_TOPIC_PREFIX + "TestTopic";
+        boolean result = BrokerMetricsManager.isRetryOrDlqTopic(topic);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testIsRetryOrDlqTopicWithDlqTopic() {
+        String topic = MixAll.DLQ_GROUP_TOPIC_PREFIX + "TestTopic";
+        boolean result = BrokerMetricsManager.isRetryOrDlqTopic(topic);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testIsRetryOrDlqTopicWithNonRetryOrDlqTopic() {
+        String topic = "NormalTopic";
+        boolean result = BrokerMetricsManager.isRetryOrDlqTopic(topic);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsRetryOrDlqTopicWithEmptyTopic() {
+        String topic = "";
+        boolean result = BrokerMetricsManager.isRetryOrDlqTopic(topic);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsRetryOrDlqTopicWithNullTopic() {
+        String topic = null;
+        boolean result = BrokerMetricsManager.isRetryOrDlqTopic(topic);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsSystemGroup_SystemGroup_ReturnsTrue() {
+        String group = "FooGroup";
+        String systemGroup = MixAll.CID_RMQ_SYS_PREFIX + group;
+        boolean result = BrokerMetricsManager.isSystemGroup(systemGroup);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testIsSystemGroup_NonSystemGroup_ReturnsFalse() {
+        String group = "FooGroup";
+        boolean result = BrokerMetricsManager.isSystemGroup(group);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsSystemGroup_EmptyGroup_ReturnsFalse() {
+        String group = "";
+        boolean result = BrokerMetricsManager.isSystemGroup(group);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsSystemGroup_NullGroup_ReturnsFalse() {
+        String group = null;
+        boolean result = BrokerMetricsManager.isSystemGroup(group);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsSystem_SystemTopicOrSystemGroup_ReturnsTrue() {
+        String topic = "FooTopic";
+        String group = "FooGroup";
+        String systemTopic = TopicValidator.RMQ_SYS_TRANS_HALF_TOPIC;
+        String systemGroup = MixAll.CID_RMQ_SYS_PREFIX + group;
+
+        boolean resultTopic = BrokerMetricsManager.isSystem(systemTopic, group);
+        assertThat(resultTopic).isTrue();
+
+        boolean resultGroup = BrokerMetricsManager.isSystem(topic, systemGroup);
+        assertThat(resultGroup).isTrue();
+    }
+
+    @Test
+    public void testIsSystem_NonSystemTopicAndGroup_ReturnsFalse() {
+        String topic = "FooTopic";
+        String group = "FooGroup";
+        boolean result = BrokerMetricsManager.isSystem(topic, group);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testIsSystem_EmptyTopicAndGroup_ReturnsFalse() {
+        String topic = "";
+        String group = "";
+        boolean result = BrokerMetricsManager.isSystem(topic, group);
+        assertThat(result).isFalse();
+    }
+
 }
