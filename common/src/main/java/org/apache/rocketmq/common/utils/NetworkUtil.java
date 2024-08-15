dd7ac488ac3518f65870c5fd1b35fa321adca8f2
zekai-li <58294989+zekai-li@users.noreply.github.com>
2024-08-16 00:33:35 +0800

    [ISSUE #8289] Fixed network bugs and merged networkutil code (#8290)
    * [ISSUE #8289] Fixed network bugs and merged networkutil code
    Fix the following three as
    1，TraceBean： private static final String LOCAL_ADDRESS = UtilAll.ipToIPv4Str(UtilAll.getIP()); getIP mayby ipv6
    2，NetworkUtil：if (ip.startsWith("127.0") || ip.startsWith("192.168") || ip.startsWith("0."))；Check whether Intranet errors exist
    
    * [ISSUE #8289] Fixed network bugs and merged networkutil code
    Fix the following three as
    1，TraceBean： private static final String LOCAL_ADDRESS = UtilAll.ipToIPv4Str(UtilAll.getIP()); getIP mayby ipv6
    2，NetworkUtil：if (ip.startsWith("127.0") || ip.startsWith("192.168") || ip.startsWith("0."))；Check whether Intranet errors exist

--- common/src/main/java/org/apache/rocketmq/common/utils/NetworkUtil.java
+++ common/src/main/java/org/apache/rocketmq/common/utils/NetworkUtil.java
@@ -19,15 +19,21 @@ package org.apache.rocketmq.common.utils;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.net.SocketAddress;
+import java.net.SocketException;
 import java.nio.channels.Selector;
 import java.nio.channels.spi.SelectorProvider;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
+
+import org.apache.commons.validator.routines.InetAddressValidator;
+import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.constant.LoggerName;
 import org.apache.rocketmq.logging.org.slf4j.Logger;
 import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
@@ -59,18 +65,14 @@ public class NetworkUtil {
         if (isLinuxPlatform()) {
             try {
                 final Class<?> providerClazz = Class.forName("sun.nio.ch.EPollSelectorProvider");
-                if (providerClazz != null) {
-                    try {
-                        final Method method = providerClazz.getMethod("provider");
-                        if (method != null) {
-                            final SelectorProvider selectorProvider = (SelectorProvider) method.invoke(null);
-                            if (selectorProvider != null) {
-                                result = selectorProvider.openSelector();
-                            }
-                        }
-                    } catch (final Exception e) {
-                        log.warn("Open ePoll Selector for linux platform exception", e);
+                try {
+                    final Method method = providerClazz.getMethod("provider");
+                    final SelectorProvider selectorProvider = (SelectorProvider) method.invoke(null);
+                    if (selectorProvider != null) {
+                        result = selectorProvider.openSelector();
                     }
+                } catch (final Exception e) {
+                    log.warn("Open ePoll Selector for linux platform exception", e);
                 }
             } catch (final Exception e) {
                 // ignore
@@ -88,53 +90,81 @@ public class NetworkUtil {
         return isLinuxPlatform;
     }
 
-    public static String getLocalAddress() {
-        try {
-            // Traversal Network interface to get the first non-loopback and non-private address
-            Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
-            ArrayList<String> ipv4Result = new ArrayList<>();
-            ArrayList<String> ipv6Result = new ArrayList<>();
-            while (enumeration.hasMoreElements()) {
-                final NetworkInterface nif = enumeration.nextElement();
-                if (isBridge(nif) || nif.isVirtual() || nif.isPointToPoint() || !nif.isUp()) {
-                    continue;
-                }
-
-                final Enumeration<InetAddress> en = nif.getInetAddresses();
-                while (en.hasMoreElements()) {
-                    final InetAddress address = en.nextElement();
-                    if (!address.isLoopbackAddress()) {
-                        if (address instanceof Inet6Address) {
-                            ipv6Result.add(normalizeHostAddress(address));
-                        } else {
-                            ipv4Result.add(normalizeHostAddress(address));
+    public static List<InetAddress> getLocalInetAddressList() throws SocketException {
+        Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+        List<InetAddress> inetAddressList = new ArrayList<>();
+        // Traversal Network interface to get the non-bridge and non-virtual and non-ppp and up address
+        while (enumeration.hasMoreElements()) {
+            final NetworkInterface nif = enumeration.nextElement();
+            if (isBridge(nif) || nif.isVirtual() || nif.isPointToPoint() || !nif.isUp()) {
+                continue;
+            }
+            InetAddressValidator validator = InetAddressValidator.getInstance();
+            final Enumeration<InetAddress> en = nif.getInetAddresses();
+            while (en.hasMoreElements()) {
+                final InetAddress address = en.nextElement();
+                if (address instanceof Inet4Address) {
+                    byte[] ipByte = address.getAddress();
+                    if (ipByte.length == 4) {
+                        if (validator.isValidInet4Address(UtilAll.ipToIPv4Str(ipByte))) {
+                            inetAddressList.add(address);
+                        }
+                    }
+                } else if (address instanceof Inet6Address) {
+                    byte[] ipByte = address.getAddress();
+                    if (ipByte.length == 16) {
+                        if (validator.isValidInet6Address(UtilAll.ipToIPv6Str(ipByte))) {
+                            inetAddressList.add(address);
                         }
                     }
                 }
             }
+        }
+        return inetAddressList;
+    }
 
-            // prefer ipv4
+    public static InetAddress getLocalInetAddress() {
+        try {
+            ArrayList<InetAddress> ipv4Result = new ArrayList<>();
+            ArrayList<InetAddress> ipv6Result = new ArrayList<>();
+            List<InetAddress> localInetAddressList = getLocalInetAddressList();
+            for (InetAddress inetAddress : localInetAddressList) {
+                if (inetAddress instanceof Inet6Address) {
+                    ipv6Result.add(inetAddress);
+                } else {
+                    ipv4Result.add(inetAddress);
+                }
+            }
+            // prefer ipv4 and prefer external ip
             if (!ipv4Result.isEmpty()) {
-                for (String ip : ipv4Result) {
-                    if (ip.startsWith("127.0") || ip.startsWith("192.168") || ip.startsWith("0.")) {
+                for (InetAddress ip : ipv4Result) {
+                    if (UtilAll.isInternalIP(ip.getAddress())) {
                         continue;
                     }
-
                     return ip;
                 }
-
                 return ipv4Result.get(ipv4Result.size() - 1);
             } else if (!ipv6Result.isEmpty()) {
+                for (InetAddress ip : ipv6Result) {
+                    if (UtilAll.isInternalV6IP(ip)) {
+                        continue;
+                    }
+                    return ip;
+                }
                 return ipv6Result.get(0);
             }
             //If failed to find,fall back to localhost
-            final InetAddress localHost = InetAddress.getLocalHost();
-            return normalizeHostAddress(localHost);
+            return InetAddress.getLocalHost();
         } catch (Exception e) {
             log.error("Failed to obtain local address", e);
         }
 
         return null;
+    }
+
+    public static String getLocalAddress() {
+        InetAddress localHost = getLocalInetAddress();
+        return normalizeHostAddress(localHost);
     }
 
     public static String normalizeHostAddress(final InetAddress localHost) {
@@ -149,8 +179,7 @@ public class NetworkUtil {
         int split = addr.lastIndexOf(":");
         String host = addr.substring(0, split);
         String port = addr.substring(split + 1);
-        InetSocketAddress isa = new InetSocketAddress(host, Integer.parseInt(port));
-        return isa;
+        return new InetSocketAddress(host, Integer.parseInt(port));
     }
 
     public static String socketAddress2String(final SocketAddress addr) {
