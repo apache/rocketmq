e5e38396ba32293b3bd40a5a40ff402d42dce928
rongtong <jinrongtong16@mails.ucas.ac.cn>
2024-08-30 14:15:03 +0800

    [ISSUE #8591] Preliminary support for key commands of LMQ (#8590)
    * Preliminary support for key commands of LMQ
    
    * Preliminary support for key commands of LMQ
    
    * Optimize some code
    
    * Fix some bugs and UTs for lmq support
    
    * Fix UTs can not pass
    
    * Fix UTs can not pass
    
    * Add some check to prevent NPE

--- example/src/main/java/org/apache/rocketmq/example/lmq/LMQPullConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/lmq/LMQPullConsumer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.rocketmq.example.simple;
+package org.apache.rocketmq.example.lmq;
 
 import java.util.Arrays;
 import java.util.HashSet;
