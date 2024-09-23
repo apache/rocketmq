2e92f3357558b9fcd2bb8d50d8b2338dcaa1d773
qianye <wuxingcan.wxc@alibaba-inc.com>
2024-09-23 10:06:22 +0800

    Ensure that when the currently selected Name Server address by the client is no longer in use, a new channel connection will be created.

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
@@ -520,10 +520,11 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
                 this.namesrvAddrList.set(addrs);
 
                 // should close the channel if choosed addr is not exist.
-                if (this.namesrvAddrChoosed.get() != null && !addrs.contains(this.namesrvAddrChoosed.get())) {
-                    String namesrvAddr = this.namesrvAddrChoosed.getAndSet(null);
+                String choseNameServerAddr = this.namesrvAddrChoosed.get();
+                if (choseNameServerAddr != null && !addrs.contains(choseNameServerAddr)) {
+                    namesrvAddrChoosed.compareAndSet(choseNameServerAddr, null);
                     for (String addr : this.channelTables.keySet()) {
-                        if (addr.contains(namesrvAddr)) {
+                        if (addr.contains(choseNameServerAddr)) {
                             ChannelWrapper channelWrapper = this.channelTables.get(addr);
                             if (channelWrapper != null) {
                                 channelWrapper.close();
