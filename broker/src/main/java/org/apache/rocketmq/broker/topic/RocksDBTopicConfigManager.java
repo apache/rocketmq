525f877f3bddced2d85d99520fd600bcbbfe3c6d
LetLetMe <43874697+LetLetMe@users.noreply.github.com>
2024-09-23 19:24:15 +0800

    [ISSUE #8589] Support file format CQ and json format offset in-place upgrade to rocksdb management (#8600)

--- broker/src/main/java/org/apache/rocketmq/broker/topic/RocksDBTopicConfigManager.java
+++ broker/src/main/java/org/apache/rocketmq/broker/topic/RocksDBTopicConfigManager.java
@@ -18,16 +18,15 @@ package org.apache.rocketmq.broker.topic;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import java.io.File;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 import org.apache.rocketmq.broker.BrokerController;
 import org.apache.rocketmq.broker.RocksDBConfigManager;
 import org.apache.rocketmq.common.TopicConfig;
 import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.utils.DataConverter;
 import org.apache.rocketmq.remoting.protocol.DataVersion;
-
-import java.io.File;
-import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
 
 public class RocksDBTopicConfigManager extends TopicConfigManager {
 
@@ -60,29 +59,35 @@ public class RocksDBTopicConfigManager extends TopicConfigManager {
 
     private boolean merge() {
         if (!brokerController.getMessageStoreConfig().isTransferMetadataJsonToRocksdb()) {
-            log.info("The switch is off, no merge operation is needed.");
+            log.info("the switch transferMetadataJsonToRocksdb is off, no merge topic operation is needed.");
             return true;
         }
         if (!UtilAll.isPathExists(this.configFilePath()) && !UtilAll.isPathExists(this.configFilePath() + ".bak")) {
-            log.info("json file and json back file not exist, so skip merge");
+            log.info("topic json file does not exist, so skip merge");
             return true;
         }
 
-        if (!super.load()) {
-            log.error("load topic config from json file error, startup will exit");
+        if (!super.loadDataVersion()) {
+            log.error("load json topic dataVersion error, startup will exit");
             return false;
         }
 
-        final ConcurrentMap<String, TopicConfig> topicConfigTable = this.getTopicConfigTable();
         final DataVersion dataVersion = super.getDataVersion();
         final DataVersion kvDataVersion = this.getDataVersion();
         if (dataVersion.getCounter().get() > kvDataVersion.getCounter().get()) {
+            if (!super.load()) {
+                log.error("load topic config from json file error, startup will exit");
+                return false;
+            }
+            final ConcurrentMap<String, TopicConfig> topicConfigTable = this.getTopicConfigTable();
             for (Map.Entry<String, TopicConfig> entry : topicConfigTable.entrySet()) {
                 putTopicConfig(entry.getValue());
                 log.info("import topic config to rocksdb, topic={}", entry.getValue());
             }
-            this.rocksDBConfigManager.getKvDataVersion().assignNewOne(dataVersion);
+            this.getDataVersion().assignNewOne(dataVersion);
             updateDataVersion();
+        } else {
+            log.info("dataVersion is not greater than kvDataVersion, no need to merge topic metaData, dataVersion={}, kvDataVersion={}", dataVersion, kvDataVersion);
         }
         log.info("finish read topic config from json file and merge to rocksdb");
         this.persist();
@@ -150,6 +155,7 @@ public class RocksDBTopicConfigManager extends TopicConfigManager {
         try {
             rocksDBConfigManager.updateKvDataVersion();
         } catch (Exception e) {
+            log.error("update topic config dataVersion error", e);
             throw new RuntimeException(e);
         }
     }
