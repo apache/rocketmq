f3511f6ac5fcc2ecd6ce8879ba29e9b8fbc627c0
boboyh <3135835602@qq.com>
2024-10-08 21:14:10 +0800

    map映射替代循环遍历

--- common/src/main/java/org/apache/rocketmq/common/action/Action.java
+++ common/src/main/java/org/apache/rocketmq/common/action/Action.java
@@ -1,49 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.rocketmq.common.action;
 
 import com.alibaba.fastjson2.annotation.JSONField;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Action {
 
     UNKNOWN((byte) 0, "Unknown"),
-
     ALL((byte) 1, "All"),
-
     ANY((byte) 2, "Any"),
-
     PUB((byte) 3, "Pub"),
-
     SUB((byte) 4, "Sub"),
-
     CREATE((byte) 5, "Create"),
-
     UPDATE((byte) 6, "Update"),
-
     DELETE((byte) 7, "Delete"),
-
     GET((byte) 8, "Get"),
-
     LIST((byte) 9, "List");
 
     @JSONField(value = true)
     private final byte code;
     private final String name;
+
+    // 添加静态Map用于存储名称到枚举值的映射
+    private static final Map<String, Action> NAME_MAP = new HashMap<>();
+
+    static {
+        // 初始化Map
+        for (Action action : values()) {
+            NAME_MAP.put(action.getName().toLowerCase(), action);
+        }
+    }
 
     Action(byte code, String name) {
         this.code = code;
@@ -51,12 +39,10 @@ public enum Action {
     }
 
     public static Action getByName(String name) {
-        for (Action action : Action.values()) {
-            if (StringUtils.equalsIgnoreCase(action.getName(), name)) {
-                return action;
-            }
+        if (StringUtils.isBlank(name)) {
+            return null;
         }
-        return null;
+        return NAME_MAP.get(name.toLowerCase());
     }
 
     public byte getCode() {
