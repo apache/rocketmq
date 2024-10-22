b139a3aefae8df7d65bf67cdef4761a1f154ae40
mawen12 <1181963012mw@gmail.com>
2024-10-22 19:06:41 +0800

    fix typo

--- test/src/main/java/org/apache/rocketmq/test/util/MQAdminTestUtils.java
+++ test/src/main/java/org/apache/rocketmq/test/util/MQAdminTestUtils.java
@@ -193,7 +193,7 @@ public class MQAdminTestUtils {
         return  true;
     }
 
-    //should only be test, if some middle operation failed, it dose not backup the brokerConfigMap
+    //should only be test, if some middle operation failed, it does not backup the brokerConfigMap
     public static Map<String, TopicConfigAndQueueMapping> createStaticTopic(String topic, int queueNum, Set<String> targetBrokers, DefaultMQAdminExt defaultMQAdminExt) throws Exception {
         Map<String, TopicConfigAndQueueMapping> brokerConfigMap = MQAdminUtils.examineTopicConfigAll(topic, defaultMQAdminExt);
         assert brokerConfigMap.isEmpty();
@@ -203,7 +203,7 @@ public class MQAdminTestUtils {
         return brokerConfigMap;
     }
 
-    //should only be test, if some middle operation failed, it dose not backup the brokerConfigMap
+    //should only be test, if some middle operation failed, it does not backup the brokerConfigMap
     public static void remappingStaticTopic(String topic, Set<String> targetBrokers, DefaultMQAdminExt defaultMQAdminExt) throws Exception {
         Map<String, TopicConfigAndQueueMapping> brokerConfigMap = MQAdminUtils.examineTopicConfigAll(topic, defaultMQAdminExt);
         assert !brokerConfigMap.isEmpty();
