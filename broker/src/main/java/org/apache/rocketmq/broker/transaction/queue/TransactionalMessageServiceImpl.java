b681f3d99aeebbb1915e7610e1b1ef90f9cdb5a2
TangJie <tangjie@codemao.cn>
2024-12-18 19:05:17 +0800

    事务消息， 回查判断OP_HALF_QUEUE 消息是否在HALF_QUEUE中，优化判断方式。 Transaction message, check back to determine if the OP-HALF-QUEUE message is in HALF-QUEUE, optimize the judgment method

--- broker/src/main/java/org/apache/rocketmq/broker/transaction/queue/TransactionalMessageServiceImpl.java
+++ broker/src/main/java/org/apache/rocketmq/broker/transaction/queue/TransactionalMessageServiceImpl.java
@@ -203,7 +203,7 @@ public class TransactionalMessageServiceImpl implements TransactionalMessageServ
                         log.info("Queue={} process time reach max={}", messageQueue, MAX_PROCESS_TIME_LIMIT);
                         break;
                     }
-                    Long removedOpOffset;
+                    Long removedOpOffset = null;
                     if ((removedOpOffset = removeMap.remove(i)) != null) {
                         log.debug("Half offset {} has been committed/rolled back", i);
                         opMsgMap.get(removedOpOffset).remove(i);
@@ -456,7 +456,7 @@ public class TransactionalMessageServiceImpl implements TransactionalMessageServ
             if (-1 == prepareQueueOffset) {
                 return false;
             } else {
-                Long tmpOpOffset;
+                Long tmpOpOffset = null;
                 if ((tmpOpOffset = removeMap.remove(prepareQueueOffset)) != null) {
                     doneOpOffset.add(tmpOpOffset);
                     log.info("removeMap contain prepareQueueOffset. real_topic={},uniqKey={},immunityTime={},offset={}",
