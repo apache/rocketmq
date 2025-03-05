2f479e8050c2fbfbe08442e66b29370a13e88c6f
wgl <2216348784@qq.com>
2025-03-05 17:28:16 +0800

    fix:Add a scheduled cleanup task

--- broker/src/main/java/org/apache/rocketmq/broker/processor/PopRebalanceCacheManager.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/PopRebalanceCacheManager.java
@@ -1,14 +1,14 @@
 package org.apache.rocketmq.broker.processor;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.apache.rocketmq.common.ThreadFactoryImpl;
 import org.apache.rocketmq.common.constant.LoggerName;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.logging.org.slf4j.Logger;
 import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -16,7 +16,14 @@ public class PopRebalanceCacheManager {
 
     private static final long LOCK_TIMEOUT_MILLIS = 3000L;
 
+    // 24 hours
+    private static final long CLEAN_INTERVAL = 86400;
+
     private static final Logger log = LoggerFactory.getLogger(LoggerName.BROKER_LOGGER_NAME);
+
+    protected final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor(
+            new ThreadFactoryImpl("PopRebalanceCacheCleanerThread")
+    );
 
     // Cache with pop mode rebalancing under the condition that consumer groups and queues remain unchanged
     private final ConcurrentMap<String, ConcurrentHashMap<String, Set<MessageQueue>>> loadBalanceDateTable = new ConcurrentHashMap<>();
@@ -26,6 +33,10 @@ public class PopRebalanceCacheManager {
     private final ConcurrentMap<String, List<MessageQueue>> topicMqAll = new ConcurrentHashMap<>();
 
     private final Lock popRebalanceCacheLock = new ReentrantLock();
+
+    public PopRebalanceCacheManager() {
+        startCacheCleaner();
+    }
 
     public Set<MessageQueue> getLoadBalanceDate(List<MessageQueue> mqAll, List<String> cidAll, String topic,
                                                 String clientId, String strategyName, int popShareQueueNum) {
@@ -73,6 +84,35 @@ public class PopRebalanceCacheManager {
             }
         } catch (InterruptedException e) {
             log.warn("PopRebalanceCacheManager putLoadBalanceDate Exception", e);
+        }
+    }
+
+    private void startCacheCleaner() {
+        executorService.scheduleAtFixedRate(() -> {
+            try {
+                this.cleanCache();
+            } catch (Throwable e) {
+                log.error("scheduleAtFixedRate cleanCache exception", e);
+            }
+        }, CLEAN_INTERVAL, CLEAN_INTERVAL, TimeUnit.SECONDS);
+        log.info("PopRebalanceCacheCleaner started with interval: {} seconds", CLEAN_INTERVAL);
+    }
+
+    private void cleanCache() {
+        try {
+            if (this.popRebalanceCacheLock.tryLock(LOCK_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS)) {
+                try {
+                    log.info("Start cleaning PopRebalanceCache.");
+                    loadBalanceDateTable.clear();
+                    topicCidAll.clear();
+                    topicMqAll.clear();
+                    log.info("PopRebalanceCache cleaning finished.");
+                } finally {
+                    this.popRebalanceCacheLock.unlock();
+                }
+            }
+        } catch (InterruptedException e) {
+            log.debug("Error cleaning PopRebalanceCache", e);
         }
     }
 
