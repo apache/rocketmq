6e1bc81ac0c4d94da6a522bf2018a246d501f752
yx9o <yangx_soft@163.com>
2025-04-25 21:41:40 +0800

    [ISSUE# 9365] Use fastjson2 in client module

--- client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
@@ -16,7 +16,7 @@
  */
 package org.apache.rocketmq.client.impl.factory;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import io.netty.channel.Channel;
 import java.util.Collections;
 import java.util.HashMap;
