e49e29ecc8a27bc2c3dadbf3cc2905c4f6cf0b44
dingshuangxi888 <dingshuangxi888@gmail.com>
2025-04-30 14:01:34 +0800

    Fix delete logic from TimeStoreTable in IndexService

--- tieredstore/src/main/java/org/apache/rocketmq/tieredstore/index/IndexStoreService.java
+++ tieredstore/src/main/java/org/apache/rocketmq/tieredstore/index/IndexStoreService.java
@@ -18,6 +18,20 @@ package org.apache.rocketmq.tieredstore.index;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Stopwatch;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.rocketmq.common.ServiceThread;
+import org.apache.rocketmq.common.UtilAll;
+import org.apache.rocketmq.store.logfile.DefaultMappedFile;
+import org.apache.rocketmq.store.logfile.MappedFile;
+import org.apache.rocketmq.tieredstore.MessageStoreConfig;
+import org.apache.rocketmq.tieredstore.common.AppendResult;
+import org.apache.rocketmq.tieredstore.file.FlatAppendFile;
+import org.apache.rocketmq.tieredstore.file.FlatFileFactory;
+import org.apache.rocketmq.tieredstore.provider.FileSegment;
+import org.apache.rocketmq.tieredstore.util.MessageStoreUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.file.Paths;
@@ -35,19 +49,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.rocketmq.common.ServiceThread;
-import org.apache.rocketmq.common.UtilAll;
-import org.apache.rocketmq.store.logfile.DefaultMappedFile;
-import org.apache.rocketmq.store.logfile.MappedFile;
-import org.apache.rocketmq.tieredstore.MessageStoreConfig;
-import org.apache.rocketmq.tieredstore.common.AppendResult;
-import org.apache.rocketmq.tieredstore.file.FlatAppendFile;
-import org.apache.rocketmq.tieredstore.file.FlatFileFactory;
-import org.apache.rocketmq.tieredstore.provider.FileSegment;
-import org.apache.rocketmq.tieredstore.util.MessageStoreUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class IndexStoreService extends ServiceThread implements IndexService {
 
@@ -341,7 +342,7 @@ public class IndexStoreService extends ServiceThread implements IndexService {
         readWriteLock.writeLock().lock();
         try {
             timeStoreTable.entrySet().removeIf(entry ->
-                entry.getKey() < expireTimestamp &&
+                entry.getValue().getEndTimestamp() <= expireTimestamp &&
                     IndexFile.IndexStatusEnum.UPLOAD.equals(entry.getValue().getFileStatus()));
             flatAppendFile.destroyExpiredFile(expireTimestamp);
         } finally {
