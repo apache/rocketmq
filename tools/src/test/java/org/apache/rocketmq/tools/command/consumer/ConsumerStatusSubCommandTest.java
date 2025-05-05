36552774a68da1a334855db028136fc8eae63f5d
yx9o <yangx_soft@163.com>
2025-05-05 09:00:35 +0800

    Update test

--- tools/src/test/java/org/apache/rocketmq/tools/command/consumer/ConsumerStatusSubCommandTest.java
+++ tools/src/test/java/org/apache/rocketmq/tools/command/consumer/ConsumerStatusSubCommandTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.rocketmq.tools.command.consumer;
 
-import java.util.HashSet;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
@@ -30,7 +29,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
+import java.util.HashSet;
 
 public class ConsumerStatusSubCommandTest {
 
@@ -65,7 +64,7 @@ public class ConsumerStatusSubCommandTest {
     private ServerResponseMocker startOneBroker() {
         ConsumerConnection consumerConnection = new ConsumerConnection();
         HashSet<Connection> connectionSet = new HashSet<>();
-        Connection connection = mock(Connection.class);
+        Connection connection = new Connection();
         connectionSet.add(connection);
         consumerConnection.setConnectionSet(connectionSet);
         // start broker
