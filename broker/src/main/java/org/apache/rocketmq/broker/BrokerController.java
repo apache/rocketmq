8062dae55437ed1e2a16b66a6ad92be062a0138c
mxsm <mxsm@apache.org>
2025-09-06 00:13:43 +0800

    [ISSUE #9670]Simplify messageStore sendMessageBackHook assignment by removing null check

--- broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
+++ broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
@@ -1016,9 +1016,8 @@ public class BrokerController {
             }
         };
 
-        if (messageStore != null) {
-            messageStore.setSendMessageBackHook(sendMessageBackHook);
-        }
+        messageStore.setSendMessageBackHook(sendMessageBackHook);
+
     }
 
     private void initialTransaction() {
