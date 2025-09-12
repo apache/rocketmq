679b4d1efde6100e53521d75f8786ae300c6e3d9
drizzle.zk <drizzle.zk@alibaba-inc.com>
2025-09-12 17:05:16 +0800

    update

--- store/src/main/java/org/apache/rocketmq/store/timer/rocksdb/TimerMessageRocksDBStore.java
+++ store/src/main/java/org/apache/rocketmq/store/timer/rocksdb/TimerMessageRocksDBStore.java
@@ -21,16 +21,12 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
-
-import javax.security.auth.callback.Callback;
-
 import com.conversantmedia.util.concurrent.DisruptorBlockingQueue;
 import com.google.common.util.concurrent.RateLimiter;
 import org.apache.commons.collections.CollectionUtils;
