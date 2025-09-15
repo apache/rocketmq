51e465d3a6f537900e63b1358fcddcdfaf870f86
qianye <wuxingcan.wxc@alibaba-inc.com>
2025-09-15 15:19:18 +0800

    fix

--- broker/src/main/java/org/apache/rocketmq/broker/offset/ConsumerOffsetManager.java
+++ broker/src/main/java/org/apache/rocketmq/broker/offset/ConsumerOffsetManager.java
@@ -97,6 +97,7 @@ public class ConsumerOffsetManager extends ConfigManager {
                 if (arrays.length == 2 && topic.equals(arrays[0])) {
                     it.remove();
                     removeConsumerOffset(topicAtGroup);
+                    pullOffsetTable.remove(topicAtGroup);
                     LOG.warn("Clean topic's offset, {}, {}", topicAtGroup, next.getValue());
                 }
             }
