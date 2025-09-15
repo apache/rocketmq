1b9c36d21247063eb383ec8347d51b722f617327
RongtongJin <jinrongtong16@mails.ucas.ac.cn>
2025-09-15 12:17:01 +0800

    Polish the code

--- broker/src/main/java/org/apache/rocketmq/broker/processor/PopBufferMergeService.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/PopBufferMergeService.java
@@ -126,9 +126,11 @@ public class PopBufferMergeService extends ServiceThread {
         if (!isShouldRunning()) {
             return;
         }
-//        while (this.buffer.size() > 0 || getOffsetTotalSize() > 0) {
-//            scan();
-//        }
+        if (!brokerController.getBrokerConfig().isInBrokerContainer()) {
+            while (this.buffer.size() > 0 || getOffsetTotalSize() > 0) {
+                scan();
+            }
+        }
     }
 
     private int scanCommitOffset() {
