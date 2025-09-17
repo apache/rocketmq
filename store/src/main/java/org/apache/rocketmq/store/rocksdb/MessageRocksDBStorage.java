653ba0ad40aecae1003029d39cbbbd12cbb7a3cf
drizzle.zk <drizzle.zk@alibaba-inc.com>
2025-09-17 09:53:52 +0800

    update

--- store/src/main/java/org/apache/rocketmq/store/rocksdb/MessageRocksDBStorage.java
+++ store/src/main/java/org/apache/rocketmq/store/rocksdb/MessageRocksDBStorage.java
@@ -621,7 +621,6 @@ public class MessageRocksDBStorage extends AbstractRocksDBStorage {
         }
         try {
             byte[] offsetBytes = get(cfHandle, readOptions, LAST_OFFSET_PY);
-            log.info("getLastOffsetPy offsetBytes : {}", offsetBytes);
             return offsetBytes == null ? 0L : ByteBuffer.wrap(offsetBytes).getLong();
         } catch (Exception e) {
             logError.error("MessageRocksDBStorage getLastOffsetPy error: {}", e.getMessage());
