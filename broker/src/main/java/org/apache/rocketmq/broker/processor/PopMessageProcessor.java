2494d5c40ecd74c33bdac655582192dc0a7fefcd
dingshuangxi888 <dingshuangxi888@gmail.com>
2025-11-19 20:24:27 +0800

    Fix the issue of missing bornTime in POP requests when broker ACL is enabled
    Change-Id: I7cf78da89109913194ccd3f02dcd9d056487a41c

--- broker/src/main/java/org/apache/rocketmq/broker/processor/PopMessageProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/PopMessageProcessor.java
@@ -29,7 +29,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
