e4648e6d7dc54beb6ea28be4beecaa3853d6dcba
RongtongJin <user@example.com>
2026-01-17 22:40:15 +0800

    Remove useless import

--- store/src/test/java/org/apache/rocketmq/store/DefaultMessageStoreTest.java
+++ store/src/test/java/org/apache/rocketmq/store/DefaultMessageStoreTest.java
@@ -24,7 +24,6 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.mockito.ArgumentCaptor;
 
