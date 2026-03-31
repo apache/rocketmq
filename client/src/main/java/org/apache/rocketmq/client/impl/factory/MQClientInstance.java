00625cc67cd2936fd46193f1c37de5f610acbb98
Kris20030907 <3185633428@qq.com>
2026-03-31 16:03:24 +0800

    fix: remove useless import.

--- client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
@@ -86,7 +86,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
