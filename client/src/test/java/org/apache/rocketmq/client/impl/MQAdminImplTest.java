798cf5690f29f764b15bb689aeb047b108e636f1
hqbfzwang <hqbfzwang@tencent.com>
2026-05-15 11:53:39 +0800

    add test

--- client/src/test/java/org/apache/rocketmq/client/impl/MQAdminImplTest.java
+++ client/src/test/java/org/apache/rocketmq/client/impl/MQAdminImplTest.java
@@ -53,7 +53,6 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
