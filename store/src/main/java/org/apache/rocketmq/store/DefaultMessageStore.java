80e6f98a6a8f2ca5cc658ed62d84639dd96625fd
winglechen@aliyun.com <winglechen@gmail.com>
2026-06-01 21:52:07 +0800

    comment: add attribute comments to batchDispatchRequestQueue of DefaultMessageStore

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -211,6 +211,13 @@ public class DefaultMessageStore implements MessageStore {
 
     private final AtomicInteger mappedPageHoldCount = new AtomicInteger(0);
 
+    /**
+     * BatchDispatchRequest queue
+     * offer by ConcurrentReputMessageService.createBatchDispatchRequest()
+     * poll by MainBatchDispatchRequestService.pollBatchDispatchRequest()
+     *
+     * <p>if enableBuildConsumeQueueConcurrently is false, It is useless
+     */
     private final ConcurrentLinkedQueue<BatchDispatchRequest> batchDispatchRequestQueue = new ConcurrentLinkedQueue<>();
 
     private final int dispatchRequestOrderlyQueueSize = 16;
