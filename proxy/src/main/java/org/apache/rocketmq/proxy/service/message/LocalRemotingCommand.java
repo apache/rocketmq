4dea970ef48c2a7bd395b94bc1da795cd2716e74
majialong <majialoong@gmail.com>
2026-07-17 13:03:15 +0800

    [ISSUE #10620] Fix NPE when recording RPC latency for local proxy requests

--- proxy/src/main/java/org/apache/rocketmq/proxy/service/message/LocalRemotingCommand.java
+++ proxy/src/main/java/org/apache/rocketmq/proxy/service/message/LocalRemotingCommand.java
@@ -16,6 +16,7 @@
  */
 package org.apache.rocketmq.proxy.service.message;
 
+import com.google.common.base.Stopwatch;
 import java.util.HashMap;
 import org.apache.rocketmq.remoting.CommandCustomHeader;
 import org.apache.rocketmq.remoting.protocol.LanguageCode;
@@ -31,6 +32,7 @@ public class LocalRemotingCommand extends RemotingCommand {
         cmd.setExtFields(new HashMap<>());
         setCmdVersion(cmd);
         cmd.makeCustomHeaderToNet();
+        cmd.setProcessTimer(Stopwatch.createStarted());
         return cmd;
     }
 }
