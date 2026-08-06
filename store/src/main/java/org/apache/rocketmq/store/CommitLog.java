0724d74128a9990696324ae8f817f07f4e52be00
fuchong <yubao.fyb@alibaba-inc.com>
2026-08-06 10:59:49 +0800

    [ISSUE #10819] Refine CommitLog comment wording

--- store/src/main/java/org/apache/rocketmq/store/CommitLog.java
+++ store/src/main/java/org/apache/rocketmq/store/CommitLog.java
@@ -2092,7 +2092,7 @@ public class CommitLog implements Swappable {
 
             final long beginTimeMills = CommitLog.this.defaultMessageStore.now();
             CommitLog.this.getMessageStore().getPerfCounter().startTick("WRITE_MEMORY_TIME_MS");
-            // Copy the encoded message into the current CommitLog write buffer
+            // Append the encoded message to the current CommitLog buffer
             byteBuffer.put(preEncodeBuffer);
             CommitLog.this.getMessageStore().getPerfCounter().endTick("WRITE_MEMORY_TIME_MS");
             msgInner.setEncodedBuff(null);
