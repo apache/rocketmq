2d79a499eaecacbac1dbb16f05fa25e766876f85
liuhy <liuhongyu@apache.org>
2026-08-09 20:08:32 -0700

    [ISSUE #10890] Cover blank client language metadata
    Signed-off-by: liuhy <liuhongyu@apache.org>

--- proxy/src/test/java/org/apache/rocketmq/proxy/grpc/v2/client/ClientActivityTest.java
+++ proxy/src/test/java/org/apache/rocketmq/proxy/grpc/v2/client/ClientActivityTest.java
@@ -345,6 +345,25 @@ public class ClientActivityTest extends BaseActivityTest {
     }
 
     @Test
+    public void testBlankLanguageIsReportedAsInvalidArgument() throws Throwable {
+        ProxyContext context = createContext().setLanguage("");
+        try {
+            this.sendClientTelemetry(
+                context,
+                Settings.newBuilder()
+                    .setClientType(ClientType.PRODUCER)
+                    .setPublishing(Publishing.newBuilder()
+                        .addTopics(Resource.newBuilder().setName(TOPIC).build())
+                        .build())
+                    .build()).get();
+            fail();
+        } catch (ExecutionException e) {
+            StatusRuntimeException exception = (StatusRuntimeException) e.getCause();
+            assertEquals(Status.Code.INVALID_ARGUMENT, exception.getStatus().getCode());
+        }
+    }
+
+    @Test
     public void testEmptySettings() throws Throwable {
         ProxyContext context = createContext();
         try {
