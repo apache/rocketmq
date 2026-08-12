c311521121372665b8711880df6c3d3cd796000f
btlqql <2977859784@qq.com>
2026-08-13 00:11:24 +0800

    [ISSUE #10876] Fix BrokerIdentityInfo.equals for partial identities

--- controller/src/main/java/org/apache/rocketmq/controller/impl/heartbeat/BrokerIdentityInfo.java
+++ controller/src/main/java/org/apache/rocketmq/controller/impl/heartbeat/BrokerIdentityInfo.java
@@ -63,7 +63,9 @@ public class BrokerIdentityInfo implements Serializable {
 
         if (obj instanceof BrokerIdentityInfo) {
             BrokerIdentityInfo addr = (BrokerIdentityInfo) obj;
-            return clusterName.equals(addr.clusterName) && brokerName.equals(addr.brokerName) && brokerId.equals(addr.brokerId);
+            return Objects.equals(clusterName, addr.clusterName)
+                && Objects.equals(brokerName, addr.brokerName)
+                && Objects.equals(brokerId, addr.brokerId);
         }
         return false;
     }
