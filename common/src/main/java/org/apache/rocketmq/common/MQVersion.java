efa1be59d6fc747ad220f036c86030f05f0419f9
lizhimins <707364882@qq.com>
2026-08-20 13:41:42 +0800

    [ISSUE #10680] Release Apache RocketMQ 5.5.1 (#10961)

--- common/src/main/java/org/apache/rocketmq/common/MQVersion.java
+++ common/src/main/java/org/apache/rocketmq/common/MQVersion.java
@@ -18,7 +18,7 @@ package org.apache.rocketmq.common;
 
 public class MQVersion {
 
-    public static final int CURRENT_VERSION = Version.V5_5_0.ordinal();
+    public static final int CURRENT_VERSION = Version.V5_5_1.ordinal();
 
     private static final Version[] VERSION_VALUES = Version.values();
 
