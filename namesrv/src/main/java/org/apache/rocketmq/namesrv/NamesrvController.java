93c6d96f9099f7075203701d50c2ac1bfee7fb3b
qianye <wuxingcan.wxc@alibaba-inc.com>
2026-09-09 09:57:33 +0800

    [ISSUE #11072] Reject route queries when NameServer shuts down (#11073)

--- namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvController.java
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvController.java
@@ -69,6 +69,8 @@ public class NamesrvController {
     private final KVConfigManager kvConfigManager;
     private final RouteInfoManager routeInfoManager;
 
+    private volatile boolean shutdown;
+
     private RemotingClient remotingClient;
     private RemotingServer remotingServer;
 
@@ -237,7 +239,12 @@ public class NamesrvController {
         this.routeInfoManager.start();
     }
 
+    public boolean isShutdown() {
+        return shutdown;
+    }
+
     public void shutdown() {
+        this.shutdown = true;
         this.remotingClient.shutdown();
         this.remotingServer.shutdown();
         this.defaultExecutor.shutdown();
